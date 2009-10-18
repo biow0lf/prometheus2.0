7825bc82581ed6a800bb4a6039fa6532a5823d7e
Igor Zubkov <igor.zubkov@gmail.com>
2009-10-18 23:14:06 +0300

    rework branches

--- db/migrate/001_create_branches.rb
+++ db/migrate/001_create_branches.rb
@@ -3,16 +3,14 @@ class CreateBranches < ActiveRecord::Migration
     create_table :branches do |t|
       t.string :fullname
       t.string :urlname
-      t.string :http_srpms_url
-      t.string :ftp_srpms_url
-      t.string :http_noarch_url
-      t.string :ftp_noarch_url
-      t.string :http_i586_url
-      t.string :ftp_i586_url
-      t.string :http_x86_64_url
-      t.string :ftp_x86_64_url
-      t.string :acl_packages_url
-      t.string :acl_groups_url
+      t.string :srpms_path
+      t.string :binary_x86_path
+      t.string :noarch_path
+      t.string :binary_x86_64_path
+      t.string :acls_url
+      t.string :leaders_url
+      t.string :acls_groups_url
+      t.boolean :altlinux
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,67 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
-#
-#
-# real@, Евгений Ростовцев, real@altlinux.org
-
-# kill all branches from database
-#Branch.delete_all
-
 # add Sisyphus branch
 branch = Branch.new
 branch.fullname = 'Sisyphus'
 branch.urlname = 'Sisyphus'
-branch.http_srpms_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/SRPMS/'
-branch.ftp_srpms_url =  'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/SRPMS/'
-branch.http_noarch_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/noarch/RPMS/'
-branch.ftp_noarch_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/noarch/RPMS/'
-branch.http_i586_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/i586/RPMS/'
-branch.ftp_i586_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/i586/RPMS/'
-branch.http_x86_64_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/x86_64/RPMS/'
-branch.ftp_x86_64_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/x86_64/RPMS/'
-branch.acl_packages_url = 'http://git.altlinux.org/acl/list.packages.sisyphus'
-branch.acl_groups_url = 'http://git.altlinux.org/acl/list.groups.sisyphus'
+branch.srpms_path = "/path/*.src.rpm"
+branch.binary_x86_path = "/path/*.i586.rpm"
+branch.noarch_path = "/path/*.noarch.rpm"
+branch.binary_x86_64_path = "/path/*.x86_64.rpm"
+branch.acls_url = 'http://git.altlinux.org/acl/list.packages.sisyphus'
+branch.leaders_url = 'http://git.altlinux.org/acl/list.packages.sisyphus'
+branch.acls_groups_url = 'http://git.altlinux.org/acl/list.groups.sisyphus'
+branch.altlinux = true
 branch.save!
-
-
-# add 4.0 branch
-branch = Branch.new
-branch.fullname = '4.0'
-branch.urlname = 'Branch4'
-#branch.http_srpms_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/SRPMS/'
-#branch.ftp_srpms_url =  'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/SRPMS/'
-#branch.http_noarch_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/noarch/RPMS/'
-#branch.ftp_noarch_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/noarch/RPMS/'
-#branch.http_i586_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/i586/RPMS/'
-#branch.ftp_i586_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/i586/RPMS/'
-#branch.http_x86_64_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/x86_64/RPMS/'
-#branch.ftp_x86_64_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/x86_64/RPMS/'
-#branch.acl_packages_url = 'http://git.altlinux.org/acl/list.packages.sisyphus'
-#branch.acl_groups_url = 'http://git.altlinux.org/acl/list.groups.sisyphus'
-branch.save!
-
 
 # add Redora Rawhide
 branch = Branch.new
 branch.fullname = 'Rawhide'
 branch.urlname = 'Rawhide'
-#branch.http_srpms_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/SRPMS/'
-#branch.ftp_srpms_url =  'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/SRPMS/'
-#branch.http_noarch_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/noarch/RPMS/'
-#branch.ftp_noarch_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/noarch/RPMS/'
-#branch.http_i586_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/i586/RPMS/'
-#branch.ftp_i586_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/i586/RPMS/'
-#branch.http_x86_64_url = 'http://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/x86_64/RPMS/'
-#branch.ftp_x86_64_url = 'ftp://ftp.altlinux.org/pub/distributions/ALTLinux/Sisyphus/files/x86_64/RPMS/'
-#branch.acl_packages_url = 'http://git.altlinux.org/acl/list.packages.sisyphus'
-#branch.acl_groups_url = 'http://git.altlinux.org/acl/list.groups.sisyphus'
+branch.srpms_path = "/path/*.src.rpm"
+branch.altlinux = false
 branch.save!
 
+# packager list
 Packager.create(:name => 'Nobody', :email => 'noboby@altlinux.org', :login => '@nobody', :team => true)
 
 Packager.create(:name => 'Igor Zubkov', :email => 'icesik@altlinux.org', :login => 'icesik', :team => false)
