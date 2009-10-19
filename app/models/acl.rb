36548cd67f8272260d3debb54e4f41c892d957b4
Igor Zubkov <igor.zubkov@gmail.com>
2009-10-19 03:01:22 +0300

    update models

--- app/models/acl.rb
+++ app/models/acl.rb
@@ -1,4 +1,8 @@
 class Acl < ActiveRecord::Base
+#  validates_presence_of :package, :login, :branch, :packager_id
   validates_presence_of :package, :login, :branch
+
   belongs_to :packager
+  belongs_to :srpm
+#  belongs_to :repocop
 end
--- app/models/packager.rb
+++ app/models/packager.rb
@@ -2,7 +2,12 @@ class Packager < ActiveRecord::Base
   validates_presence_of :name, :email, :login
   validates_uniqueness_of :login
 #  validates_uniqueness_of :email, :login
-  has_many :acls
+  # packager_id
+  has_many :acls, :order => "package ASC"
+  has_many :srpms, :through => :acls, :order => "name ASC"
+#  has_many :repocops, :through => :srpms, :order => "srcname ASC"
+#  has_many :repocops, :through => :acls, :order => "package ASC"
+
 #  has_many :packages
 #  has_many :srpms
 end
