4cc6df7a9c714292f5a6d61b12a48c5d29558e06
Igor Zubkov <icesik@altlinux.org>
2010-01-03 23:10:41 +0200

    cleanup

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,23 @@
 class Team < ActiveRecord::Base
-  validates_presence_of :name, :login, :branch_id
+  validates_presence_of :name, :login, :branch, :vendor
+
+  def self.update_from_gitalt(vendor, branch)
+    ActiveRecord::Base.transaction do
+      url = Branch.find :first, :conditions => { :vendor => vendor, :name => branch }
+      ActiveRecord::Base.connection.execute("DELETE FROM teams WHERE branch = '" + branch + "' AND vendor = '" + vendor + "'")
+
+      file = open(URI.escape(url.acls_groups_url)).read
+      file.each_line do |line|
+        team_name  = line.split[0]
+        for i in 1..line.split.count-1
+          if i == 1
+            Team.create :name => team_name, :login => line.split[i], :leader => true, :branch => branch, :vendor => vendor
+          else
+            Team.create :name => team_name, :login => line.split[i], :leader => false, :branch => branch, :vendor => vendor
+          end
+        end
+      end
+    end
+ end
+
 end
