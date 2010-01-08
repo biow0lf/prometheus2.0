3d9a19d5a9668be55263176776677f006724d695
Igor Zubkov <icesik@altlinux.org>
2010-01-08 03:36:24 +0200

    update iphone page

--- app/controllers/iphone_controller.rb
+++ app/controllers/iphone_controller.rb
@@ -4,6 +4,19 @@ class IphoneController < ApplicationController
   def index
     @packagers = Packager.find_all_packagers_in_sisyphus
     @teams = Packager.find_all_teams_in_sisyphus
+    @groups = Group.find_groups_in_sisyphus
   end
+
+#  def packager
+#    @branch = Branch.first :conditions => { :vendor => 'ALT Linux', :name => 'Sisyphus' }
+#    @packager = Packager.first :conditions => {
+#                                 :login => params[:login].downcase,
+#                                 :team => false }
+#    @acls = Acl.all :select => 'package',
+#                    :conditions => {
+#                      :login => params[:login],
+#                      :branch => @branch.name,
+#                      :vendor => @branch.vendor }
+#  end
 
 end
