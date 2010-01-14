39d8093069ce7110568fee07e4b0f23119d31be2
Igor Zubkov <icesik@altlinux.org>
2010-01-15 01:32:21 +0200

    clean

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -11,47 +11,43 @@ class SitemapController < ApplicationController
   def sitemap_ru1
     headers['Content-Type'] = "application/xml"
 
-    @srpms = Srpm.find :all,
-                       :select => 'name, branch',
-                       :order => 'name ASC',
-                       :limit => 5000,
-                       :conditions => {
-                         :branch => 'Sisyphus' }
+    @srpms = Srpm.all :select => 'name, branch',
+                      :order => 'name ASC',
+                      :limit => 5000,
+                      :conditions => { :branch => 'Sisyphus',
+                                       :vendor => 'ALT Linux' }
   end
 
   def sitemap_ru2
     headers['Content-Type'] = "application/xml"
 
-    @srpms = Srpm.find :all,
-                       :select => 'name, branch',
-                       :order => 'name ASC',
-                       :limit => 5000,
-                       :offset => 5000,
-                       :conditions => {
-                         :branch => 'Sisyphus' }
+    @srpms = Srpm.all :select => 'name, branch, vendor',
+                      :order => 'name ASC',
+                      :limit => 5000,
+                      :offset => 5000,
+                      :conditions => { :branch => 'Sisyphus',
+                                       :vendor => 'ALT Linux' }
   end
 
   def sitemap_en1
     headers['Content-Type'] = "application/xml"
 
-    @srpms = Srpm.find :all,
-                       :select => 'name, branch',
-                       :order => 'name ASC',
-                       :limit => 5000,
-                       :conditions => {
-                         :branch => 'Sisyphus' }
+    @srpms = Srpm.all :select => 'name, branch, vendor',
+                      :order => 'name ASC',
+                      :limit => 5000,
+                      :conditions => { :branch => 'Sisyphus',
+                                       :vendor => 'ALT Linux' }
   end
 
   def sitemap_en2
     headers['Content-Type'] = "application/xml"
 
-    @srpms = Srpm.find :all,
-                       :select => 'name, branch',
-                       :order => 'name ASC',
-                       :limit => 5000,
-                       :offset => 5000,
-                       :conditions => {
-                         :branch => 'Sisyphus' }
+    @srpms = Srpm.all :select => 'name, branch, vendor',
+                      :order => 'name ASC',
+                      :limit => 5000,
+                      :offset => 5000,
+                      :conditions => { :branch => 'Sisyphus',
+                                       :vendor => 'ALT Linux' }
   end
 
 end
