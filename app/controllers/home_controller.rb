76c035129577ed6810458b6cad23b46876a368e6
Igor Zubkov <igor.zubkov@gmail.com>
2010-03-01 23:11:51 +0200

    remove home#stats and home#top15

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,15 +14,6 @@ class HomeController < ApplicationController
   def rss
   end
 
-  def stats
-#    @packages_counter = Srpm.count
-  end
-
-  def top15
-#    @top15 = Packager.find_by_sql("SELECT COUNT(srpms.name) AS counter, packagers.name AS name, packagers.login AS login FROM srpms, packagers WHERE packagers.id = srpms.packager_id AND packagers.team = false AND srpms.branch = 'Sisyphus' GROUP BY packagers.name, packagers.login ORDER BY 1 DESC LIMIT 1500")
-    @top15 = Packager.find_by_sql("SELECT COUNT(acls.package) AS counter, packagers.name AS name, packagers.login AS login FROM acls, packagers WHERE packagers.login = acls.login AND packagers.team = false AND acls.branch = 'Sisyphus' GROUP BY packagers.name, packagers.login ORDER BY 1 DESC LIMIT 1500")
-  end
-
   def search
     if params[:request].empty?
       redirect_to :action => "index"
