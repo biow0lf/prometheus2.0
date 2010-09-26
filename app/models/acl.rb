5edc2d2a34a6ed2ce3faed5ee5454c322f5a5ee3
Igor Zubkov <igor.zubkov@gmail.com>
2010-09-27 02:15:27 +0300

    fix acl model

--- app/models/acl.rb
+++ app/models/acl.rb
@@ -7,7 +7,7 @@ class Acl < ActiveRecord::Base
     ActiveRecord::Base.transaction do
       ActiveRecord::Base.connection.execute("DELETE FROM acls WHERE branch = '" + branch + "' AND vendor = '" + vendor + "'")
 
-      file = open(URI.escape(url.acls_url)).read
+      file = open(URI.escape(url)).read
       file.each_line do |line|
         package = line.split[0]
         for i in 1..line.split.count-1
