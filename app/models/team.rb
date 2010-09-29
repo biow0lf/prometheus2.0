b50d241cd706dea5ee2e9a2ff8c47acb38e7cb08
Igor Zubkov <igor.zubkov@gmail.com>
2010-09-29 05:18:18 +0300

    update team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,12 +1,11 @@
 class Team < ActiveRecord::Base
   validates_presence_of :name, :login, :branch, :vendor
 
-  def self.update_from_gitalt(vendor, branch)
+  def self.update_from_gitalt(vendor, branch, url)
     ActiveRecord::Base.transaction do
-      url = Branch.find :first, :conditions => { :vendor => vendor, :name => branch }
       ActiveRecord::Base.connection.execute("DELETE FROM teams WHERE branch = '" + branch + "' AND vendor = '" + vendor + "'")
 
-      file = open(URI.escape(url.acls_groups_url)).read
+      file = open(URI.escape(url)).read
       file.each_line do |line|
         team_name  = line.split[0]
         for i in 1..line.split.count-1
@@ -19,5 +18,4 @@ class Team < ActiveRecord::Base
       end
     end
  end
-
 end
