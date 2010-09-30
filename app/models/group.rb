e36555c733a71ab4e2cb9f520e0a11f4c95fe53d
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-01 02:21:23 +0300

    update group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,10 +5,11 @@ class Group < ActiveRecord::Base
   def self.find_groups_in_sisyphus
     find_by_sql("SELECT COUNT(srpms.name) AS counter, groups.name
                  FROM srpms, groups, branches
-                 WHERE groups.branch = branches.name
+                 WHERE groups.branch_id = branches.id
                  AND branches.name = 'Sisyphus'
+                 AND branches.vendor = 'ALT Linux'
                  AND srpms.group = groups.name
-                 AND srpms.branch = branches.name
+                 AND srpms.branch_id = branches.id
                  GROUP BY groups.name
                  ORDER BY groups.name")
   end
