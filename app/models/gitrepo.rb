2c96275678e68d78f3b23315515b78d63a4c3d4f
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-04 03:07:40 +0300

    fix gitrepo model

--- app/models/gitrepo.rb
+++ app/models/gitrepo.rb
@@ -1,4 +1,4 @@
 class Gitrepo < ActiveRecord::Base
   validates_presence_of :repo, :login, :lastchange
-  belongs_to :packager, :foreign_key => 'login', :primary_key => 'login'
+  belongs_to :maintainer, :foreign_key => 'login', :primary_key => 'login'
 end
