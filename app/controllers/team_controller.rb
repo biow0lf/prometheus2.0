67427fdf8ebd58e5d518c21265c8269fe8ba635b
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-04 08:36:32 +0300

    update team controller

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -6,28 +6,32 @@ class TeamController < ApplicationController
     @team = Maintainer.first :conditions => {
                                :login => '@' + params[:name],
                                :team => true }
+#    @acls = Acl.all :conditions => {
+#                      :login => '@' + params[:name],
+#                      :branch_id => @branch.id },
+#                    :order => 'LOWER(package)'
+
     @acls = Acl.all :conditions => {
-                      :login => '@' + params[:name],
-                      :branch_id => @branch.id },
-                    :order => 'LOWER(package)'
+                      :maintainer_id => @team.id,
+                      :branch_id => @branch.id }
 
     if @team != nil
-      @leader = Team.find_by_sql(['SELECT teams.login, maintainers.name
-                               FROM teams, maintainers, branches
-                               WHERE maintainers.login = teams.login
-                               AND teams.name = ?
-                               AND teams.branch_id = branches.id
-                               AND branches.name = ?
-                               AND leader = true
-                               LIMIT 1', '@' + params[:name], @branch.name ])
-
-      @members = Team.find_by_sql(['SELECT teams.login, maintainers.name
-                               FROM teams, maintainers, branches
-                               WHERE maintainers.login = teams.login
-                               AND teams.name = ?
-                               AND teams.branch_id = branches.id
-                               AND branches.name = ?
-                               ORDER BY LOWER(maintainers.name)', '@' + params[:name], @branch.name ])
+#      @leader = Team.find_by_sql(['SELECT teams.login, maintainers.name
+#                               FROM teams, maintainers, branches
+#                               WHERE maintainers.login = teams.login
+#                               AND teams.name = ?
+#                               AND teams.branch_id = branches.id
+#                               AND branches.name = ?
+#                               AND leader = true
+#                               LIMIT 1', '@' + params[:name], @branch.name ])
+#
+#      @members = Team.find_by_sql(['SELECT teams.login, maintainers.name
+#                               FROM teams, maintainers, branches
+#                               WHERE maintainers.login = teams.login
+#                               AND teams.name = ?
+#                               AND teams.branch_id = branches.id
+#                               AND branches.name = ?
+#                               ORDER BY LOWER(maintainers.name)', '@' + params[:name], @branch.name ])
     else
       render :status => 404, :action => "nosuchteam"
     end
