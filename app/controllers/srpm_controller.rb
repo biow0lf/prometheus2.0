70a40e3223ff591f3a2330685411a07a7727d762
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-04 04:24:23 +0300

    show other branches on changelog page

--- app/controllers/srpm_controller.rb
+++ app/controllers/srpm_controller.rb
@@ -53,7 +53,12 @@ class SrpmController < ApplicationController
                          :name => params[:name],
                          :branch_id => branch.id },
                        :include => [:group, :branch]
-    if @srpm == nil
+    if @srpm != nil
+      @allsrpms = Srpm.find :all, :conditions => {
+                                    :name => params[:name] },
+                                  :joins => :branch,
+                                  :order => "branches.order_id"
+    else
       render :status => 404, :action => "nosuchpackage"
     end
   end
