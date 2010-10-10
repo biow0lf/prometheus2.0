24f12cf65e660505c1ede35025ac25ccddd45808
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-10 14:56:25 +0300

    fix typos

--- app/models/group.rb
+++ app/models/group.rb
@@ -31,31 +31,25 @@ class Group < ActiveRecord::Base
         param1 = line.split('/')[1]
         param2 = line.split('/')[2]
         
-        if !Group.first(:conditions => { :name => param0, :branch_id => br.id })
+        if !Group.first(:conditions => { :name => param0, :branch_id => br.id } )
           group0 = Group.create(:name => param0, :branch_id => br.id)
         else
-          group0 = Group.first(:name => param0, :branch_id => br.id)
+          group0 = Group.first(:conditions => { :name => param0, :branch_id => br.id } )
         end
         
-        puts param0
-        
         if param1 != nil
-          if !Group.first(:conditions => { :name => param1, :branch_id => br.id, :parent_id => group0.id })
+          if !Group.first(:conditions => { :name => param1, :branch_id => br.id, :parent_id => group0.id } )
             group1 = Group.create(:name => param1, :branch_id => br.id)
             group1.move_to_child_of(group0)
           else
-            group1 = Group.first(:name => param1, :branch_id => br.id, :parent_id => group0.id)
+            group1 = Group.first(:conditions => { :name => param1, :branch_id => br.id, :parent_id => group0.id } )
           end
         end
-        
-        puts param1
         
         if param2 != nil
           group2 = Group.create(:name => param2, :branch_id => br.id)
           group2.move_to_child_of(group1)
         end
-        
-        puts param2
       end
     else
       puts Time.now.to_s + ": groups already imported"
