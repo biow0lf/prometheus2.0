c0f81b456c8f0baf52902f0e8d93f393a26813a2
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-19 16:04:32 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,42 @@
 SUPPORTED_LOCALES = /(en|ru|uk|br)/
 
 Prometheus20::Application.routes.draw do |map|
-  match '(/:locale)/iphone/', :to => 'iphone#index'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/iphone/packager/:login', :to => 'iphone#maintainer_info'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/iphone/packages/:group(/:group2(/:group3))', :to => 'iphone#bygroup'#, :requirements => { :locale => /(en|ru|uk|br)/ }
+  match '(/:locale)/iphone/', :to => 'iphone#index', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/iphone/packager/:login', :to => 'iphone#maintainer_info', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/iphone/packages/:group(/:group2(/:group3))', :to => 'iphone#bygroup', :constraints => { :locale => SUPPORTED_LOCALES }
 
-  match '(/:locale)/search', :to => 'home#search'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/find.shtml', :to => 'home#search'#, :requirements => { :locale => /(en|ru|uk|br)/ }
+  match '(/:locale)/search', :to => 'home#search', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/find.shtml', :to => 'home#search', :constraints => { :locale => SUPPORTED_LOCALES }
 
-  match '(/:locale)/news', :to => 'pages#news'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/rss', :to => 'pages#rss'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/security', :to => 'pages#security'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/project', :to => 'pages#project'#, :requirements => { :locale => /(en|ru|uk|br)/ }
+  match '(/:locale)/news', :to => 'pages#news', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/rss', :to => 'pages#rss', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/security', :to => 'pages#security', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/project', :to => 'pages#project', :constraints => { :locale => SUPPORTED_LOCALES }
 
-  match '(/:locale)/packages', :to => 'home#groups_list'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/people', :to => 'home#maintainers_list'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/team/:name', :to => 'team#info'#, :requirements => { :locale => /(en|ru|uk|br)/ }
+  match '(/:locale)/packages', :to => 'home#groups_list', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/people', :to => 'home#maintainers_list', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/team/:name', :to => 'team#info', :constraints => { :locale => SUPPORTED_LOCALES }
 
-  match '(/:locale)/packager/:login', :to => 'maintainer#info'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/packager/:login/srpms', :to => 'maintainer#srpms'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/packager/:login/acls', :to => 'maintainer#acls'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/packager/:login/gear', :to => 'maintainer#gear'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/packager/:login/bugs', :to => 'maintainer#bugs'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/packager/:login/allbugs', :to => 'maintainer#allbugs'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/packager/:login/repocop', :to => 'maintainer#repocop'#, :requirements => { :locale => /(en|ru|uk|br)/ }
-#  match '(/:locale)/packager/:login/repocop/rss', :to => 'maintainer#repocop'
+  match '(/:locale)/packager/:login', :to => 'maintainer#info', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/packager/:login/srpms', :to => 'maintainer#srpms', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/packager/:login/acls', :to => 'maintainer#acls', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/packager/:login/gear', :to => 'maintainer#gear', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/packager/:login/bugs', :to => 'maintainer#bugs', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/packager/:login/allbugs', :to => 'maintainer#allbugs', :constraints => { :locale => SUPPORTED_LOCALES }
+  match '(/:locale)/packager/:login/repocop', :to => 'maintainer#repocop', :constraints => { :locale => SUPPORTED_LOCALES }
+#  match '(/:locale)/packager/:login/repocop/rss', :to => 'maintainer#repocop', :constraints => { :locale => SUPPORTED_LOCALES }
 
-  match '(/:locale)/packages/:group(/:group2(/:group3))', :to => 'home#bygroup'#, :requirements => { :locale => /(en|ru|uk|br)/ }
+  match '(/:locale)/packages/:group(/:group2(/:group3))', :to => 'home#bygroup', :constraints => { :locale => SUPPORTED_LOCALES }
 
-  match '(/:locale)/srpm/:branch/:name', :to => 'srpm#main'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/srpm/:branch/:name/changelog', :to => 'srpm#changelog'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/srpm/:branch/:name/spec', :to => 'srpm#rawspec'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/srpm/:branch/:name/get', :to => 'srpm#download'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/srpm/:branch/:name/gear', :to => 'srpm#gear'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/srpm/:branch/:name/bugs', :to => 'srpm#bugs'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/srpm/:branch/:name/allbugs', :to => 'srpm#allbugs'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-  match '(/:locale)/srpm/:branch/:name/repocop', :to => 'srpm#repocop'#, :requirements => { :branch => /[^\/]+/, :name => /[^\/]+/, :locale => /(en|ru|uk|br)/ }
-#  match '(/:locale)/srpm/:branch/:name/repocop.:format', :to => 'home#repocop'#, :requirements => { :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name', :to => 'srpm#main', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name/changelog', :to => 'srpm#changelog', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name/spec', :to => 'srpm#rawspec', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name/get', :to => 'srpm#download', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name/gear', :to => 'srpm#gear', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name/bugs', :to => 'srpm#bugs', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name/allbugs', :to => 'srpm#allbugs', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+  match '(/:locale)/srpm/:branch/:name/repocop', :to => 'srpm#repocop', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
+#  match '(/:locale)/srpm/:branch/:name/repocop.:format', :to => 'home#repocop', :constraints => { :locale => SUPPORTED_LOCALES, :branch => /[^\/]+/, :name => /[^\/]+/ }
 
 #  map.connect '/repocop', :controller => 'repocop', :action => 'index'
 #  map.connect '/repocop/by-test/:testname', :controller => 'repocop', :action => 'bytest'
