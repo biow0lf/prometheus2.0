2ee35d215d7f3301efc40fd4d94e7bfd227eabcb
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-19 19:36:18 +0300

    update production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Prometheus20::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end
