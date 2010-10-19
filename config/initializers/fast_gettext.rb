6b72849674e22058e54877ab950ca2668432ad0d
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-19 15:53:46 +0300

    fix fast_gettext initializer

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,3 @@
-FastGettext.add_text_domain 'prometheus', :path => 'po'
-FastGettext.default_text_domain = 'prometheus'
+FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
+FastGettext.default_text_domain = 'app'
 FastGettext.default_available_locales = ['en', 'ru', 'uk', 'br']
