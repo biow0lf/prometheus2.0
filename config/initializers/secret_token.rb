a3f2d8889c5cf40a43f658fa9151ffbc8b111ce2
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-19 13:39:45 +0300

    upgrade to rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prometheus20::Application.config.secret_token = 'a87e2803d5e0d4a638fc8e7e5e98855ef95e6ccd31c12426d7db0039d699245e2ccb4a7c391afa103d369f050db1e9732e7cebd9bfd5c248fc986db8b68cd2c6'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_sisyphus_session',
-  :secret      => 'eb9ff600199d7eb0f4ca70eebdf3fae11807c36f1a26a0d0bd8066664b0eaca548bb5470eeb994989244d69bf37e63c3689174f8c15cb585b2485a456699b6e5'
-}
+Prometheus20::Application.config.session_store :cookie_store, :key => '_prometheus2.0_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Prometheus20::Application.config.session_store :active_record_store
