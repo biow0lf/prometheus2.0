e5f809e169c1f4328c829568d3d676ba28e26bde
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-25 15:04:23 +0300

    layout nil to cli controller

--- app/controllers/cli_controller.rb
+++ app/controllers/cli_controller.rb
@@ -1,4 +1,6 @@
 class CliController < ApplicationController
+  layout nil
+
   def srpm_info
     @branch = Branch.where(:name => params[:branch], :vendor => params[:vendor]).first
     @srpm = Srpm.where(:name => params[:name], :branch_id => @branch.id).first
