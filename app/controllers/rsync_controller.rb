60277376f47ed4a20d417822026533690fe0619c
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-28 00:50:38 +0300

    rsync page

--- app/controllers/rsync_controller.rb
+++ app/controllers/rsync_controller.rb
@@ -1,5 +1,6 @@
 class RsyncController < ApplicationController
   def index
+    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
+    @groups = @branch.groups.find(:all, :conditions => {:parent_id => nil}, :order => 'LOWER(name)')
   end
-
 end
