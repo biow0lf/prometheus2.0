fb74bfa14aefa2052b991dba5c35e93d0274cbab
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-30 16:23:29 +0300

    fix

--- app/controllers/redirector_controller.rb
+++ app/controllers/redirector_controller.rb
@@ -3,9 +3,9 @@ class RedirectorController < ApplicationController
     @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
     @package = @branch.packages.where(:name => params[:name]).first
     if !@package.nil?
-      redirect_to '/en/srpm/Sisyphus/' + @package.srpm.name, 302
+      redirect_to srpm_main_path(:name => @package.srpm.name, :branch => 'Sisyphus'), 302
     else
-      redirect_to '/en/srpm/Sisyphus/' + params[:name], 302
+      redirect_to srpm_main_path(:name => params[:name], :branch => 'Sisyphus'), 302
     end
   end
 end
