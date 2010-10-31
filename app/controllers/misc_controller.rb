583dfb9646d695376439ed6b15399e07b3667a57
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-31 16:46:56 +0200

    order

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,7 +1,7 @@
 class MiscController < ApplicationController
   def bugs
-    @bug_statuses = Bug.where("").select("DISTINCT bug_status").all
-    @resolutions = Bug.where("").select("DISTINCT resolution").all
+    @bug_statuses = Bug.where("").select("DISTINCT bug_status").order("bug_status ASC").all
+    @resolutions = Bug.where("").select("DISTINCT resolution").order("resolution ASC").all
     @bugs = Bug.where("").select("bug_status","resolution")
   end
 end
