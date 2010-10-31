a58514f77202b95582709a7fc76eb8c7cfe06eb8
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-01 01:04:14 +0200

    clean

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  #include ExceptionNotifiable
-
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   before_filter :set_search
