389588979b1cc3011a20ea4f516f1c933d21c1c7
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-07 15:40:39 +0200

    update changelog model

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -1,4 +1,8 @@
 class Changelog < ActiveRecord::Base
-  validates_presence_of :srpm_id, :changelogtime, :changelogname, :changelogtext
+  validates :srpm_id, :presence => true
+  validates :changelogtime, :presence => true
+  validates :changelogname, :presence => true
+  validates :changelogtext, :presence => true
+
   belongs_to :srpm
 end
