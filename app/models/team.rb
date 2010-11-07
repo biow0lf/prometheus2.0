1c0ac1463853aff1049e25304304f71c01364ee0
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-07 13:55:23 +0200

    clean

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,20 +7,20 @@ class Team < ActiveRecord::Base
   belongs_to :maintainer
 
   def self.import_teams(vendor, branch, url)
-    br = Branch.first :conditions => { :name => branch, :vendor => vendor }
-    if br.teams.count(:all) == 0
+    b = Branch.where(:name => branch, :vendor => vendor).first
+    if b.teams.count(:all) == 0
       file = open(URI.escape(url)).read
       file.each_line do |line|
         team_name  = line.split[0]
         for i in 1..line.split.count-1
-          maintainer = Maintainer.first :conditions => { :login => line.split[i] }
+          maintainer = Maintainer.where(:login => line.split[i]).first
           if maintainer.nil?
             puts Time.now.to_s + ": maintainer not found '" + line.split[i] + "'"
           else
             if i == 1
-              Team.create :name => team_name, :maintainer_id => maintainer.id, :leader => true, :branch_id => br.id
+              Team.create(:name => team_name, :maintainer_id => maintainer.id, :leader => true, :branch_id => b.id)
             else
-              Team.create :name => team_name, :maintainer_id => maintainer.id, :leader => false, :branch_id => br.id
+              Team.create(:name => team_name, :maintainer_id => maintainer.id, :leader => false, :branch_id => b.id)
             end
           end
         end
