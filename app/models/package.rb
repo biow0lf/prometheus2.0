e6d5cc2837bf7b3af1d57ead46fcf2ddf616199a
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-07 02:46:01 +0200

    don't add package in db if srpm is missing

--- app/models/package.rb
+++ app/models/package.rb
@@ -8,44 +8,48 @@ class Package < ActiveRecord::Base
   belongs_to :group
 
   def self.import_rpm(vendor, branch, file)
-    br = Branch.first :conditions => { :name => branch, :vendor => vendor }
+    b = Branch.where(:name => branch, :vendor => vendor).first
     rpm = RPM::Package::open(file)
-    package = Package.new
-    package.filename = file.split('/')[-1]
-    package.sourcepackage = rpm[1044]
-    package.name = rpm.name
-    package.version = rpm.version.v
-    package.release = rpm.version.r
-    package.arch = rpm.arch
+    if b.srpms.where(:name => rpm[1044]).count == 1
+      package = Package.new
+      package.filename = file.split('/')[-1]
+      package.sourcepackage = rpm[1044]
+      package.name = rpm.name
+      package.version = rpm.version.v
+      package.release = rpm.version.r
+      package.arch = rpm.arch
 
-    case rpm[1016].split('/').count
-    when 1
-      group = br.groups.where(:name => rpm[1016], :parent_id => nil).first
-    when 2
-      group = br.groups.where(:name => rpm[1016].split('/')[0], :parent_id => nil).first.children.where(:name => rpm[1016].split('/')[1]).first
-    when 3
-      group = br.groups.where(:name => rpm[1016].split('/')[0], :parent_id => nil).first.children.where(:name => rpm[1016].split('/')[1]).first.children.where(:name => rpm[1016].split('/')[2]).first
-    else
-      puts Time.now.to_s + ": too nested groups level"
-    end    
+      case rpm[1016].split('/').count
+      when 1
+        group = b.groups.where(:name => rpm[1016], :parent_id => nil).first
+      when 2
+        group = b.groups.where(:name => rpm[1016].split('/')[0], :parent_id => nil).first.children.where(:name => rpm[1016].split('/')[1]).first
+      when 3
+        group = b.groups.where(:name => rpm[1016].split('/')[0], :parent_id => nil).first.children.where(:name => rpm[1016].split('/')[1]).first.children.where(:name => rpm[1016].split('/')[2]).first
+      else
+        puts Time.now.to_s + ": too nested groups level"
+      end    
 
-    package.group_id = group.id
-    package.epoch = rpm[1003]
-    package.summary = rpm[1004]
-    package.summary = 'Broken' if rpm.name == 'openmoko_dfu-util'
-    package.license = rpm[1014]
-    package.url = rpm[1020]
-    package.description = rpm[1005]
-    package.buildtime = Time.at(rpm[1006])
-    package.size = File.size(file)
-    package.branch_id = br.id
-    srpm = Srpm.find :first, :conditions => { :filename => rpm[1044], :branch_id => br.id }
-    package.srpm_id = srpm.id
-    if package.save
-      $redis.set br.name + ":" + package.filename, 1
-      puts Time.now.to_s + ": updated '" + package.filename + "'"
+      package.group_id = group.id
+      package.epoch = rpm[1003]
+      package.summary = rpm[1004]
+      package.summary = 'Broken' if rpm.name == 'openmoko_dfu-util'
+      package.license = rpm[1014]
+      package.url = rpm[1020]
+      package.description = rpm[1005]
+      package.buildtime = Time.at(rpm[1006])
+      package.size = File.size(file)
+      package.branch_id = b.id
+      srpm = b.srpms.where(:filename => rpm[1044]).first
+      package.srpm_id = srpm.id
+      if package.save
+        $redis.set b.name + ":" + package.filename, 1
+        puts Time.now.to_s + ": updated '" + package.filename + "'"
+      else
+        puts Time.now.to_s + ": failed to update '" + package.filename + "'"
+      end
     else
-      puts Time.now.to_s + ": failed to update '" + package.filename + "'"
+      puts Time.now.to_s + ": srpm '" + rpm[1044] + "' not found in db"
     end
   end
 
