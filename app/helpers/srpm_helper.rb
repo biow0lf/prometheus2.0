3f8dd4e95aaef0961a2f00b32466c3ecb6962ed0
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-07 16:58:58 +0200

    simple_changelog

--- app/helpers/srpm_helper.rb
+++ app/helpers/srpm_helper.rb
@@ -1,2 +1,11 @@
 module SrpmHelper
+  def simple_changelog(text, html_options={}, options={})                                                                                                                      
+    text = ''.html_safe if text.nil?                                                                                                                                        
+    start_tag = tag('<li>', html_options, true)                                                                                                                                
+    text = sanitize(text) unless options[:sanitize] == false                                                                                                                
+    text.gsub!(/\r\n?/, "\n")                    # \r\n and \r -> \n                                                                                                        
+    text.gsub!(/([^\n]\n)(?=[^\n])/, '\1</li><li>') # 1 newline   -> </li><li>                                                                                                        
+    text.insert 0, start_tag                                                                                                                                                
+    text.html_safe.safe_concat("</li>")                                                                                                                                      
+  end
 end
