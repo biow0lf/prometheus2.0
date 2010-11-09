d706fb212ce840ea7d95dc83e93a89fbaeda1d8b
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-09 15:24:15 +0200

    fix changelog order on srpm#changelog

--- app/controllers/srpm_controller.rb
+++ app/controllers/srpm_controller.rb
@@ -13,7 +13,8 @@ class SrpmController < ApplicationController
 
   def changelog
     @branch = Branch.where(:vendor => 'ALT Linux', :name => params[:branch]).first
-    @srpm = @branch.srpms.where(:name => params[:name]).includes(:group, :branch, :changelog).first
+    @srpm = @branch.srpms.where(:name => params[:name]).includes(:group, :branch).first
+    @changelogs = @srpm.changelog.order('create_at ASC').all
     if @srpm != nil
       @allsrpms = Srpm.where(:name => params[:name]).joins(:branch).order("branches.order_id").all
     else
