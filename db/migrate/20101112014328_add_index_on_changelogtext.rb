fa740b916edc2e971bd963d0884084c3bdadc0b1
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-14 14:09:29 +0200

    remove broken migration

--- db/migrate/20101112014328_add_index_on_changelogtext.rb
+++
@@ -1,9 +0,0 @@
-class AddIndexOnChangelogtext < ActiveRecord::Migration
-  def self.up
-    add_index :changelogs, :changelogtext
-  end
-
-  def self.down
-    remove_index :changelogs, :changelogtext
-  end
-end
