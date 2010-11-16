e444c397ebc4c94b2f80a2f311b1af717c806c99
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-16 18:08:12 +0200

    fix title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def title(page_title)
-    content_for(:title) { page_title }
+    content_for(:title) { page_title.html_safe }
   end
 
   def url(string)
