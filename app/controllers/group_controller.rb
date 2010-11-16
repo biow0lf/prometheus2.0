abc3252f124d27614f508da4afdaf4b17861680b
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-16 03:43:31 +0200

    move group actions to group controller

---
+++ app/controllers/group_controller.rb
@@ -0,0 +1,20 @@
+class GroupController < ApplicationController
+  def groups_list
+    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
+    @groups = @branch.groups.find(:all, :conditions => {:parent_id => nil}, :order => 'LOWER(name)')
+  end
+
+  def bygroup
+    # TODO: branch can be not only Sisyphus!
+    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
+    @group = @branch.groups.find(:first, :conditions => { :name => params[:group], :parent_id => nil })
+    if !params[:group2].nil?
+      @group = @branch.groups.find(:first, :conditions => { :name => params[:group2], :parent_id => @group.id })
+      if !params[:group3].nil?
+        @group = @branch.groups.find(:first, :conditions => { :name => params[:group3], :parent_id => @group.id })
+      end
+    end
+    render :status => 404, :action => "nosuchgroup" and return if @group == nil
+    @srpms = @group.srpms.find(:all, :order => 'LOWER(name)')
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,24 +19,6 @@ class HomeController < ApplicationController
     end
   end
 
-  def groups_list
-    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
-    @groups = @branch.groups.find(:all, :conditions => {:parent_id => nil}, :order => 'LOWER(name)')
-  end
-
-  def bygroup
-    # TODO: branch can be not only Sisyphus!
-    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
-    @group = @branch.groups.find(:first, :conditions => { :name => params[:group], :parent_id => nil })
-    if !params[:group2].nil?
-      @group = @branch.groups.find(:first, :conditions => { :name => params[:group2], :parent_id => @group.id })
-      if !params[:group3].nil?
-        @group = @branch.groups.find(:first, :conditions => { :name => params[:group3], :parent_id => @group.id })
-      end
-    end
-    @srpms = @group.srpms.find(:all, :order => 'LOWER(name)')
-  end
-
   def maintainers_list
     @maintainers = Maintainer.find_all_maintainers_in_sisyphus
     @teams = Maintainer.find_all_teams_in_sisyphus
