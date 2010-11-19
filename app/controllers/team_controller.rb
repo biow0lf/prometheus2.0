5040901a0bb90fabba0d764fcd18c99ba6de4f5f
Igor Zubkov <igor.zubkov@gmail.com>
2010-11-19 07:46:07 +0200

    cleanup sql

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -11,22 +11,22 @@ class TeamController < ApplicationController
                       :branch_id => @branch.id },
                     :include => [:srpm]
     if @team != nil
-      @leader = Team.find_by_sql(['SELECT maintainers.login, maintainers.name
+      @leader = Team.find_by_sql(["SELECT maintainers.login, maintainers.name
                                FROM teams, maintainers, branches
                                WHERE maintainers.id = teams.maintainer_id
                                AND teams.name = ?
                                AND teams.branch_id = branches.id
                                AND branches.name = ?
-                               AND leader = true
-                               LIMIT 1', '@' + params[:name], @branch.name ])
+                               AND leader = 'true'
+                               LIMIT 1", '@' + params[:name], @branch.name ])
 
-      @members = Team.find_by_sql(['SELECT maintainers.login, maintainers.name
+      @members = Team.find_by_sql(["SELECT maintainers.login, maintainers.name
                                FROM teams, maintainers, branches
                                WHERE maintainers.id = teams.maintainer_id
                                AND teams.name = ?
                                AND teams.branch_id = branches.id
                                AND branches.name = ?
-                               ORDER BY LOWER(maintainers.name)', '@' + params[:name], @branch.name ])
+                               ORDER BY LOWER(maintainers.name)", '@' + params[:name], @branch.name ])
     else
       render :status => 404, :action => "nosuchteam"
     end
