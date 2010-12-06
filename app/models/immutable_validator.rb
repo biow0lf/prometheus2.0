e6fc3f59f3027ffdcac560bbd68c389ed8717a98
Igor Zubkov <igor.zubkov@gmail.com>
2010-12-06 23:33:20 +0200

    update user model

---
+++ app/models/immutable_validator.rb
@@ -0,0 +1,5 @@
+class ImmutableValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors[attribute] << "cannot be changed after creation" if record.send("#{attribute}_changed?") && !record.new_record?
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :email, :immutable => true
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :confirmable, :database_authenticatable, :registerable,
@@ -6,4 +8,12 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def is_alt_team?
+    if email.split('@')[1] == 'altlinux.org' || email.split('@')[1] == 'altlinux.ru'
+      true
+    else
+      false
+    end
+  end
 end
