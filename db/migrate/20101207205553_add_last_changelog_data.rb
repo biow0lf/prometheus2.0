ac5aafa4f59427fecfc04d138173eef98f164f88
Igor Zubkov <igor.zubkov@gmail.com>
2010-12-08 00:42:28 +0200

    update migration

--- db/migrate/20101207205553_add_last_changelog_data.rb
+++ db/migrate/20101207205553_add_last_changelog_data.rb
@@ -1,18 +1,18 @@
 class AddLastChangelogData < ActiveRecord::Migration
   def self.up
-    ActiveRecord::Base.transaction do
-      srpms = Srpm.scoped
-      srpms.each do |srpm|
-        changelog = srpm.changelogs.order('changelogtime DESC').first
-        
-        srpm.changelogtime = changelog.changelogtime
-        srpm.changelogname = changelog.changelogname
-        srpm.changelogtext = changelog.changelogtext        
-        unless srpm.save
-          p "shit happens!"
-        end
-      end
-    end
+#    ActiveRecord::Base.transaction do
+#      srpms = Srpm.scoped
+#      srpms.each do |srpm|
+#        changelog = srpm.changelogs.order('changelogtime DESC').first
+#        
+#        srpm.changelogtime = changelog.changelogtime
+#        srpm.changelogname = changelog.changelogname
+#        srpm.changelogtext = changelog.changelogtext        
+#        unless srpm.save
+#          p "shit happens!"
+#        end
+#      end
+#    end
   end
 
   def self.down
