25d7bd888b692da3932618cd6ad41db33b00eba3
Igor Zubkov <igor.zubkov@gmail.com>
2010-12-20 18:51:48 +0200

    Fix redirects.

--- app/controllers/redirector_controller.rb
+++ app/controllers/redirector_controller.rb
@@ -3,9 +3,9 @@ class RedirectorController < ApplicationController
     @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
     @package = @branch.packages.where(:name => params[:name]).first
     if !@package.nil?
-      redirect_to srpm_main_path(:name => @package.srpm.name, :branch => 'Sisyphus'), :status => 302
+      redirect_to srpm_path(:id => @package.srpm.name, :branch => 'Sisyphus'), :status => 302
     else
-      redirect_to srpm_main_path(:name => params[:name], :branch => 'Sisyphus'), :status => 302
+      redirect_to srpm_path(:id => params[:name], :branch => 'Sisyphus'), :status => 302
     end
   end
 end
