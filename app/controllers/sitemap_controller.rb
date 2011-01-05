9e924b2d44c1b013edb490729a7a64746343c159
Igor Zubkov <igor.zubkov@gmail.com>
2011-01-05 21:43:11 +0200

    fix sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,40 +2,37 @@ class SitemapController < ApplicationController
   layout nil
 
   def sitemap
-    headers['Content-Type'] = "application/xml"
+    headers['Content-Type'] = 'application/xml'
   end
 
   def sitemap_basic
-    headers['Content-Type'] = "application/xml"
+    headers['Content-Type'] = 'application/xml'
   end
 
   def sitemap_part1
-    headers['Content-Type'] = "application/xml"
-    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
+    headers['Content-Type'] = 'application/xml'
 
     @srpms = Srpm.all(:select => 'name',
-                      :order => 'name ASC',
+                      :order => 'srpms.name ASC',
                       :limit => 5000,
                       :conditions => { :branch_id => @branch.id })
   end
 
   def sitemap_part2
-    headers['Content-Type'] = "application/xml"
-    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
+    headers['Content-Type'] = 'application/xml'
 
     @srpms = Srpm.all(:select => 'name',
-                      :order => 'name ASC',
+                      :order => 'srpms.name ASC',
                       :limit => 5000,
                       :offset => 5000,
                       :conditions => { :branch_id => @branch.id })
   end
 
   def sitemap_part3
-    headers['Content-Type'] = "application/xml"
-    @branch = Branch.where(:name => 'Sisyphus', :vendor => 'ALT Linux').first
+    headers['Content-Type'] = 'application/xml'
 
     @srpms = Srpm.all(:select => 'name',
-                      :order => 'name ASC',
+                      :order => 'srpms.name ASC',
                       :limit => 5000,
                       :offset => 10000,
                       :conditions => { :branch_id => @branch.id })
