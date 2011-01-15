ec4420d152fc6b2caadf6e3e14e696f80b721f49
Igor Zubkov <igor.zubkov@gmail.com>
2011-01-15 18:33:51 +0200

    update branch spec

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -8,6 +8,7 @@ describe Branch do
     it { should have_many :groups }
     it { should have_many :leaders }
     it { should have_many :teams }
+    it { should have_many :mirrors }
 
     it { should validate_presence_of :name }
     it { should validate_presence_of :vendor }
