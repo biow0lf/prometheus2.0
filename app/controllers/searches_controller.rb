8d89a90b3047eac1e96f6eb8a48e640a69368528
Igor Zubkov <igor.zubkov@gmail.com>
2011-01-22 10:39:36 +0200

    improve search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,7 +9,8 @@ class SearchesController < ApplicationController
     if params[:query].nil? or params[:query].empty?
       redirect_to :action => "index"
     else
-      @search = Srpm.search(:name_or_summary_or_description_contains_all => params[:query].to_s.split).where(:branch_id => @branch.id).order("LOWER(srpms.name) ASC")
+      @exact_srpm = Srpm.where(:name => params[:query].strip, :branch_id => @branch.id).first
+      @search = Srpm.search(:name_or_summary_or_description_contains_all => params[:query].strip.to_s.split).where(:branch_id => @branch.id).order("LOWER(srpms.name) ASC")
       @srpms, @srpms_count = @search.all, @search.count
       redirect_to(srpm_path(:id => @srpms.first.name, :branch => @branch.name), :status => 302) if @search.count == 1
     end
