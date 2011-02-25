27e6263ebee930883a432f2b82ec744302d301c6
Igor Zubkov <igor.zubkov@gmail.com>
2011-02-26 00:08:08 +0200

    simplify gitrepo model

--- app/models/gitrepo.rb
+++ app/models/gitrepo.rb
@@ -14,27 +14,24 @@ class Gitrepo < ActiveRecord::Base
   def self.import_gitrepos(url)
     if Gitrepo.count(:all) == 0
       branch = Branch.first :conditions => { :name => 'Sisyphus', :vendor => 'ALT Linux' }
-      ActiveRecord::Base.transaction do
-        file = open(URI.escape(url)).read
+      file = open(URI.escape(url)).read
+      file.each_line do |line|
+        gitrepo = line.split[0]
+        login = gitrepo.split('/')[2]
+        login = 'php-coder' if login == 'php_coder'
+        login = 'p_solntsev' if login == 'psolntsev'
+        package = gitrepo.split('/')[4]
+        time = Time.at(line.split[1].to_i)
 
-        file.each_line do |line|
-          gitrepo = line.split[0]
-          login = gitrepo.split('/')[2]
-          login = 'php-coder' if login == 'php_coder'
-          login = 'p_solntsev' if login == 'psolntsev'
-          package = gitrepo.split('/')[4]
-          time = Time.at(line.split[1].to_i)
+        maintainer = Maintainer.first :conditions => { :login => login }
+        srpm = Srpm.first :conditions => { :name => package.gsub(/\.git/,''), :branch_id => branch.id }
 
-          maintainer = Maintainer.first :conditions => { :login => login }
-          srpm = Srpm.first :conditions => { :name => package.gsub(/\.git/,''), :branch_id => branch.id }
-
-          if maintainer.nil?
-            puts Time.now.to_s + ": maintainer not found '" + login + "'"
-          elsif srpm.nil?
-            puts Time.now.to_s + ": srpm not found '" + package.gsub(/\.git/,'') + "'"
-          else
-            Gitrepo.create(:repo => package.gsub(/\.git/,''), :maintainer_id => maintainer.id, :lastchange => time, :srpm_id => srpm.id)
-          end
+        if maintainer.nil?
+          puts Time.now.to_s + ": maintainer not found '" + login + "'"
+        elsif srpm.nil?
+          puts Time.now.to_s + ": srpm not found '" + package.gsub(/\.git/,'') + "'"
+        else
+          Gitrepo.create(:repo => package.gsub(/\.git/,''), :maintainer_id => maintainer.id, :lastchange => time, :srpm_id => srpm.id)
         end
       end
     else
