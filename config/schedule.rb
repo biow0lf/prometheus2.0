71e9a73d8b52c3089ad6ec4b59593a434f359083
Igor Zubkov <igor.zubkov@gmail.com>
2011-02-25 04:44:30 +0200

    update cron script

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 every 1.hour, :at => 5 do
-  rake "sisyphus:update sisyphusarm:update platform5:update:srpms platform5:update:binary 51:update:srpms 51:update:binary 50:update:srpms 50:update:binary 41:update:srpms 41:update:binary 40:update:srpms 40:update:binary"
+  rake "sisyphus:update sisyphusarm:update platform5:update 51:update:srpms 51:update:binary 50:update:srpms 50:update:binary 41:update:srpms 41:update:binary 40:update:srpms 40:update:binary"
 end
 
 every 1.day, :at => '5:00 am' do 
