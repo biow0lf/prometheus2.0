4df682ea8268878ee457ab84a6c279226789ee93
Igor Zubkov <igor.zubkov@gmail.com>
2011-02-26 00:09:42 +0200

    update cron script

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 every 1.hour, :at => 5 do
-  rake "sisyphus:update sisyphusarm:update platform5:update 51:update 50:update 41:update 40:update"
+  rake "sisyphus:update sisyphusarm:update platform5:update 51:update 50:update 41:update 40:update gitrepos:update"
 end
 
 every 1.day, :at => '5:00 am' do 
