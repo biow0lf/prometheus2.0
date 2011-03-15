c585577275d98ddfc0368ac6c64234ff1aad5d45
Igor Zubkov <igor.zubkov@gmail.com>
2011-03-15 02:32:56 +0200

    Demo implementation of patches

--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -1,13 +1,9 @@
 class PatchesController < ApplicationController
-  layout nil
-
   def index
   end
 
   def show
-    @udiff = File.read("pmount-0.9.19-alt-ext4.patch")
-    @pretty = PrettyDiff::Diff.new(@udiff)
-    # @pretty.to_html
+    @patch = File.read("pmount-0.9.19-alt-ext4.patch")
   end
 
   def get
