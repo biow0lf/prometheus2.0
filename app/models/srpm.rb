f9d9fac4d46f1b0c6772ad7aaec03ab4cdf03207
Igor Zubkov <igor.zubkov@gmail.com>
2011-03-19 16:33:52 +0200

    Cleanup

--- app/models/srpm.rb
+++ app/models/srpm.rb
@@ -23,7 +23,7 @@ class Srpm < ActiveRecord::Base
     b = Branch.where(:name => branch, :vendor => vendor).first
     b.srpms.each do |srpm|
       unless File.exists?("#{path}#{srpm.filename}")
-        puts Time.now.to_s + ": deleted '" + srpm.filename + "'"
+        puts "#{Time.now.to_s}: deleted '#{srpm.filename}'"
         srpm.destroy
       end
     end
@@ -35,7 +35,7 @@ class Srpm < ActiveRecord::Base
       begin
         rpm = RPM::Package::open(file)
         srpm = Srpm.new
-        srpm.filename = rpm.name + '-' + rpm.version.v + '-' + rpm.version.r + '.src.rpm'
+        srpm.filename = "#{rpm.name}-#{rpm.version.v}-#{rpm.version.r}.src.rpm"
         srpm.name = rpm.name
         srpm.version = rpm.version.v
         srpm.release = rpm.version.r
@@ -54,6 +54,7 @@ class Srpm < ActiveRecord::Base
         srpm.group_id = group.id
         srpm.epoch = rpm[1003]
         srpm.summary = rpm[1004]
+        # hack for very long summary in openmoko_dfu-util src.rpm
         srpm.summary = 'Broken' if rpm.name == 'openmoko_dfu-util'
         srpm.license = rpm[1014]
         srpm.url = rpm[1020]
@@ -73,16 +74,16 @@ class Srpm < ActiveRecord::Base
             changelog.changelogname = c.name
             changelog.changelogtime = c.time
             changelog.changelogtext = c.text
-            changelog.save!                                                                                                                                              
+            changelog.save!
           end
           Specfile.import_specfile(file, srpm, b)
-        end        
+        end
       rescue RuntimeError
-        puts "RuntimeError at file: " + file
+        puts "RuntimeError at file: #{file}"
       end
     end
   end
-  
+
   def self.import_srpm(vendor, branch, file)
     b = Branch.where(:name => branch, :vendor => vendor).first
     rpm = RPM::Package::open(file)
@@ -94,11 +95,11 @@ class Srpm < ActiveRecord::Base
       Srpm.destroy_all(:branch_id => b.id, :name => rpm.name)
     end
     srpm = Srpm.new
-    srpm.filename = rpm.name + '-' + rpm.version.v + '-' + rpm.version.r + '.src.rpm'
+    srpm.filename = "#{rpm.name}-#{rpm.version.v}-#{rpm.version.r}.src.rpm"
     srpm.name = rpm.name
     srpm.version = rpm.version.v
     srpm.release = rpm.version.r
-    
+
     case rpm[1016].split('/').count
     when 1
       group = b.groups.where(:name => rpm[1016], :parent_id => nil).first
@@ -107,7 +108,7 @@ class Srpm < ActiveRecord::Base
     when 3
       group = b.groups.where(:name => rpm[1016].split('/')[0], :parent_id => nil).first.children.where(:name => rpm[1016].split('/')[1]).first.children.where(:name => rpm[1016].split('/')[2]).first
     else
-      puts Time.now.to_s + ": too nested groups level"
+      puts "#{Time.now.to_s}: too nested groups level"
     end
 
     srpm.group_id = group.id
