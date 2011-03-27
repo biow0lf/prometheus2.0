f7aabed610d1d0c613b5cce3b636a04363d9ec6d
Igor Zubkov <igor.zubkov@gmail.com>
2011-03-28 02:52:40 +0300

    fix misc#bugs page

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,7 +1,7 @@
 class MiscController < ApplicationController
   def bugs
-    @bug_statuses = Bug.where("").select("DISTINCT bug_status").order("bugs.bug_status ASC").all
-    @resolutions = Bug.where("").select("DISTINCT resolution").order("bugs.resolution ASC").all
-    @bugs = Bug.where("").select("bug_status","resolution")
+    @bug_statuses = Bug.unscoped.select("DISTINCT bug_status").order("bugs.bug_status ASC").all
+    @resolutions = Bug.unscoped.select("DISTINCT resolution").order("bugs.resolution ASC").all
+    @bugs = Bug.unscoped.select("bug_status, resolution")
   end
 end
