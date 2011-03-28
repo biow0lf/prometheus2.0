810c8c4197f1992f6633880af5eb060e8f0c4b11
Igor Zubkov <igor.zubkov@gmail.com>
2011-03-28 03:24:20 +0300

    Cleanup maintainers controller

--- app/controllers/maintainers_controller.rb
+++ app/controllers/maintainers_controller.rb
@@ -1,15 +1,15 @@
 class MaintainersController < ApplicationController
   def show
-    @branch = Branch.where(:name => params[:branch], :vendor => "ALT Linux").first
+    @branch = Branch.where(:name => params[:branch], :vendor => 'ALT Linux').first
     @maintainer = Maintainer.where(:login => params[:id].downcase, :team => false).first
-    render(:status => 404, :action => "nosuchmaintainer") and return if @maintainer == nil
+    render(:status => 404, :action => 'nosuchmaintainer') and return if @maintainer == nil
     @acls = Acl.where(:maintainer_id => @maintainer.id, :branch_id => @branch.id)
   end
 
   def srpms
-    @branch = Branch.where(:name => params[:branch], :vendor => "ALT Linux").first
+    @branch = Branch.where(:name => params[:branch], :vendor => 'ALT Linux').first
     @maintainer = Maintainer.where(:login => params[:id].downcase, :team => false).first
-    render(:status => 404, :action => "nosuchmaintainer") and return if @maintainer == nil
+    render(:status => 404, :action => 'nosuchmaintainer') and return if @maintainer == nil
     @acls = Acl.where(:maintainer_id => @maintainer.id, :branch_id => @branch.id).includes(:srpm => [:repocop_patch]).order('LOWER(srpms.name)')
   end
 
@@ -27,52 +27,35 @@ class MaintainersController < ApplicationController
 #  end
 
   def gear
-    @branch = Branch.where(:name => params[:branch], :vendor => "ALT Linux").first
     @maintainer = Maintainer.where(:login => params[:id].downcase, :team => false).first
-    render(:status => 404, :action => "nosuchmaintainer") and return if @maintainer == nil    
-    @gitrepos = Gitrepo.all :conditions => {
-                              :maintainer_id => @maintainer.id },
-                             :order => 'LOWER(repo)'
+    render(:status => 404, :action => 'nosuchmaintainer') and return if @maintainer == nil
+    @gitrepos = Gitrepo.where(:maintainer_id => @maintainer.id).order('LOWER(repo)')
   end
 
   def bugs
     @maintainer = Maintainer.where(:login => params[:id].downcase, :team => false).first
-    render(:status => 404, :action => "nosuchmaintainer") and return if @maintainer == nil    
-    @bugs = Bug.all :conditions => {
-                      :assigned_to => params[:id].downcase + '@altlinux.org',
+    render(:status => 404, :action => 'nosuchmaintainer') and return if @maintainer == nil
+    @bugs = Bug.where(:assigned_to => params[:id].downcase + '@altlinux.org',
                       :product => 'Sisyphus',
-                      :bug_status => ["NEW", "ASSIGNED", "VERIFIED", "REOPENED"]},
-                    :order => "bug_id DESC"
-    @allbugs = Bug.all :conditions => {
-                         :assigned_to => params[:id].downcase + '@altlinux.org',
-                         :product => 'Sisyphus' },
-                       :order => "bug_id DESC"
+                      :bug_status => ['NEW', 'ASSIGNED', 'VERIFIED', 'REOPENED']).order('bug_id DESC')
+    @allbugs = Bug.where(:assigned_to => params[:id].downcase + '@altlinux.org',
+                         :product => 'Sisyphus').order('bug_id DESC')
   end
 
   def allbugs
     @maintainer = Maintainer.where(:login => params[:id].downcase, :team => false).first
-    render(:status => 404, :action => "nosuchmaintainer") and return if @maintainer == nil    
-    @bugs = Bug.all :conditions => {
-                      :assigned_to => params[:id].downcase + '@altlinux.org',
+    render(:status => 404, :action => 'nosuchmaintainer') and return if @maintainer == nil
+    @bugs = Bug.where(:assigned_to => params[:id].downcase + '@altlinux.org',
                       :product => 'Sisyphus',
-                      :bug_status => ["NEW", "ASSIGNED", "VERIFIED", "REOPENED"]},
-                    :order => "bug_id DESC"
-    @allbugs = Bug.all :conditions => {
-                         :assigned_to => params[:id].downcase + '@altlinux.org',
-                         :product => 'Sisyphus' },
-                       :order => "bug_id DESC"
+                      :bug_status => ['NEW', 'ASSIGNED', 'VERIFIED', 'REOPENED']).order('bug_id DESC')
+    @allbugs = Bug.where(:assigned_to => params[:id].downcase + '@altlinux.org',
+                         :product => 'Sisyphus').order('bug_id DESC')
   end
 
   def repocop
     @branch = Branch.where(:vendor => 'ALT Linux', :name => 'Sisyphus').first
-    @maintainer = Maintainer.first :conditions => {
-                                     :login => params[:id].downcase,
-                                     :team => false },
-                                   :include => [:srpms],
-                                   :order => 'LOWER(srpms.name)'
-    @srpms = @maintainer.srpms.find(:all, :conditions => { :branch_id => @branch.id }, :include => [:repocops])
-    if @maintainer == nil
-      render :status => 404, :action => "nosuchmaintainer"
-    end
+    @maintainer = Maintainer.where(:login => params[:id].downcase, :team => false).includes(:srpms).order('LOWER(srpms.name)').first
+    render(:status => 404, :action => 'nosuchmaintainer') and return if @maintainer == nil
+    @srpms = @maintainer.srpms.where(:branch_id => @branch.id).includes(:repocops)
   end
 end
