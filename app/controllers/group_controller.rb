861582d341127fc91d4c83d2b159b65f60d83601
Igor Zubkov <igor.zubkov@gmail.com>
2011-03-28 03:34:38 +0300

    Cleanup group controller

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -6,14 +6,14 @@ class GroupController < ApplicationController
 
   def bygroup
     @branch = Branch.where(:name => params[:branch], :vendor => 'ALT Linux').first
-    @group = @branch.groups.find(:first, :conditions => { :name => params[:group], :parent_id => nil })
+    @group = @branch.groups.where(:name => params[:group], :parent_id => nil).first
     if !params[:group2].nil?
-      @group = @branch.groups.find(:first, :conditions => { :name => params[:group2], :parent_id => @group.id })
+      @group = @branch.groups.where(:name => params[:group2], :parent_id => @group.id).first
       if !params[:group3].nil?
-        @group = @branch.groups.find(:first, :conditions => { :name => params[:group3], :parent_id => @group.id })
+        @group = @branch.groups.where(:name => params[:group3], :parent_id => @group.id).first
       end
     end
-    render :status => 404, :action => "nosuchgroup" and return if @group == nil
+    render :status => 404, :action => 'nosuchgroup' and return if @group == nil
     @srpms = @group.srpms.find(:all, :order => 'LOWER(name)')
   end
 end
