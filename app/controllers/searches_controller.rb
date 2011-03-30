e30031e464f0cc8dbfe264c5889a82679b27ba73
Igor Zubkov <igor.zubkov@gmail.com>
2011-03-30 03:12:31 +0300

    Cleanup

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,10 +7,10 @@ class SearchesController < ApplicationController
     end
     @branches = Branch.all
     if params[:query].nil? or params[:query].empty?
-      redirect_to :action => "index"
+      redirect_to :action => 'index'
     else
-      @exact_srpm = Srpm.where(:name => params[:query].strip, :branch_id => @branch.id).first
-      @search = Srpm.search(:name_or_summary_or_description_contains_all => params[:query].strip.to_s.split).where(:branch_id => @branch.id).order("LOWER(srpms.name) ASC")
+      @exact_srpm = Srpm.where(:name => params[:query].strip, :branch => @branch).first
+      @search = Srpm.search(:name_or_summary_or_description_contains_all => params[:query].strip.to_s.split).where(:branch => @branch).order("LOWER(srpms.name) ASC")
       @srpms, @srpms_count = @search.all, @search.count
       redirect_to(srpm_path(@branch, @srpms.first), :status => 302) if @search.count == 1
     end
