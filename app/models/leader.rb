a52d35b3e11d65a6b4d7b98307a3c8a906e67203
Igor Zubkov <igor.zubkov@gmail.com>
2011-05-23 00:56:15 +0300

    Cleanup

--- app/models/leader.rb
+++ app/models/leader.rb
@@ -7,9 +7,9 @@ class Leader < ActiveRecord::Base
   validates :srpm, :presence => true
   validates :maintainer, :presence => true
 
-  def self.import_leaders(vendor, branch, url)
-    br = Branch.first :conditions => { :name => branch, :vendor => vendor }
-    if br.leaders.count(:all) == 0
+  def self.import_leaders(vendor_name, branch_name, url)
+    branch = Branch.where(:name => branch_name, :vendor => vendor_name).first
+    if branch.leaders.count(:all) == 0
       ActiveRecord::Base.transaction do
         file = open(URI.escape(url)).read
         file.each_line do |line|
@@ -19,40 +19,40 @@ class Leader < ActiveRecord::Base
           login = 'php-coder' if login == 'php_coder'
           login = 'p_solntsev' if login == 'psolntsev'
           login = '@vim-plugins' if login == '@vim_plugins'
-          maintainer = Maintainer.first :conditions => { :login => login }
+          maintainer = Maintainer.where(:login => login).first
           if maintainer.nil?
-            puts Time.now.to_s + ": maintainer not found '" + login + "'"
+            puts "#{Time.now.to_s}: maintainer not found '#{login}'"
           elsif srpm.nil?
-            puts Time.now.to_s + ": srpm not found '" + package + "'"
+            puts "#{Time.now.to_s}: srpm not found '#{package}'"
           else
-            Leader.create :srpm_id => srpm.id, :branch_id => br.id, :maintainer_id => maintainer.id
+            Leader.create!(:srpm => srpm, :branch => branch, :maintainer => maintainer)
           end
         end
       end
     else
-      puts Time.now.to_s + ": leaders already imported"
+      puts "#{Time.now.to_s}: leaders already imported"
     end
   end
-  
-  def self.create_leader_for_package(vendor, branch, url, package)
-    br = Branch.first :conditions => { :name => branch, :vendor => vendor }
-    if br.srpms.where(:name => package).first.leader != nil
-      br.srpms.where(:name => package).first.leader.delete
+
+  def self.create_leader_for_package(vendor_name, branch_name, url, package)
+    branch = Branch.where(:name => branch_name, :vendor => vendor_name).first
+    if branch.srpms.where(:name => package).first.leader != nil
+      branch.srpms.where(:name => package).first.leader.delete
     end
     file = open(URI.escape(url)).read
     file.each_line do |line|
       packagename = line.split[0]
       if packagename == package
-        srpm = Srpm.first :conditions => { :name => packagename, :branch_id => br.id }
+        srpm = Srpm.where(:name => packagename, :branch => branch).first
         login = line.split[1]
         login = 'php-coder' if login == 'php_coder'
         login = 'p_solntsev' if login == 'psolntsev'
         login = '@vim-plugins' if login == '@vim_plugins'
-        maintainer = Maintainer.first :conditions => { :login => login }
+        maintainer = Maintainer.where(:login => login).first
         if maintainer.nil?
-          puts Time.now.to_s + ": maintainer not found '" + login + "'"
+          puts "#{Time.now.to_s}: maintainer not found '#{login}'"
         else
-          Leader.create :srpm_id => srpm.id, :branch_id => br.id, :maintainer_id => maintainer.id
+          Leader.create!(:srpm => srpm, :branch => branch, :maintainer => maintainer)
         end
       end
     end
