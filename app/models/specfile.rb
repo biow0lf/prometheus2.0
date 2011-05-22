3cf8e6a1a864ea8e8f6c7050cbcf3e583326448a
Igor Zubkov <igor.zubkov@gmail.com>
2011-05-22 20:53:39 +0300

    force encoding for specfile

--- app/models/specfile.rb
+++ app/models/specfile.rb
@@ -2,15 +2,15 @@ class Specfile < ActiveRecord::Base
   validates :branch, :presence => true
   validates :srpm, :presence => true
   validates :spec, :presence => true
-  
+
   belongs_to :srpm
   belongs_to :branch
-  
+
   def self.import_specfile(file, srpm, branch)
     specfilename = `rpm -qp --queryformat=\"[%{FILEFLAGS} %{FILENAMES}\n]\" "#{file}" | grep \"32 \" | sed -e 's/32 //'`
     specfilename.strip!
-    spec = `rpm2cpio "#{file}" | cpio -i --to-stdout "#{specfilename}"`
-    
+    spec = `rpm2cpio "#{file}" | cpio -i --to-stdout "#{specfilename}"`.force_encoding("BINARY")
+
     specfile = Specfile.new
     specfile.srpm_id = srpm.id
     specfile.branch_id = branch.id
@@ -18,10 +18,10 @@ class Specfile < ActiveRecord::Base
     unless specfile.save
       puts "shit happens"
     end
-    
+
     srpm.specfile_id = specfile.id
     unless srpm.save
       puts "shit happens"
-    end    
+    end
   end
 end
