ade5dcba0cccf664e0e4d9a66f5c362d3f643521
Igor Zubkov <igor.zubkov@gmail.com>
2011-05-23 00:48:42 +0300

    Cleanup

--- app/models/group.rb
+++ app/models/group.rb
@@ -32,23 +32,23 @@ class Group < ActiveRecord::Base
         param1 = line.split('/')[1]
         param2 = line.split('/')[2]
 
-        if !Group.first(:conditions => { :name => param0, :branch => branch, :parent_id => nil })
-          group0 = Group.create(:name => param0, :branch => branch)
+        if !Group.where(:name => param0, :branch => branch, :parent_id => nil).first
+          group0 = Group.create!(:name => param0, :branch => branch)
         else
-          group0 = Group.first(:conditions => { :name => param0, :branch => branch, :parent_id => nil })
+          group0 = Group.where(:name => param0, :branch => branch, :parent_id => nil).first
         end
 
         if param1 != nil
-          if !Group.first(:conditions => { :name => param1, :branch => branch, :parent_id => group0.id })
-            group1 = Group.create(:name => param1, :branch => branch)
+          if !Group.where(:name => param1, :branch => branch, :parent_id => group0.id).first
+            group1 = Group.create!(:name => param1, :branch => branch)
             group1.move_to_child_of(group0)
           else
-            group1 = Group.first(:conditions => { :name => param1, :branch => branch, :parent_id => group0.id })
+            group1 = Group.where(:name => param1, :branch => branch, :parent_id => group0.id).first
           end
         end
 
         if param2 != nil
-          group2 = Group.create(:name => param2, :branch => branch)
+          group2 = Group.create!(:name => param2, :branch => branch)
           group2.move_to_child_of(group1)
         end
       end
