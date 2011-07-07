72b5538a7f7ae9309d6a380e2c6060b6f1667b00
Igor Zubkov <igor.zubkov@gmail.com>
2011-07-07 16:50:47 +0300

    Index Srpm.filename, Package.filename and Package.sourcepackage

--- app/models/srpm.rb
+++ app/models/srpm.rb
@@ -19,9 +19,12 @@ class Srpm < ActiveRecord::Base
     indexes name, :sortable => true
     indexes summary
     indexes description
+    indexes filename
     indexes packages.name, :as => :packages_name, :sortable => true
     indexes packages.summary, :as => :packages_summary
     indexes packages.description, :as => :packages_description
+    indexes packages.filename, :as => :packages_filename
+    indexes packages.sourcepackage, :as => :packages_sourcepackage
 
     has branch_id
   end
