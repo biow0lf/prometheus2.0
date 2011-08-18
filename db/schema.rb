a50ef0cf75339b6cf87d42bab0355543c552d6cf
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-19 01:03:22 +0300

    Rollback changes from user model

--- db/schema.rb
+++ db/schema.rb
@@ -325,7 +325,7 @@ ActiveRecord::Schema.define(:version => 20110818200148) do
     t.string   "email",                               :default => "", :null => false
     t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
     t.string   "reset_password_token"
-    t.string   "remember_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
