c1c47b7faacc4f04adda62008e76c69c90bce3ef
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-24 16:21:47 +0300

    Fix javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,7 @@ Prometheus20::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( local_en.js local_ru.js local_uk.js local_br.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
