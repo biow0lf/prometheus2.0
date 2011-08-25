a6569dedcc6f8a4ad430ff89b04c72ab25b23bfb
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-25 21:53:46 +0300

    Link sphinx db

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,8 @@ namespace :deploy do
     run "ln -nfs #{deploy_to}/shared/config/redis.yml #{release_path}/config/redis.yml"
     run "cp -f #{deploy_to}/shared/config/initializers/devise.rb #{release_path}/config/initializers/"
     run "cp -f #{deploy_to}/shared/config/initializers/secret_token.rb #{release_path}/config/initializers/"
+    # sphinx
+    run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
     # precompile the assets
     run "cd #{release_path} && bundle exec rake assets:precompile"
   end
