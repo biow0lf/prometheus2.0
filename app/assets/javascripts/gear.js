b40807376220d1da1259a7ab2146fd0505a45675
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-29 02:15:22 +0300

    Javascript

--- app/assets/javascripts/gear.js
+++ app/assets/javascripts/gear.js
@@ -1,5 +1,14 @@
-$(document).ready(function()
-  {
-    $(".gear").tablesorter();
-  }
-);
+$(document).ready(function() {
+  $('.gear').tablesorter();
+});
+
+// $('#sort_by_name').click(function() {
+//    if ($("#by_name").attr("src") == "<%= asset_path 'stock_navigate-prev.png' %>") {
+//      $('#by_name').attr("src", "<%= asset_path 'stock_navigate-next.png' %>");
+//    } else {
+//      $('#by_name').attr("src", "<%= asset_path 'stock_navigate-prev.png' %>");
+//    }
+//    var sorting = [[0,0]];
+//    $(".gear").trigger("sorton", [sorting]);
+//    return false;
+// });
