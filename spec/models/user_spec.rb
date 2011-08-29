90e725144d9f7983bd7474b7c089c8029d3e9476
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-29 04:01:53 +0300

    Add specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should return login on User.login" do
+    user = User.create!(:email => 'icesik@altlinux.org',
+                        :password => 'password',
+                        :password_confirmation => 'password')
+    user.confirmed_at = Time.zone.now
+    user.save!
+    user.login.should == 'icesik'
+  end
+
+  it "should return true for altlinux team member" do
+    user = User.create!(:email => 'icesik@altlinux.org',
+                        :password => 'password',
+                        :password_confirmation => 'password')
+    user.confirmed_at = Time.zone.now
+    user.save!
+    user.is_alt_team?.should be_true
+  end
+
+  it "should return false for non altlinux team member" do
+    user = User.create!(:email => 'user@email.com',
+                        :password => 'password',
+                        :password_confirmation => 'password')
+    user.confirmed_at = Time.zone.now
+    user.save!
+    user.is_alt_team?.should be_false
+  end
+
+  it "should deny change email" do
+    user = User.create!(:email => 'user@email.com',
+                        :password => 'password',
+                        :password_confirmation => 'password')
+    user.confirmed_at = Time.zone.now
+    user.save!
+    user.email = 'icesik@altlinux.org'
+    user.save.should be_false
+  end
 end
