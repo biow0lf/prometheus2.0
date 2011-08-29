5fde8996c8f491cfa683a9a49b7b52982472f7d5
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-29 03:38:37 +0300

    Reformat

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Branch do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :vendor }
+
   it { should have_many :acls }
   it { should have_many :srpms }
   it { should have_many :packages }
@@ -10,9 +13,6 @@ describe Branch do
   it { should have_many :mirrors }
   it { should have_many :patches }
   it { should have_many :ftbfs }
-
-  it { should validate_presence_of :name }
-  it { should validate_presence_of :vendor }
 
   it "should return Branch.name on .to_param" do
     Branch.create!(:name => 'Sisyphus', :vendor => 'ALT Linux').to_param.should == 'Sisyphus'
