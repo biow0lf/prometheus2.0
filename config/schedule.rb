b93cd904196d49937c0027d4622b6177089fd6e3
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-30 16:59:15 +0300

    Fix time

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,27 +4,27 @@ job_type :rake, "cd :path && RAILS_ENV=:environment bundle exec rake :task :outp
 #   rake "sisyphus:update platform6:update t6:update platform5:update 51:update 50:update 41:update 40:update gear:update ts:reindex"
 # end
 # 
-# every 1.day, :at => '5:30 am' do
+# every 1.day, :at => '5:30' do
 #   rake "sisyphusarm:update ts:reindex"
 # end
 
-every 1.day, :at => '5:00 am' do
+every 1.day, :at => '5:00' do
   rake "sisyphus:bugs"
 end
 
-# every 1.day, :at => '6:15 am' do
+# every 1.day, :at => '6:15' do
 #   rake "sisyphus:repocops sisyphus:repocop_patches"
 # end
 
-every 1.day, :at => '01:00 pm' do
+every 1.day, :at => '13:00' do
   rake "ftbfs:update"
 end
 
-# every :sunday, :at => '3:30 am' do
+# every :sunday, :at => '3:30' do
 #   rake "sitemap:refresh"
 # end
 
-every :sunday, :at => '6:30 am' do
+every :sunday, :at => '6:30' do
   rake "perlwatch:update"
 end
 
