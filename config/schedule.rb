703ae0ea25d8dc4691ae134f47ad2b79c4891bb5
Igor Zubkov <igor.zubkov@gmail.com>
2011-08-30 16:23:00 +0300

    Disable some cron tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,26 +1,26 @@
-every 1.hour, :at => 5 do
-  rake "sisyphus:update platform6:update t6:update platform5:update 51:update 50:update 41:update 40:update gear:update ts:reindex"
-end
-
-every 1.day, :at => '5:30 am' do
-  rake "sisyphusarm:update ts:reindex"
-end
+# every 1.hour, :at => 5 do
+#   rake "sisyphus:update platform6:update t6:update platform5:update 51:update 50:update 41:update 40:update gear:update ts:reindex"
+# end
+# 
+# every 1.day, :at => '5:30 am' do
+#   rake "sisyphusarm:update ts:reindex"
+# end
 
 every 1.day, :at => '5:00 am' do
   rake "sisyphus:bugs"
 end
 
-every 1.day, :at => '6:15 am' do
-  rake "sisyphus:repocops sisyphus:repocop_patches"
-end
+# every 1.day, :at => '6:15 am' do
+#   rake "sisyphus:repocops sisyphus:repocop_patches"
+# end
 
 every 1.day, :at => '01:00 pm' do
   rake "ftbfs:update"
 end
 
-every :sunday, :at => '3:30 am' do
-  rake "sitemap:refresh"
-end
+# every :sunday, :at => '3:30 am' do
+#   rake "sitemap:refresh"
+# end
 
 every :sunday, :at => '6:30 am' do
   rake "perlwatch:update"
