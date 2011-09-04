95a464b15b4cbbb3b5d985a93d2922a67b15e9e5
Igor Zubkov <igor.zubkov@gmail.com>
2011-09-04 05:32:40 +0300

    Refactoring Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,6 +10,16 @@ class Group < ActiveRecord::Base
   has_many :srpms
   has_many :packages
 
+  def full_name
+    full = self.name
+    parent = self.parent
+    while parent
+      full = "#{parent.name}/#{full}"
+      parent = parent.parent
+    end
+    full
+  end
+
   def self.find_groups_in_sisyphus
     find_by_sql("SELECT COUNT(srpms.name) AS counter, groups.name
                  FROM srpms, groups, branches
@@ -20,6 +30,17 @@ class Group < ActiveRecord::Base
                  AND srpms.branch_id = branches.id
                  GROUP BY groups.name
                  ORDER BY groups.name")
+  end
+
+  def self.import_group(branch, group_string)
+    prev_id = nil
+    group_string.split('/').each_with_index  do |item, index|
+      group = Group.where(:branch_id => branch.id, :parent_id => prev_id, :name => item).first
+      unless group
+        group = Group.create(:branch_id => branch.id, :name => item, :parent_id => prev_id)
+      end
+      prev_id = group.id
+    end
   end
 
   def self.import_groups(vendor_name, branch_name, url)
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -9,4 +9,46 @@ describe Group do
   it { should validate_presence_of :name }
 
   it { should have_db_index :branch_id }
+
+  it "should return full group name on #full_name" do
+    branch = Branch.create!(:name => 'Sisyphus', :vendor => 'ALT Linux')
+    Group.import_group(branch, 'System/Configuration/Boot and Init')
+    Group.all.last.full_name.should == 'System/Configuration/Boot and Init'
+  end
+
+  it "should import group 'Shells'" do
+    branch = Branch.create!(:name => 'Sisyphus', :vendor => 'ALT Linux')
+    Group.import_group(branch, 'Shells')
+    Group.all.count.should == 1
+    Group.all.first.branch_id.should == branch.id
+    Group.all.first.name.should == 'Shells'
+    Group.all.first.parent_id.should be_nil
+  end
+
+  it "should import group 'Archiving/Backup'" do
+    branch = Branch.create!(:name => 'Sisyphus', :vendor => 'ALT Linux')
+    Group.import_group(branch, 'Archiving/Backup')
+    Group.all.count.should == 2
+    Group.all.first.branch_id.should == branch.id
+    Group.all.first.name.should == 'Archiving'
+    Group.all.first.parent_id.should be_nil
+    Group.all.second.branch_id.should == branch.id
+    Group.all.second.name.should == 'Backup'
+    Group.all.second.parent_id.should == Group.all.first.id
+  end
+
+  it "should import group 'System/Configuration/Boot and Init'" do
+    branch = Branch.create!(:name => 'Sisyphus', :vendor => 'ALT Linux')
+    Group.import_group(branch, 'System/Configuration/Boot and Init')
+    Group.all.count.should == 3
+    Group.all.first.branch_id.should == branch.id
+    Group.all.first.name.should == 'System'
+    Group.all.first.parent_id.should be_nil
+    Group.all.second.branch_id.should == branch.id
+    Group.all.second.name.should == 'Configuration'
+    Group.all.second.parent_id.should == Group.all.first.id
+    Group.all.third.branch_id.should == branch.id
+    Group.all.third.name.should == 'Boot and Init'
+    Group.all.third.parent_id.should == Group.all.second.id
+  end
 end
