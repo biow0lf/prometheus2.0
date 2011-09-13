cdebe77782b931d9b9eedf4a644691f331a96550
Igor Zubkov <igor.zubkov@gmail.com>
2011-09-13 04:11:59 +0300

    Changelog#import

--- app/models/changelog.rb
+++ app/models/changelog.rb
@@ -5,4 +5,27 @@ class Changelog < ActiveRecord::Base
   validates :changelogtime, :presence => true
   validates :changelogname, :presence => true
   validates :changelogtext, :presence => true
+
+  def self.import(branch, file, srpm)
+    changelogs = `rpm -qp --queryformat='[%{CHANGELOGTIME}\n**********\n%{CHANGELOGNAME}\n**********\n%{CHANGELOGTEXT}\n**********\n]' #{file}`
+    index = 0
+    counter = 0
+    changelog = Changelog.new
+    changelogs.split("\n**********\n").each do |item|
+      counter += 1
+      if index == 0
+        changelog = Changelog.new
+        changelog.srpm_id = srpm.id
+        changelog.changelogtime = item
+        index = 1
+      elsif index == 1
+        changelog.changelogname = item
+        index = 2
+      elsif index == 2
+        changelog.changelogtext = item
+        changelog.save
+        index = 0
+      end
+    end
+  end
 end
--- spec/models/changelog_spec.rb
+++ spec/models/changelog_spec.rb
@@ -9,4 +9,17 @@ describe Changelog do
   it { should validate_presence_of :changelogtext }
 
   it { should have_db_index :srpm_id }
+
+  it "should import changelogs" do
+    branch = FactoryGirl.create(:branch)
+    group = FactoryGirl.create(:group, :branch_id => branch.id)
+    srpm = FactoryGirl.create(:srpm, :branch_id => branch.id, :group_id => group.id)
+
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    Changelog.should_receive(:`).with("rpm -qp --queryformat='[%{CHANGELOGTIME}\n**********\n%{CHANGELOGNAME}\n**********\n%{CHANGELOGTEXT}\n**********\n]' #{file}").and_return("1312545600\n**********\nMykola Grechukh <gns@altlinux.ru> 3.5.0-alt1\n**********\n3.4.11.1 -> 3.5.0\n**********\n1312545600\n**********\nMykola Grechukh <gns@altlinux.ru> 3.5.0-alt1\n**********\n3.4.11.1 -> 3.5.0\n**********\n")
+
+    expect{
+      Changelog.import(branch, file, srpm)
+    }.to change{ Changelog.count }.from(0).to(2)
+  end
 end
