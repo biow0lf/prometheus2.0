cd11e16d83acb77d4587def83641de9d1c686853
Igor Zubkov <igor.zubkov@gmail.com>
2011-11-14 23:09:56 +0200

    Cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :branch,          'origin/master'
 set :migrate_target,  :current
 set :ssh_options,     { :forward_agent => true }
 set :rails_env,       'production'
-set :deploy_to,       "/home/prometheusapp/www"
+set :deploy_to,       '/home/prometheusapp/www'
 set :normalize_asset_timestamps, false
 
 set :user,            'prometheusapp'
