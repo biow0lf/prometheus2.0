f378739f9ea18b4a844a1e5d692a5398f731ee53
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-03 09:41:43 +0200

    Fix rpm -K --nogpg check

--- app/models/rpm.rb
+++ app/models/rpm.rb
@@ -2,7 +2,7 @@
 
 class Rpm
   def self.check_md5(file)
-    output = `rpm -K --nogpg #{file}`
+    output = `export LANG=C && rpm -K --nogpg #{file}`
     if !output.empty? && output.chop.split(': ').last == 'md5 OK'
       true
     else
--- spec/models/rpm_spec.rb
+++ spec/models/rpm_spec.rb
@@ -5,13 +5,13 @@ require 'spec_helper'
 describe RPM do
   it "should verify md5 sum of srpm before import and return true for good srpm" do
     file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
-    RPM.should_receive(:`).with("rpm -K --nogpg #{file}").and_return("openbox-3.5.0-alt1.src.rpm: md5 OK\n")
+    RPM.should_receive(:`).with("export LANG=C && rpm -K --nogpg #{file}").and_return("openbox-3.5.0-alt1.src.rpm: md5 OK\n")
     RPM.check_md5(file).should be_true
   end
 
   it "should verify md5 sum of srpm before import and return false for broken srpm" do
     file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
-    RPM.should_receive(:`).with("rpm -K --nogpg #{file}").and_return("")
+    RPM.should_receive(:`).with("export LANG=C && rpm -K --nogpg #{file}").and_return("")
     RPM.check_md5(file).should be_false
   end
 end
