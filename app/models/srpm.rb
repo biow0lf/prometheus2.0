33a5b121cacb9e5d180176f10d642ae860c66b2b
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-04 02:07:38 +0200

    LANG=C before call rpm

--- app/models/srpm.rb
+++ app/models/srpm.rb
@@ -48,35 +48,35 @@ class Srpm < ActiveRecord::Base
 
   def self.import(branch, file)
     srpm = Srpm.new
-    srpm.name = `rpm -qp --queryformat='%{NAME}' #{file}`
-    srpm.version = `rpm -qp --queryformat='%{VERSION}' #{file}`
-    srpm.release = `rpm -qp --queryformat='%{RELEASE}' #{file}`
-    srpm.epoch = `rpm -qp --queryformat='%{EPOCH}' #{file}`
+    srpm.name = `export LANG=C && rpm -qp --queryformat='%{NAME}' #{file}`
+    srpm.version = `export LANG=C && rpm -qp --queryformat='%{VERSION}' #{file}`
+    srpm.release = `export LANG=C && rpm -qp --queryformat='%{RELEASE}' #{file}`
+    srpm.epoch = `export LANG=C && rpm -qp --queryformat='%{EPOCH}' #{file}`
     # TODO: make test for this
     srpm.epoch = nil if srpm.epoch == '(none)'
     srpm.filename = "#{srpm.name}-#{srpm.version}-#{srpm.release}.src.rpm"
 
-    group_name = `rpm -qp --queryformat='%{GROUP}' #{file}`
+    group_name = `export LANG=C && rpm -qp --queryformat='%{GROUP}' #{file}`
     Group.import(branch, group_name)
     group = Group.in_branch(branch, group_name)
 
     srpm.group_id = group.id
-    srpm.summary = `rpm -qp --queryformat='%{SUMMARY}' #{file}`
+    srpm.summary = `export LANG=C && rpm -qp --queryformat='%{SUMMARY}' #{file}`
     # TODO: test for this
     # hack for very long summary in openmoko_dfu-util src.rpm
     srpm.summary = 'Broken' if srpm.name == 'openmoko_dfu-util'
-    srpm.license = `rpm -qp --queryformat='%{LICENSE}' #{file}`
-    srpm.url = `rpm -qp --queryformat='%{URL}' #{file}`
-    srpm.description = `rpm -qp --queryformat='%{DESCRIPTION}' #{file}`
-    srpm.vendor = `rpm -qp --queryformat='%{VENDOR}' #{file}`
-    srpm.distribution = `rpm -qp --queryformat='%{DISTRIBUTION}' #{file}`
-    srpm.buildtime = Time.at(`rpm -qp --queryformat='%{BUILDTIME}' #{file}`.to_i)
+    srpm.license = `export LANG=C && rpm -qp --queryformat='%{LICENSE}' #{file}`
+    srpm.url = `export LANG=C && rpm -qp --queryformat='%{URL}' #{file}`
+    srpm.description = `export LANG=C && rpm -qp --queryformat='%{DESCRIPTION}' #{file}`
+    srpm.vendor = `export LANG=C && rpm -qp --queryformat='%{VENDOR}' #{file}`
+    srpm.distribution = `export LANG=C && rpm -qp --queryformat='%{DISTRIBUTION}' #{file}`
+    srpm.buildtime = Time.at(`export LANG=C && rpm -qp --queryformat='%{BUILDTIME}' #{file}`.to_i)
     srpm.size = File.size(file)
     srpm.md5 = `/usr/bin/md5sum #{file}`.split[0]
     srpm.branch_id = branch.id
-    srpm.changelogtime = Time.at(`rpm -qp --queryformat='%{CHANGELOGTIME}' #{file}`.to_i)
-    srpm.changelogname = `rpm -qp --queryformat='%{CHANGELOGNAME}' #{file}`
-    srpm.changelogtext = `rpm -qp --queryformat='%{CHANGELOGTEXT}' #{file}`
+    srpm.changelogtime = Time.at(`export LANG=C && rpm -qp --queryformat='%{CHANGELOGTIME}' #{file}`.to_i)
+    srpm.changelogname = `export LANG=C && rpm -qp --queryformat='%{CHANGELOGNAME}' #{file}`
+    srpm.changelogtext = `export LANG=C && rpm -qp --queryformat='%{CHANGELOGTEXT}' #{file}`
     if srpm.save
       $redis.set("#{branch.name}:#{srpm.filename}", 1)
       # FIXME:
