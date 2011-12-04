95edfef9a611bf1d7bae4fdd51c7c0b8e1249149
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-04 09:26:55 +0200

    Skip import srpm file if file not exists

--- app/models/srpm.rb
+++ app/models/srpm.rb
@@ -92,6 +92,7 @@ class Srpm < ActiveRecord::Base
   def self.import_all(branch, path)
     Dir.glob(path).each do |file|
       unless $redis.exists("#{branch.name}:#{file.split('/')[-1]}")
+        next unless File.exist?(file)
         next unless Rpm.check_md5(file)
         puts "#{Time.now.to_s}: import '#{file.split('/')[-1]}'"
         Srpm.import(branch, file)
