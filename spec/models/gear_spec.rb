0a02666d71c8366da6f53dcc637a7c718e5978c8
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-04 23:48:34 +0200

    Add spec for Gear.update_gitrepos

--- spec/models/gear_spec.rb
+++ spec/models/gear_spec.rb
@@ -11,4 +11,40 @@ describe Gear do
 
   it { should have_db_index :maintainer_id }
   it { should have_db_index :srpm_id }
+
+  it "should import gitrepos" do
+    branch = FactoryGirl.create(:branch)
+
+    group0 = Group.create!(name: 'System', branch_id: branch.id)
+    group = Group.create!(name: 'Servers', branch_id: branch.id)
+    group.move_to_child_of(group0)
+
+    Maintainer.create(name: 'Igor Zubkov',
+                      email: 'icesik@altlinux.org',
+                      login: 'icesik',
+                      team: false)
+
+    Srpm.create(branch_id: branch.id,
+                name: 'pulseaudio',
+                version: '1.1',
+                release: 'alt1.1',
+                summary: 'PulseAudio is a networked sound server',
+                description: 'long description',
+                group_id: group.id,
+                license: 'LGPL',
+                url: 'http://pulseaudio.org/',
+                size: 10482200,
+                filename: 'pulseaudio-1.1-alt1.1.src.rpm',
+                md5: '602df8c1227b9b5ddf2ba87efb081007',
+                buildtime: '2011-11-17 13:48:29 UTC')
+
+    page = `cat spec/data/people-packages-list`
+    FakeWeb.register_uri(:get,
+                         "http://git.altlinux.org/people-packages-list",
+                         response: page)
+
+    expect{
+      Gear.update_gitrepos('http://git.altlinux.org/people-packages-list')
+      }.to change{ Gear.count }.from(0).to(1)
+  end
 end
