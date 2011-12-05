c5d269a6461be327d9aafcf486fb49a79026de87
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-05 05:19:11 +0200

    Fix gear import

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -15,30 +15,26 @@ class Gear < ActiveRecord::Base
   end
 
   def self.import_gitrepos(url)
-    if Gear.count(:all) == 0
-      branch = Branch.where(name: 'Sisyphus', vendor: 'ALT Linux').first
-      file = open(URI.escape(url)).read
-      file.each_line do |line|
-        gitrepo = line.split[0]
-        login = gitrepo.split('/')[2]
-        login = 'php-coder' if login == 'php_coder'
-        login = 'p_solntsev' if login == 'psolntsev'
-        package = gitrepo.split('/')[4]
-        time = Time.at(line.split[1].to_i)
+    branch = Branch.where(name: 'Sisyphus', vendor: 'ALT Linux').first
+    file = open(URI.escape(url)).read
+    file.each_line do |line|
+      gitrepo = line.split[0]
+      login = gitrepo.split('/')[2]
+      login = 'php-coder' if login == 'php_coder'
+      login = 'p_solntsev' if login == 'psolntsev'
+      package = gitrepo.split('/')[4]
+      time = Time.at(line.split[1].to_i)
 
-        maintainer = Maintainer.where(login: login).first
-        srpm = Srpm.where(name: package.gsub(/\.git/,''), branch_id: branch).first
+      maintainer = Maintainer.where(login: login).first
+      srpm = Srpm.where(name: package.gsub(/\.git/,''), branch_id: branch).first
 
-        if maintainer.nil?
-          puts "#{Time.now.to_s}: maintainer not found '#{login}'"
-        elsif srpm.nil?
-          puts "#{Time.now.to_s}: srpm not found '#{package.gsub(/\.git/,'')}'"
-        else
-          Gear.create(repo: package.gsub(/\.git/,''), maintainer: maintainer, lastchange: time, srpm: srpm)
-        end
+      if maintainer.nil?
+        puts "#{Time.now.to_s}: maintainer not found '#{login}'"
+      elsif srpm.nil?
+        puts "#{Time.now.to_s}: srpm not found '#{package.gsub(/\.git/,'')}'"
+      else
+        Gear.create(repo: package.gsub(/\.git/,''), maintainer: maintainer, lastchange: time, srpm: srpm)
       end
-    else
-      puts "#{Time.now.to_s}: gitrepos already imported"
     end
   end
 end
