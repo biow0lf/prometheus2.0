2ed3c6122dd284148c961c422f0762b52ddf917b
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-21 23:19:11 +0200

    Prepare for devise 2.0.0.rc

--- db/migrate/20101206144602_devise_create_users.rb
+++ db/migrate/20101206144602_devise_create_users.rb
@@ -3,12 +3,26 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
-      t.confirmable
-      t.encryptable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      t.datetime :remember_created_at
+
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      # t.string   :unconfirmed_email # Only if using reconfirmable
+
+      t.string :password_salt
 
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
