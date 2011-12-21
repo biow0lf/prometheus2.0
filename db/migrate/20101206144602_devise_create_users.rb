e9cf1127d358156cb5ae46ced0ea9a33bf5c9f21
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-22 01:54:22 +0200

    Cleanup migration

--- db/migrate/20101206144602_devise_create_users.rb
+++ db/migrate/20101206144602_devise_create_users.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class DeviseCreateUsers < ActiveRecord::Migration
-  def up
+  def change
     create_table(:users) do |t|
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
@@ -34,9 +34,5 @@ class DeviseCreateUsers < ActiveRecord::Migration
     add_index :users, :reset_password_token, :unique => true
     add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
-  end
-
-  def down
-    drop_table :users
   end
 end
