d72bf3f7ae11ab6b06475b9e96136df63bd25b34
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-22 13:59:28 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111221202949) do
+ActiveRecord::Schema.define(:version => 20111222010150) do
 
   create_table "acls", :force => true do |t|
     t.datetime "created_at"
@@ -346,6 +346,7 @@ ActiveRecord::Schema.define(:version => 20111221202949) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
