e7caa2fccfb1a1377ec75134ef5cc17c73ce0c25
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-26 22:02:26 +0200

    Convert to new hash syntax

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -2,20 +2,20 @@
 
 class GroupController < ApplicationController
   def index
-    @branch = Branch.where(:name => params[:branch], :vendor => 'ALT Linux').first
-    @groups = @branch.groups.where(:parent_id => nil).order('LOWER(name)')
+    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+    @groups = @branch.groups.where(parent_id: nil).order('LOWER(name)')
   end
 
   def bygroup
-    @branch = Branch.where(:name => params[:branch], :vendor => 'ALT Linux').first
-    @group = @branch.groups.where(:name => params[:group], :parent_id => nil).first
+    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+    @group = @branch.groups.where(name: params[:group], parent_id: nil).first
     if !params[:group2].nil?
-      @group = @branch.groups.where(:name => params[:group2], :parent_id => @group.id).first
+      @group = @branch.groups.where(name: params[:group2], parent_id: @group.id).first
       if !params[:group3].nil?
-        @group = @branch.groups.where(:name => params[:group3], :parent_id => @group.id).first
+        @group = @branch.groups.where(name: params[:group3], parent_id: @group.id).first
       end
     end
-    render :status => 404, :action => 'nosuchgroup' and return if @group == nil
-    @srpms = @group.srpms.find(:all, :order => 'LOWER(name)')
+    render status: 404, action: 'nosuchgroup' and return if @group == nil
+    @srpms = @group.srpms.find(:all, order: 'LOWER(name)')
   end
 end
