e0b58392e19dae7024c8d192fd9ec36b5c02ef94
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-27 04:05:47 +0200

    More routing specs

---
+++ spec/routing/redirector_routing_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe RedirectorController do
+  describe "routing" do
+    it "should route /glibc to redirector#index" do
+      { :get => "/glibc" }.should route_to(:controller => 'redirector', :action => 'index', :name => 'glibc')
+    end
+
+    # it "should route /src:glibc" do
+    #   { :get => "/src:glibc" }.should route_to(:controller => 'redirector', :action => 'index', :name => 'glibc')
+    # end
+  end
+end
+
+# match '/src\::name' => redirect("/en/Sisyphus/srpms/%{name}"), :name => /[^\/]+/
