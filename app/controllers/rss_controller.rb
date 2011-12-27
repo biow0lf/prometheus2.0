aaa561d08507fb4e9e3a4523623b8355535beb0c
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-27 03:31:26 +0200

    Cleanup

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -4,7 +4,7 @@ class RssController < ApplicationController
   def index
     @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
     @srpms = @branch.srpms.where("srpms.created_at > ?", Time.now - 2.day).includes(:group, :maintainer).order('srpms.created_at DESC').all
-    render :layout => nil
+    render layout: nil
     response.headers["Content-Type"] = "application/xml; charset=utf-8"
   end
 end
