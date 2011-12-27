e01514d2d6b4052b01392ca4ec6f43fc59d74f01
Igor Zubkov <igor.zubkov@gmail.com>
2011-12-27 03:38:29 +0200

    Remove simplecov stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
 # encoding: utf-8
 
-require 'simplecov'
-SimpleCov.start
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
