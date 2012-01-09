373b057e9d4fa5d536045c99ba0a3b72b758169f
Igor Zubkov <igor.zubkov@gmail.com>
2012-01-09 21:58:37 +0200

    Cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,10 @@
 class HomeController < ApplicationController
   def index
     @branches = Branch.order('order_id').all
-    if !fragment_exist? "#{I18n.locale}_top15"
+    if !fragment_exist?("#{I18n.locale}_top15")
       @top15 = Maintainer.top15
     end
-    if !fragment_exist? "#{I18n.locale}_srpms_#{@branch.name}_#{params[:page]}"
+    if !fragment_exist?("#{I18n.locale}_srpms_#{@branch.name}_#{params[:page]}")
       @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").includes(:group, :maintainer).order('srpms.created_at DESC').page(params[:page]).per(50)
     end
   end
