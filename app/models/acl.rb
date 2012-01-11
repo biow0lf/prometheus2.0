5bc1473f972f3f4d8b30e8fd5ced41a230c2153f
Igor Zubkov <igor.zubkov@gmail.com>
2012-01-11 05:01:17 +0200

    Fix typo

--- app/models/acl.rb
+++ app/models/acl.rb
@@ -12,7 +12,7 @@ class Acl < ActiveRecord::Base
   def self.create_redis_cache(vendor_name, branch_name, url)
     branch = Branch.where(vendor: vendor_name, name: branch_name).first
     file = open(URI.escape(url)).read
-    Maintainer.all.each { |maintainer| $redis.del("#{branch.name}:maintainers:#{login}") }
+    Maintainer.all.each { |maintainer| $redis.del("#{branch.name}:maintainers:#{maintainer.login}") }
     file.each_line do |line|
       package = line.split[0]
       for i in 1..line.split.count-1
@@ -30,7 +30,7 @@ class Acl < ActiveRecord::Base
     branch = Branch.where(vendor: vendor_name, name: branch_name).first
     file = open(URI.escape(url)).read
     $redis.multi
-    Maintainer.all.each { |maintainer| $redis.del("#{branch.name}:maintainers:#{login}") }
+    Maintainer.all.each { |maintainer| $redis.del("#{branch.name}:maintainers:#{maintainer.login}") }
     file.each_line do |line|
       package = line.split[0]
       $redis.del("#{branch.name}:#{package}:acls")
