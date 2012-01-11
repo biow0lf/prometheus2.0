bc10af2b641e0e83e624e5fd5786fa5a0b91c4e7
Igor Zubkov <igor.zubkov@gmail.com>
2012-01-11 08:36:44 +0200

    More teams#show fixes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,26 +7,21 @@ class TeamsController < ApplicationController
 
     render(status: 404, action: 'nosuchteam') and return if @team == nil
 
-    # $redis.smembers("Sisyphus:maintainers:@ruby")
-
     @srpms = @branch.srpms.where(name: $redis.smembers("#{@branch.name}:maintainers:@#{params[:id]}")).includes(:repocop_patch).order('LOWER(srpms.name)')
-
-    # @acls = Acl.where(maintainer_id: @team.id, branch_id: @branch.id).includes(:srpm => [:repocop_patch]).order('LOWER(srpms.name)')
-    # @leader = Team.find_by_sql(["SELECT maintainers.login, maintainers.name
-    #                          FROM teams, maintainers, branches
-    #                          WHERE maintainers.id = teams.maintainer_id
-    #                          AND teams.name = ?
-    #                          AND teams.branch_id = branches.id
-    #                          AND branches.name = ?
-    #                          AND leader = 'true'
-    #                          LIMIT 1", '@' + params[:id], @branch.name ])
-    # 
-    # @members = Team.find_by_sql(["SELECT maintainers.login, maintainers.name
-    #                          FROM teams, maintainers, branches
-    #                          WHERE maintainers.id = teams.maintainer_id
-    #                          AND teams.name = ?
-    #                          AND teams.branch_id = branches.id
-    #                          AND branches.name = ?
-    #                          ORDER BY LOWER(maintainers.name)", '@' + params[:id], @branch.name ])
+    @leader = Team.find_by_sql(["SELECT maintainers.login, maintainers.name
+                             FROM teams, maintainers, branches
+                             WHERE maintainers.id = teams.maintainer_id
+                             AND teams.name = ?
+                             AND teams.branch_id = branches.id
+                             AND branches.name = ?
+                             AND leader = 'true'
+                             LIMIT 1", "@#{params[:id]}", @branch.name ])
+    @members = Team.find_by_sql(["SELECT maintainers.login, maintainers.name
+                             FROM teams, maintainers, branches
+                             WHERE maintainers.id = teams.maintainer_id
+                             AND teams.name = ?
+                             AND teams.branch_id = branches.id
+                             AND branches.name = ?
+                             ORDER BY LOWER(maintainers.name)", "@#{params[:id]}", @branch.name ])
   end
 end
