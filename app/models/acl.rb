1ad9eb527d881ca6f256a4cd7996cc00bdb726ca
Igor Zubkov <igor.zubkov@gmail.com>
2012-01-11 04:57:18 +0200

    Cache acls for maintainers in redis

--- app/models/acl.rb
+++ app/models/acl.rb
@@ -12,6 +12,7 @@ class Acl < ActiveRecord::Base
   def self.create_redis_cache(vendor_name, branch_name, url)
     branch = Branch.where(vendor: vendor_name, name: branch_name).first
     file = open(URI.escape(url)).read
+    Maintainer.all.each { |maintainer| $redis.del("#{branch.name}:maintainers:#{login}") }
     file.each_line do |line|
       package = line.split[0]
       for i in 1..line.split.count-1
@@ -20,6 +21,7 @@ class Acl < ActiveRecord::Base
         login = 'p_solntsev' if login == 'psolntsev'
         login = '@vim-plugins' if login == '@vim_plugins'
         $redis.sadd("#{branch.name}:#{package}:acls", login)
+        $redis.sadd("#{branch.name}:maintainers:#{login}", package)
       end
     end
   end
@@ -27,9 +29,10 @@ class Acl < ActiveRecord::Base
   def self.update_redis_cache(vendor_name, branch_name, url)
     branch = Branch.where(vendor: vendor_name, name: branch_name).first
     file = open(URI.escape(url)).read
+    $redis.multi
+    Maintainer.all.each { |maintainer| $redis.del("#{branch.name}:maintainers:#{login}") }
     file.each_line do |line|
       package = line.split[0]
-      $redis.multi
       $redis.del("#{branch.name}:#{package}:acls")
       for i in 1..line.split.count-1
         login = line.split[i]
@@ -37,8 +40,9 @@ class Acl < ActiveRecord::Base
         login = 'p_solntsev' if login == 'psolntsev'
         login = '@vim-plugins' if login == '@vim_plugins'
         $redis.sadd("#{branch.name}:#{package}:acls", login)
+        $redis.sadd("#{branch.name}:maintainers:#{login}", package)
       end
-      $redis.exec
     end
+    $redis.exec
   end
 end
--- spec/models/acl_spec.rb
+++ spec/models/acl_spec.rb
@@ -18,4 +18,6 @@ describe Acl do
   it { should have_db_index :branch_id }
   it { should have_db_index :maintainer_id }
   it { should have_db_index :srpm_id }
+
+  pending "acl import and update"
 end
