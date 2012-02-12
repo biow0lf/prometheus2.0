f1e8a7bccba00095bed154f3a34a72b77c4a2bc7
Igor Zubkov <igor.zubkov@gmail.com>
2012-02-12 22:36:31 +0200

    Route specs for group controller

---
+++ spec/routing/group_routing_spec.rb
@@ -0,0 +1,75 @@
+# encoding: utf-8
+
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe GroupController do
+  describe "routing" do
+    # TODO?
+    # it "should route /Sisyphus/packages to group#index" do
+    #   { :get => "/Sisyphus/packages" }.should route_to(:controller => 'group',
+    #                                                    :action => 'index',
+    #                                                    :branch => 'Sisyphus')
+    # end
+
+    it "should route /en/Sisyphus/packages to group#index" do
+      { :get => "/en/Sisyphus/packages" }.should route_to(:controller => 'group',
+                                                          :action => 'index',
+                                                          :locale => 'en',
+                                                          :branch => 'Sisyphus')
+    end
+
+    # TODO?
+    # it "should route /Sisyphus/packages/Accessibility to group#bygroup" do
+    #   { :get => "/Sisyphus/packages/Accessibility" }.should route_to(:controller => 'group',
+    #                                                                  :action => 'bygroup',
+    #                                                                  :branch => 'Sisyphus',
+    #                                                                  :group => 'Accessibility')
+    # end
+
+    it "should route /en/Sisyphus/packages/Accessibility to group#bygroup" do
+      { :get => "/en/Sisyphus/packages/Accessibility" }.should route_to(:controller => 'group',
+                                                                        :action => 'bygroup',
+                                                                        :locale => 'en',
+                                                                        :branch => 'Sisyphus',
+                                                                        :group => 'Accessibility')
+    end
+
+    # TODO?
+    # it "should route /Sisyphus/packages/Archiving/Compression to group#bygroup" do
+    #   { :get => "/Sisyphus/packages/Archiving/Compression" }.should route_to(:controller => 'group',
+    #                                                                          :action => 'bygroup',
+    #                                                                          :branch => 'Sisyphus',
+    #                                                                          :group => 'Archiving',
+    #                                                                          :group2 => 'Compression')
+    # end
+
+    it "should route /en/Sisyphus/packages/Archiving/Compression to group#bygroup" do
+      { :get => "/en/Sisyphus/packages/Archiving/Compression" }.should route_to(:controller => 'group',
+                                                                                :action => 'bygroup',
+                                                                                :locale => 'en',
+                                                                                :branch => 'Sisyphus',
+                                                                                :group => 'Archiving',
+                                                                                :group2 => 'Compression')
+    end
+
+    # TODO?
+    # it "should route /Sisyphus/packages/System/Configuration/Hardware to group#bygroup" do
+    #   { :get => "/Sisyphus/packages/System/Configuration/Hardware" }.should route_to(:controller => 'group',
+    #                                                                                  :action => 'bygroup',
+    #                                                                                  :branch => 'Sisyphus',
+    #                                                                                  :group => 'System',
+    #                                                                                  :group2 => 'Configuration',
+    #                                                                                  :group3 => 'Hardware')
+    # end
+
+    it "should route /en/Sisyphus/packages/System/Configuration/Hardware to group#bygroup" do
+      { :get => "/en/Sisyphus/packages/System/Configuration/Hardware" }.should route_to(:controller => 'group',
+                                                                                        :action => 'bygroup',
+                                                                                        :locale => 'en',
+                                                                                        :branch => 'Sisyphus',
+                                                                                        :group => 'System',
+                                                                                        :group2 => 'Configuration',
+                                                                                        :group3 => 'Hardware')
+    end
+  end
+end
