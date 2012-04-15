9eccc81f638334eff98847245f9e05b80969e07b
Igor Zubkov <icesik@altlinux.org>
2012-04-15 15:30:07 +0300

    Don't run updates at 00:00 (backup time)

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,8 +6,8 @@ every 1.day, :at => '00:00' do
   rake 'db:backup'
 end
 
-every 1.hour, :at => 5 do
-  rake 'sisyphus:update platform6:update t6:update platform5:update 51:update 50:update 41:update 40:update gear:update ts:in:delta'
+every '5 1-23 * * *' do
+  rake 'sisyphus:update platform6:update t6:update platform5:update 51:update 50:update 41:update 40:update   gear:update ts:in:delta'
 end
 
 every 1.day, :at => '05:30' do
