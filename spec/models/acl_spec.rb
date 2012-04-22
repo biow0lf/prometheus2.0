863e5b56ce7de1a85d9f0eebd6befb6da66144b4
Igor Zubkov <icesik@altlinux.org>
2012-04-22 10:58:18 +0300

    Add specs for Acl model

--- spec/models/acl_spec.rb
+++ spec/models/acl_spec.rb
@@ -3,5 +3,27 @@
 require 'spec_helper'
 
 describe Acl do
-  pending "import and update"
+  it "should import and update acls" do
+    Branch.create(name: 'Sisyphus', vendor: 'ALT Linux')
+
+    page = `cat spec/data/list.packages.sisyphus`
+
+    FakeWeb.register_uri(:get,
+                         'http://git.altlinux.org/acl/list.packages.sisyphus',
+                         response: page)
+
+    Acl.update_redis_cache('ALT Linux',
+                           'Sisyphus',
+                           'http://git.altlinux.org/acl/list.packages.sisyphus')
+
+    $redis.smembers('Sisyphus:rpm:acls').sort.should == ['ldv', 'at'].sort
+    $redis.smembers('Sisyphus:maintainers:ldv').should == ['rpm']
+    $redis.smembers('Sisyphus:maintainers:at').should == ['rpm']
+
+    $redis.smembers('Sisyphus:chkrootkit:acls').sort.should == ['php-coder', '@everybody'].sort
+
+    $redis.smembers('Sisyphus:docs-cppreference:acls').sort.should == ['p_solntsev', '@qa'].sort
+
+    $redis.smembers('Sisyphus:vim-plugin-CRefVIM:acls').should == ['@vim-plugins']
+  end
 end
