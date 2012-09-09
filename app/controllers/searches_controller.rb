980c90969ef5c40b45b4d3ea497fa96589f0b3ba
Igor Zubkov <icesik@altlinux.org>
2012-09-09 15:35:57 +0300

    Paginate

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,7 @@ class SearchesController < ApplicationController
     if params[:query].nil? || params[:query].empty?
       redirect_to action: 'index'
     else
-      @srpms = Srpm.search(params[:query], :order => :name, :max_matches => 10_000, :per_page => 10_000, :with => { :branch_id => @branch.id }, :include => :branch, :page => params[:page], :per_page => 500)
+      @srpms = Srpm.search(params[:query], :order => :name, :max_matches => 10_000, :per_page => 500, :page => params[:page], :with => { :branch_id => @branch.id }, :include => :branch)
       redirect_to(srpm_path(@branch, @srpms.first), status: 302) if @srpms.count == 1
     end
   end
