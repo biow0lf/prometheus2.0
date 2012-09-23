a80f9fc9c10c021867d8c703d6b330f9345d5f00
Igor Zubkov <icesik@altlinux.org>
2012-09-23 13:02:17 +0300

    Update cron scripts

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,32 +12,32 @@ every 1.day, :at => '00:10' do
   rake 'ts:index'
 end
 
-##every '5 1-23 * * *' do
-##  rake 'sisyphus:update p6:update t6:update p5:update 51:update 50:update 41:update 40:update gear:update ts:in:delta'
-##end
+every '5 1-23 * * *' do
+  rake 'sisyphus:update p6:update t6:update p5:update 51:update 50:update 41:update 40:update gear:update ts:in:delta'
+end
 
 #every 1.day, :at => '05:30' do
 #  rake 'sisyphusarm:update ts:in:delta'
 #end
 
-##every 1.day, :at => '05:00' do
-##  rake 'sisyphus:bugs'
-##end
+every 1.day, :at => '05:00' do
+  rake 'sisyphus:bugs'
+end
 
-##every 1.day, :at => '06:45' do
-##  rake 'sisyphus:repocops sisyphus:repocop_patches'
-##end
+every 1.day, :at => '06:45' do
+  rake 'sisyphus:repocops sisyphus:repocop_patches'
+end
 
-##every 1.day, :at => '13:00' do
-##  rake 'ftbfs:update'
-##end
+every 1.day, :at => '13:00' do
+  rake 'ftbfs:update'
+end
 
-##every :sunday, :at => '03:30' do
-##  rake 'sitemap:clean sitemap:refresh'
-##end
+every :sunday, :at => '03:30' do
+  rake 'sitemap:clean sitemap:refresh'
+end
 
-##every :sunday, :at => '06:30' do
-##  rake 'perlwatch:update'
-##end
+every :sunday, :at => '06:30' do
+  rake 'perlwatch:update'
+end
 
 # Learn more: http://github.com/javan/whenever
