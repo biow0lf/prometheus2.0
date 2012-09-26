b7875285911a0c34f08cbfbfe6ded934c68d1997
Igor Zubkov <icesik@altlinux.org>
2012-09-27 00:23:52 +0300

    Fix download

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -13,9 +13,9 @@ class SourcesController < ApplicationController
   	@branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
   	@srpm = @branch.srpms.where(name: params[:srpm_id]).first
     @source = @srpm.sources.where(filename: params[:id]).first
-    if @srpm && !@source.source?
+    if @srpm && @source.source?
       send_data @source.source, disposition: 'attachment', filename: @source.filename
-    elsif @srpm && @source.source?
+    elsif @srpm && !@source.source?
       render layout: false
     else
       render status: 404, action: 'nosuchfile'
