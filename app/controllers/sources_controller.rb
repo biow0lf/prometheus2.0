6995040fcd2c8da16be9f54975a199efdb30ebe0
Igor Zubkov <icesik@altlinux.org>
2012-09-26 22:49:43 +0300

    Show sources index

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -2,6 +2,11 @@
 
 class SourcesController < ApplicationController
   def index
+  	@branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+  	@srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:sources).first
+  	if @srpm
+  	  @allsrpms = Srpm.where(name: params[:srpm_id]).includes(:branch).order('branches.order_id')
+  	end
   end
 
   def show
