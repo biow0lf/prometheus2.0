1e6289984c2f57ed2ea6702e76f3b3111fc7a3c5
Igor Zubkov <icesik@altlinux.org>
2012-10-11 11:38:56 +0300

    Cleanup

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -2,16 +2,16 @@
 
 class SourcesController < ApplicationController
   def index
-  	@branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
-  	@srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:sources).first
-  	if @srpm
-  	  @allsrpms = Srpm.where(name: params[:srpm_id]).includes(:branch).order('branches.order_id')
-  	end
+    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+    @srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:sources).first
+    if @srpm
+      @allsrpms = Srpm.where(name: params[:srpm_id]).includes(:branch).order('branches.order_id')
+    end
   end
 
   def download
-  	@branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
-  	@srpm = @branch.srpms.where(name: params[:srpm_id]).first
+    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+    @srpm = @branch.srpms.where(name: params[:srpm_id]).first
     @source = @srpm.sources.where(filename: params[:id]).first
     if @srpm && @source && @source.source?
       send_data @source.source, disposition: 'attachment', filename: @source.filename
