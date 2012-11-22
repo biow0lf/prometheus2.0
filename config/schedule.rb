96a9ec439010fb13ab584250396538fdeaaa4509
Igor Zubkov <icesik@altlinux.org>
2012-11-22 03:01:04 +0200

    Disable update

--- config/schedule.rb
+++ config/schedule.rb
@@ -11,9 +11,9 @@ end
 ##  rake 'db:backup ts:index'
 ##end
 
-every '5 1-23 * * *' do
-  rake 'sisyphus:update p6:update t6:update p5:update 51:update 50:update 41:update 40:update gear:update ts:in:delta'
-end
+##every '5 1-23 * * *' do
+##  rake 'sisyphus:update p6:update t6:update p5:update 51:update 50:update 41:update 40:update gear:update ts:in:delta'
+##end
 
 #every 1.day, :at => '05:30' do
 #  rake 'sisyphusarm:update ts:in:delta'
