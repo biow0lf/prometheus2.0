d2a104b70a86c9de8e026f796f9f6eede3b5a4ff
Igor Zubkov <icesik@altlinux.org>
2013-01-15 16:00:02 +0200

    Revert "Update thinking-sphinx gem to 3.0.0"
    It's not ready.
    
    This reverts commit dec481c222688dae5727407fe4b6b3b1b0811ee5.

--- app/models/srpm.rb
+++ app/models/srpm.rb
@@ -19,6 +19,23 @@ class Srpm < ActiveRecord::Base
 
   has_one :builder, class_name: "Maintainer", foreign_key: 'id', primary_key: 'builder_id'
 
+  define_index do
+    indexes name, sortable: true
+    indexes summary
+    indexes description
+    indexes filename
+    indexes url
+    indexes packages.name, as: :packages_name, sortable: true
+    indexes packages.summary, as: :packages_summary
+    indexes packages.description, as: :packages_description
+    indexes packages.filename, as: :packages_filename
+    indexes packages.sourcepackage, as: :packages_sourcepackage
+
+    has branch_id
+
+    set_property :delta => :datetime, :threshold => 1.hour
+  end
+
   def to_param
     name
   end
