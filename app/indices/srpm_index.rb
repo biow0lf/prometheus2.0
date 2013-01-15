dec481c222688dae5727407fe4b6b3b1b0811ee5
Igor Zubkov <icesik@altlinux.org>
2013-01-15 15:09:50 +0200

    Update thinking-sphinx gem to 3.0.0

---
+++ app/indices/srpm_index.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+ThinkingSphinx::Index.define :srpm, :with => :active_record do
+  indexes name, sortable: true
+  indexes summary
+  indexes description
+  indexes filename
+  indexes url
+  indexes packages.name, as: :packages_name, sortable: true
+  indexes packages.summary, as: :packages_summary
+  indexes packages.description, as: :packages_description
+  indexes packages.filename, as: :packages_filename
+  indexes packages.sourcepackage, as: :packages_sourcepackage
+
+  has branch_id
+
+  set_property :delta => :datetime, :threshold => 1.hour
+end
