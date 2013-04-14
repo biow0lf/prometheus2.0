a9b1b034cd82c8d26340d334f7a5c4a120ae51e1
Igor Zubkov <icesik@altlinux.org>
2013-04-15 00:17:25 +0300

    Cleanup

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,10 +4,15 @@ class SearchesController < ApplicationController
   def show
     @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
     @branches = Branch.order('order_id').all
-    if params[:query].nil? || params[:query].empty?
+    if params[:query].empty?
       redirect_to action: 'index'
     else
-      @srpms = Srpm.search(params[:query], :order => :name, :max_matches => 10_000, :per_page => 10_000, :with => { :branch_id => @branch.id }, :include => :branch)
+      @srpms = Srpm.search(params[:query],
+                           :order => :name,
+                           :max_matches => 10_000,
+                           :per_page => 10_000,
+                           :with => { :branch_id => @branch.id },
+                           :include => :branch)
       redirect_to(srpm_path(@branch, @srpms.first), status: 302) if @srpms.count == 1
     end
   end
