d0bc5c7e0624f07d9c772579d24f3311ed2d6e94
Igor Zubkov <icesik@altlinux.org>
2013-05-11 17:06:06 +0300

    Branch has many changelogs

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -5,6 +5,7 @@ class Branch < ActiveRecord::Base
   attr_accessible :name, :vendor, :path, :order_id
 
   has_many :srpms
+  has_many :changelogs, through: :srpms
   has_many :packages
   has_many :groups
   has_many :teams
