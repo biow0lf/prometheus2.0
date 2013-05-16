025445feb6ea0f01119b8f22d7db14ed90e94806
Igor Zubkov <icesik@altlinux.org>
2013-05-16 22:11:25 +0300

    Cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
-# Methods added to this helper will be available to all templates in the
-# application.
 module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title.html_safe }
