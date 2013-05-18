11af6a6b44dfc75a9e5fa4880963dfb5025da81b
Igor Zubkov <icesik@altlinux.org>
2013-05-18 15:49:26 +0300

    Update rss routing spec

--- spec/routing/rss_routing_spec.rb
+++ spec/routing/rss_routing_spec.rb
@@ -2,12 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RssController do
   describe "routing" do
-    # TODO?
-    # it "should route /Sisyphus/rss to rss#index" do
-    #   { :get => "/Sisyphus/rss" }.should route_to(:controller => 'rss',
-    #                                               :action => 'index',
-    #                                               :branch => 'Sisyphus')
-    # end
+    it "should route /Sisyphus/rss to rss#index" do
+      { :get => "/Sisyphus/rss" }.should route_to(:controller => 'rss',
+                                                  :action => 'index',
+                                                  :branch => 'Sisyphus')
+    end
 
     it "should route /en/Sisyphus/rss to rss#index" do
       { :get => "/en/Sisyphus/rss" }.should route_to(:controller => 'rss',
