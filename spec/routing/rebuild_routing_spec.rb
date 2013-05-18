b03167918a88d8b05024ef81ddd628703fe3c18b
Igor Zubkov <icesik@altlinux.org>
2013-05-18 17:09:08 +0300

    Update rebuild routing spec

--- spec/routing/rebuild_routing_spec.rb
+++ spec/routing/rebuild_routing_spec.rb
@@ -2,11 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RebuildController do
   describe "routing" do
-    # TODO?
-    # it "should route /rebuild to rebuild#index" do
-    #   { :get => "/rebuild" }.should route_to(:controller => 'rebuild',
-    #                                          :action => 'index')
-    # end
+    it "should route /rebuild to rebuild#index" do
+      { :get => "/rebuild" }.should route_to(:controller => 'rebuild',
+                                             :action => 'index')
+    end
 
     it "should route /en/rebuild to rebuild#index" do
       { :get => "/en/rebuild" }.should route_to(:controller => 'rebuild',
