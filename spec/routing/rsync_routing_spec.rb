d38ced3b3059b79dfe3937ca416f6b2c8e3f1efe
Igor Zubkov <icesik@altlinux.org>
2013-05-18 15:50:45 +0300

    Update rsync routing spec

--- spec/routing/rsync_routing_spec.rb
+++ spec/routing/rsync_routing_spec.rb
@@ -2,11 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe RsyncController do
   describe "routing" do
-    # TODO?
-    # it "should route /rsync/new to rsync#new" do
-    #   { :get => "/rsync/new" }.should route_to(:controller => 'rsync',
-    #                                            :action => 'new')
-    # end
+    it "should route /rsync/new to rsync#new" do
+      { :get => "/rsync/new" }.should route_to(:controller => 'rsync',
+                                               :action => 'new')
+    end
 
     it "should route /en/rsync/new to rsync#new" do
       { :get => "/en/rsync/new" }.should route_to(:controller => 'rsync',
