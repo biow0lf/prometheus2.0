dd9741d1d685fa6ad925eb626f42090a9b3c43db
Igor Zubkov <icesik@altlinux.org>
2013-05-18 16:02:19 +0300

    Update redirector routing spec

--- spec/routing/redirector_routing_spec.rb
+++ spec/routing/redirector_routing_spec.rb
@@ -3,13 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe RedirectorController do
   describe "routing" do
     it "should route /glibc to redirector#index" do
-      { :get => "/glibc" }.should route_to(:controller => 'redirector', :action => 'index', :name => 'glibc')
+      { :get => "/glibc" }.should route_to(:controller => 'redirector',
+                                           :action => 'index',
+                                           :name => 'glibc')
     end
 
-    # it "should route /src:glibc" do
-    #   { :get => "/src:glibc" }.should route_to(:controller => 'redirector', :action => 'index', :name => 'glibc')
-    # end
+    it "should route /src:glibc to redirector#index" do
+      { :get => "/src:glibc" }.should route_to(:controller => 'redirector',
+                                               :action => 'index',
+                                               :name => 'src:glibc')
+    end
   end
 end
-
-# match '/src\::name' => redirect("/en/Sisyphus/srpms/%{name}"), :name => /[^\/]+/
