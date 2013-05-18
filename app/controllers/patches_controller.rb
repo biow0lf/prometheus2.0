fff5249f8dc657cda90a1460d43327ebc683c7af
Igor Zubkov <icesik@altlinux.org>
2013-05-18 16:24:56 +0300

    Cleanup actions

--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -2,11 +2,11 @@ class PatchesController < ApplicationController
   def index
   end
 
-  def show
-    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
-    @patch = File.read("pmount-0.9.17-alt-floppy.patch")
-  end
-
-  def get
-  end
+#  def show
+#    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+#    @patch = File.read("pmount-0.9.17-alt-floppy.patch")
+#  end
+#
+#  def get
+#  end
 end
