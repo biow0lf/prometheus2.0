a836441dd59e81b8219c6ebb542a589002947d07
Igor Zubkov <icesik@altlinux.org>
2013-05-18 15:22:46 +0300

    Update pages routing spec

--- spec/routing/pages_routing_spec.rb
+++ spec/routing/pages_routing_spec.rb
@@ -2,11 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PagesController do
   describe "routing" do
-    # TODO?
-    # it "should route /project to pages#project" do
-    #   { :get => "/project" }.should route_to(:controller => 'pages',
-    #                                          :action => 'project')
-    # end
+    it "should route /project to pages#project" do
+      { :get => "/project" }.should route_to(:controller => 'pages',
+                                             :action => 'project')
+    end
 
     it "should route /en/project to pages#project" do
       { :get => "/en/project" }.should route_to(:controller => 'pages',
