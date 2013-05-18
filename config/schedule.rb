d25a88b2413ea121e7b0219f62c63bafdcb976f6
Igor Zubkov <icesik@altlinux.org>
2013-05-18 16:32:40 +0300

    Add p7 branch update to cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,7 +13,7 @@ end
 ##end
 
 every '5 1-23 * * *' do
-  rake 'sisyphus:update p6:update t6:update p5:update 51:update 50:update 41:update 40:update gear:update'
+  rake 'sisyphus:update p7:update p6:update t6:update p5:update 51:update 50:update 41:update 40:update gear:update'
 end
 
 #every 1.day, :at => '05:30' do
