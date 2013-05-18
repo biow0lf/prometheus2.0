42eebd98e26b52af7a4918ae9be87ff6f292d915
Igor Zubkov <icesik@altlinux.org>
2013-05-18 20:23:10 +0300

    Optimize

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -1,6 +1,6 @@
 class SecurityController < ApplicationController
   def index
     @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
-    @changelogs = @branch.changelogs.where("changelogs.changelogtext LIKE '%CVE%'").order('changelogs.changelogtime DESC').page(params[:page]).per(50)
+    @changelogs = @branch.changelogs.where("changelogs.changelogtext LIKE '%CVE%'").includes(:srpm).includes(:srpm => [:branch]).order('changelogs.changelogtime DESC').page(params[:page]).per(50)
   end
 end
