eb0d4d4f23381f95c7845461f569cdf1e7adef31
Igor Zubkov <icesik@altlinux.org>
2013-05-18 15:51:57 +0300

    Update teams routing spec

--- spec/routing/teams_routing_spec.rb
+++ spec/routing/teams_routing_spec.rb
@@ -2,13 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe TeamsController do
   describe "routing" do
-    # TODO?
-    # it "should route /Sisyphus/teams/ruby to teams#show" do
-    #   { :get => "/Sisyphus/teams/ruby" }.should route_to(:controller => 'teams',
-    #                                                      :action => 'show',
-    #                                                      :branch => 'Sisyphus',
-    #                                                      :id => 'ruby')
-    # end
+    it "should route /Sisyphus/teams/ruby to teams#show" do
+      { :get => "/Sisyphus/teams/ruby" }.should route_to(:controller => 'teams',
+                                                         :action => 'show',
+                                                         :branch => 'Sisyphus',
+                                                         :id => 'ruby')
+    end
 
     it "should route /en/Sisyphus/teams/ruby to teams#show" do
       { :get => "/en/Sisyphus/teams/ruby" }.should route_to(:controller => 'teams',
