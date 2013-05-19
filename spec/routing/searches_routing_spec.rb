edc214b40d475e678b52b5457933e741d6db569a
Igor Zubkov <icesik@altlinux.org>
2013-05-19 13:56:42 +0300

    Fix warning

--- spec/routing/searches_routing_spec.rb
+++ spec/routing/searches_routing_spec.rb
@@ -2,21 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SearchesController do
   describe "routing" do
-    it "should route /search?utf8=✓&branch=Sisyphus&query=openbox to home#index" do
-      { :get => "/search?utf8=✓&branch=Sisyphus&query=openbox" }.should route_to(:controller => 'searches',
-                                                                                 :action => 'show')
-                                                                                 # :utf8 => '✓',
-                                                                                 # :branch => 'Sisyphus',
-                                                                                 # :query => 'openbox')
+    it "should route /search?utf8=yes&branch=Sisyphus&query=openbox to home#index" do
+      { :get => "/search?utf8=yes&branch=Sisyphus&query=openbox" }.should route_to(:controller => 'searches',
+                                                                                   :action => 'show')
+                                                                                   # :utf8 => '✓',
+                                                                                   # :branch => 'Sisyphus',
+                                                                                   # :query => 'openbox')
     end
 
-    it "should route /en/search?utf8=✓&branch=Sisyphus&query=openbox to home#index" do
-      { :get => "/en/search?utf8=✓&branch=Sisyphus&query=openbox" }.should route_to(:controller => 'searches',
-                                                                                    :action => 'show',
-                                                                                    # :utf8 => '✓',
-                                                                                    # :branch => 'Sisyphus',
-                                                                                    # :query => 'openbox',
-                                                                                    :locale => 'en')
+    it "should route /en/search?utf8=yes&branch=Sisyphus&query=openbox to home#index" do
+      { :get => "/en/search?utf8=yes&branch=Sisyphus&query=openbox" }.should route_to(:controller => 'searches',
+                                                                                      :action => 'show',
+                                                                                      # :utf8 => '✓',
+                                                                                      # :branch => 'Sisyphus',
+                                                                                      # :query => 'openbox',
+                                                                                      :locale => 'en')
     end
   end
 end
