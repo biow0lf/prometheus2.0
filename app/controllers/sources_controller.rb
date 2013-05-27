f94cb4b4c7cb789d13facec43ec775a8ec47a0c3
Igor Zubkov <icesik@altlinux.org>
2013-05-28 00:24:50 +0300

    Fix sources controller

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,14 +1,17 @@
 class SourcesController < ApplicationController
   def index
-    @branch = Branch.find_by_name_and_vendor!(params[:branch], 'ALT Linux')
-    @srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:sources).first!
+    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+    @srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:sources).first
+    render status: 404, action: '404' and return if @srpm == nil
     @allsrpms = Srpm.where(name: params[:srpm_id]).includes(:branch).order('branches.order_id')
   end
 
   def download
-    @branch = Branch.find_by_name_and_vendor!(params[:branch], 'ALT Linux')
-    @srpm = @branch.srpms.where(name: params[:srpm_id]).first!
+    @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
+    @srpm = @branch.srpms.where(name: params[:srpm_id]).first
+    render status: 404, action: '404' and return if @srpm == nil
     @source = @srpm.sources.where(filename: params[:id]).first
+    render status: 404, action: '404' and return if @source == nil
     if @source && @source.source?
       send_data @source.source, disposition: 'attachment', filename: @source.filename
     elsif @source && !@source.source?
