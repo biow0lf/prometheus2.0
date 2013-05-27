1c5497a93872db621220246d3109f335af4093c5
Igor Zubkov <icesik@altlinux.org>
2013-05-28 00:19:37 +0300

    Fix patches controller

--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -1,24 +1,28 @@
 class PatchesController < ApplicationController
   def index
     @branch = Branch.find_by_name_and_vendor!(params[:branch], 'ALT Linux')
-    @srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:patches).first!
+    @srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:patches).first
+    render status: 404, action: '404' and return if @srpm == nil
     @allsrpms = Srpm.where(name: params[:srpm_id]).includes(:branch).order('branches.order_id')
   end
 
   def download
     @branch = Branch.find_by_name_and_vendor!(params[:branch], 'ALT Linux')
-    @srpm = @branch.srpms.where(name: params[:srpm_id]).first!
+    @srpm = @branch.srpms.where(name: params[:srpm_id]).first
+    render status: 404, action: '404' and return if @srpm == nil
     @patch = @srpm.patches.where(filename: params[:id]).first
+    render status: 404, action: '404' and return if @patch == nil
     if @patch && @patch.patch?
       send_data @patch.patch, disposition: 'attachment', filename: @patch.filename
-    else @patch && !@patch.patch?
+    elsif @patch && !@patch.patch?
       render layout: false
     end
   end
 
   def show
     @branch = Branch.find_by_name_and_vendor!(params[:branch], 'ALT Linux')
-    @srpm = @branch.srpms.where(name: params[:srpm_id]).first!
+    @srpm = @branch.srpms.where(name: params[:srpm_id]).first
+    render status: 404, action: '404' and return if @srpm == nil
     @patch = @srpm.patches.where(filename: params[:id]).first
     @html_data = CodeRay.scan(@patch.patch, :diff).div(:line_numbers => :table)
   end
