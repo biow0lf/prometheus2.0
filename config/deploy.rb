0405681952681a76656c274b4da1bd8a1d7d3a2c
Igor Zubkov <icesik@altlinux.org>
2013-06-01 14:10:15 +0300

    Update deploy process

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,6 +95,7 @@ namespace :deploy do
       ln -sf #{shared_path}/config/redis.yml #{latest_release}/config/redis.yml &&
       ln -sf #{shared_path}/config/initializers/devise.rb #{latest_release}/config/initializers/devise.rb &&
       ln -sf #{shared_path}/config/initializers/secret_token.rb #{latest_release}/config/initializers/secret_token.rb &&
+      ln -sf #{shared_path}/config/initializers/airbrake.rb #{latest_release}/config/initializers/airbrake.rb &&
       cd #{release_path} && bundle exec rake assets:precompile
     CMD
 
