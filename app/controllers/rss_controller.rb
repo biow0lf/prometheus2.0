2dafb00993dc11bca60d7e6e5a9c3ab841e52860
Igor Zubkov <icesik@altlinux.org>
2013-06-01 14:17:15 +0300

    Optimize

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -1,7 +1,7 @@
 class RssController < ApplicationController
   def index
     @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
-    @srpms = @branch.srpms.where("srpms.created_at > ?", Time.now - 2.day).includes(:group).order('srpms.created_at DESC').all
+    @srpms = @branch.srpms.where("srpms.created_at > ?", Time.now - 2.day).order('srpms.created_at DESC').all
     render layout: nil
     response.headers["Content-Type"] = "application/xml; charset=utf-8"
   end
