b76234df1fe28e9113e150398b2fd5638d073c45
Igor Zubkov <icesik@altlinux.org>
2013-06-04 00:13:08 +0300

    Extract rpm stuff to class

--- lib/rpm.rb
+++ lib/rpm.rb
@@ -1,4 +1,78 @@
 class Rpm
+  def initialize(file)
+    @file = file
+  end
+
+#  def file
+#    @file
+#  end
+
+  def name
+    @name ||= extract_tag('NAME')
+  end
+
+  def version
+    @version ||= extract_tag('VERSION')
+  end
+
+  def release
+    @release ||= extract_tag('RELEASE')
+  end
+
+  def filename
+    "#{name}-#{version}-#{release}.src.rpm"
+  end
+
+  def epoch
+    # TODO: make test for this
+    # TODO: srpm.epoch = nil if srpm.epoch == '(none)'
+    @epoch ||= extract_tag('EPOCH')
+  end
+
+  def summary
+    @summary ||= extract_tag('SUMMARY')
+  end
+
+  def group
+    @group ||= extract_tag('GROUP')
+  end
+
+  def packager
+    @packager ||= extract_tag('PACKAGER')
+  end
+
+  def url
+    @url ||= extract_tag('URL')
+  end
+
+  def license
+    @license ||= extract_tag('LICENSE')
+  end
+
+  def vendor
+    @vendor ||= extract_tag('VENDOR')
+  end
+
+  def distribution
+    @distribution ||= extract_tag('DISTRIBUTION')
+  end
+
+  def description
+    @description ||= extract_tag('DESCRIPTION')
+  end
+
+  def buildtime
+    @buildtime ||= extract_tag('BUILDTIME')
+  end
+
+  def extract_tag(tag)
+    `export LANG=C && rpm -qp --queryformat='%{#{ tag }}' #{ @file }`
+  end
+
+#  def none_is_nil(value)
+#    nil if value == '(none)'
+#  end
+
   def self.check_md5(file)
     output = `export LANG=C && rpm -K --nogpg #{file}`
     if !output.empty? && output.chop.split(': ').last == 'md5 OK'
@@ -6,5 +80,3 @@ class Rpm
     end
   end
 end
-
-#RPM = Rpm
--- spec/lib/rpm_spec.rb
+++ spec/lib/rpm_spec.rb
@@ -1,15 +1,132 @@
 require 'spec_helper'
 
 describe Rpm do
-  it 'should verify md5 sum of rpm and return true if rpm is OK' do
+  it 'should return content of TAG' do
     file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
-    Rpm.should_receive(:`).with("export LANG=C && rpm -K --nogpg #{file}").and_return("openbox-3.5.0-alt1.src.rpm: md5 OK\n")
-    Rpm.check_md5(file).should be_true
+    tag = 'NAME'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{#{ tag }}' #{ file }").and_return('openbox')
+
+    rpm.extract_tag('NAME').should == 'openbox'
   end
 
-  it 'should verify md5 sum of rpm and return nil if rpm is broken' do
+  it 'should return package name' do
     file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
-    Rpm.should_receive(:`).with("export LANG=C && rpm -K --nogpg #{file}").and_return("")
-    Rpm.check_md5(file).should be_nil
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{NAME}' #{ file }").and_return('openbox')
+    rpm.name.should == 'openbox'
   end
+
+  it 'should return package version' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{VERSION}' #{ file }").and_return('3.4.11.1')
+    rpm.version.should == '3.4.11.1'
+  end
+
+  it 'should return package release' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{RELEASE}' #{ file }").and_return('alt1.1.1')
+    rpm.release.should == 'alt1.1.1'
+  end
+
+  it 'should return package epoch' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{EPOCH}' #{ file }").and_return('(none)')
+    rpm.epoch.should == '(none)'
+  end
+
+  it 'should return package summary' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{SUMMARY}' #{ file }").and_return('short description')
+    rpm.summary.should == 'short description'
+  end
+
+  it 'should return package group' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{GROUP}' #{ file }").and_return('Graphical desktop/Other')
+    rpm.group.should == 'Graphical desktop/Other'
+  end
+
+  it 'should return package packager' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{PACKAGER}' #{ file }").and_return('Igor Zubkov <icesik@altlinux.org>')
+    rpm.packager.should == 'Igor Zubkov <icesik@altlinux.org>'
+  end
+
+  it 'should return package license' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{LICENSE}' #{ file }").and_return('GPLv2+')
+    rpm.license.should == 'GPLv2+'
+  end
+
+  it 'should return package url' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{URL}' #{ file }").and_return('http://openbox.org/')
+    rpm.url.should == 'http://openbox.org/'
+  end
+
+  it 'should return package description' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{DESCRIPTION}' #{ file }").and_return('long description')
+    rpm.description.should == 'long description'
+  end
+
+  it 'should return package vendor' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{VENDOR}' #{ file }").and_return('ALT Linux Team')
+    rpm.vendor.should == 'ALT Linux Team'
+  end
+
+  it 'should return package distribution' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{DISTRIBUTION}' #{ file }").and_return('ALT Linux')
+    rpm.distribution.should == 'ALT Linux'
+  end
+
+  it 'should return package buildtime' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{BUILDTIME}' #{ file }").and_return('1315301838')
+    rpm.buildtime.should == '1315301838'
+  end
+
+  # TODO: check for real rpm tag for this
+  it 'should return package filename' do
+    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+    rpm = Rpm.new(file)
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{NAME}' #{ file }").and_return('openbox')
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{VERSION}' #{ file }").and_return('3.4.11.1')
+    rpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{RELEASE}' #{ file }").and_return('alt1.1.1')
+    rpm.filename.should == 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+  end
+
+#    Srpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{CHANGELOGTIME}' #{file}").and_return('1312545600')
+#    Srpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{CHANGELOGNAME}' #{file}").and_return('Igor Zubkov <icesik@altlinux.org> 3.
+#    Srpm.should_receive(:`).with("export LANG=C && rpm -qp --queryformat='%{CHANGELOGTEXT}' #{file}").and_return('- 3.4.11.1')
+
+#    File.should_receive(:size).with(file).and_return(831617)
+
+
+#  it 'should verify md5 sum of rpm and return true if rpm is OK' do
+#    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+#    Rpm.should_receive(:`).with("export LANG=C && rpm -K --nogpg #{file}").and_return("openbox-3.5.0-alt1.src.rpm: md5 OK\n")
+#    Rpm.check_md5(file).should be_true
+#  end
+#
+#  it 'should verify md5 sum of rpm and return nil if rpm is broken' do
+#    file = 'openbox-3.4.11.1-alt1.1.1.src.rpm'
+#    Rpm.should_receive(:`).with("export LANG=C && rpm -K --nogpg #{file}").and_return("")
+#    Rpm.check_md5(file).should be_nil
+#  end
 end
