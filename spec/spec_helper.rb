31047ffc083b023a08815fcea35d44ae9a3c3c72
Igor Zubkov <icesik@altlinux.org>
2013-06-15 02:08:38 +0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,11 @@ require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 FakeWeb.allow_net_connect = false
 FakeWeb.allow_net_connect = 'https://coveralls.io/api/v1/jobs'
