d3668437e5e22ddb678df0410ac37b6f83cd1224
Igor Zubkov <icesik@altlinux.org>
2013-12-10 00:40:18 +0200

    Refactor

--- app/decorators/srpm_decorator.rb
+++ app/decorators/srpm_decorator.rb
@@ -3,17 +3,21 @@ class SrpmDecorator < Draper::Decorator
 
   def short_url
     if object.url
-      if object.url.length > 27
-        local_link_to("#{object.url[0..26]}...", object.url)
-      else
-        local_link_to(object.url, object.url)
-      end
+      create_link(object.url)
     else
       '&ndash;'.html_safe
     end
   end
 
   private
+
+  def create_link(url)
+    if url.length > 27
+      local_link_to("#{ url[0..26] }...", url)
+    else
+      local_link_to(url, url)
+    end
+  end
 
   def local_link_to(text, url)
     h.link_to(text, url, class: 'news', rel: 'nofollow')
