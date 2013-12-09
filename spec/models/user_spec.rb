73808f5fd6c8cdd01ef471d5335f20e2d8862440
Igor Zubkov <icesik@altlinux.org>
2013-12-10 01:24:25 +0200

    Cleanup user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,39 +2,19 @@ require 'spec_helper'
 
 describe User do
   it 'should return login on User.login' do
-    user = User.create!(email: 'icesik@altlinux.org',
-                        password: 'password',
-                        password_confirmation: 'password')
-    user.confirmed_at = Time.zone.now
-    user.save!
-    user.login.should == 'icesik'
+    User.new(email: 'icesik@altlinux.org').login.should == 'icesik'
   end
 
   it 'should return true for @altlinux.org emails' do
-    user = User.create!(email: 'icesik@altlinux.org',
-                        password: 'password',
-                        password_confirmation: 'password')
-    user.confirmed_at = Time.zone.now
-    user.save!
-    user.is_alt_team?.should be_true
+    User.new(email: 'icesik@altlinux.org').is_alt_team?.should be_true
   end
 
   it 'should return true for @altlinux.ru emails' do
-    user = User.create!(email: 'icesik@altlinux.ru',
-                        password: 'password',
-                        password_confirmation: 'password')
-    user.confirmed_at = Time.zone.now
-    user.save!
-    user.is_alt_team?.should be_true
+    User.new(email: 'icesik@altlinux.ru').is_alt_team?.should be_true
   end
 
   it 'should return false for non altlinux team member' do
-    user = User.create!(email: 'user@email.com',
-                        password: 'password',
-                        password_confirmation: 'password')
-    user.confirmed_at = Time.zone.now
-    user.save!
-    user.is_alt_team?.should be_false
+    User.new(email: 'user@email.com').is_alt_team?.should be_false
   end
 
   it 'should deny change email' do
