f3ba17cac33209e4fc12a50e761fef607215c887
Igor Zubkov <icesik@altlinux.org>
2013-12-10 00:38:48 +0200

    Cleanup

--- app/decorators/maintainer_decorator.rb
+++ app/decorators/maintainer_decorator.rb
@@ -3,6 +3,6 @@ class MaintainerDecorator < Draper::Decorator
 
   def avatar_url
     gravatar_id = Digest::MD5.hexdigest(object.email.downcase)
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=420&r=g"
+    "http://gravatar.com/avatar/#{ gravatar_id }.png?s=420&r=g"
   end
 end
