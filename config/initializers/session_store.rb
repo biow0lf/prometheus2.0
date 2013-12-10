ee7444768b9105daaec659010ea9dfb6065e3487
Igor Zubkov <icesik@altlinux.org>
2013-12-10 23:21:31 +0200

    Cleanup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Prometheus20::Application.config.session_store :cookie_store, :key => '_prometheus2.0_session'
+Prometheus20::Application.config.session_store :cookie_store, key: '_prometheus2.0_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
