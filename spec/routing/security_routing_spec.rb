d4a5e542c1506dfc8b246858d3bd2002f3549581
Igor Zubkov <icesik@altlinux.org>
2013-12-22 01:09:06 +0200

    Fix security routing spec

--- spec/routing/security_routing_spec.rb
+++ spec/routing/security_routing_spec.rb
@@ -3,29 +3,31 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe SecurityController do
   describe "routing" do
     it "should route /en/Sisyphus/security to security#index" do
-      { :get => "/en/Sisyphus/security" }.should route_to(:controller => 'security',
-                                                          :action => 'index',
-                                                          :branch => 'Sisyphus',
-                                                          :locale => 'en')
+      { :get => "/en/Sisyphus/security" }.should route_to(controller: 'security',
+                                                          action: 'index',
+                                                          branch: 'Sisyphus',
+                                                          locale: 'en')
     end
 
     it "should route /Sisyphus/security to security#index" do
-      { :get => "/Sisyphus/security" }.should route_to(:controller => 'security',
-                                                       :action => 'index',
-                                                       :branch => 'Sisyphus')
+      { :get => "/Sisyphus/security" }.should route_to(controller: 'security',
+                                                       action: 'index',
+                                                       branch: 'Sisyphus')
     end
 
     it "should route /en/Sisyphus/security?page=2 to security#index" do
-      { :get => "/en/Sisyphus/security?page=2" }.should route_to(:controller => 'security',
-                                                                 :action => 'index',
-                                                                 :branch => 'Sisyphus',
-                                                                 :locale => 'en')
+      { :get => "/en/Sisyphus/security?page=2" }.should route_to(controller: 'security',
+                                                                 action: 'index',
+                                                                 locale: 'en',
+                                                                 branch: 'Sisyphus',
+                                                                 page: '2')
     end
 
     it "should route /Sisyphus/security?page=2 to security#index" do
-      { :get => "/Sisyphus/security?page=2" }.should route_to(:controller => 'security',
-                                                              :action => 'index',
-                                                              :branch => 'Sisyphus')
+      { :get => "/Sisyphus/security?page=2" }.should route_to(controller: 'security',
+                                                              action: 'index',
+                                                              branch: 'Sisyphus',
+                                                              page: '2')
     end
   end
 end
