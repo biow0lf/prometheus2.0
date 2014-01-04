47e366b418a4d4d232d0be01391f34d4a4a48f6a
Igor Zubkov <icesik@altlinux.org>
2014-01-04 17:35:38 +0200

    Disable caching

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,12 @@ class HomeController < ApplicationController
   def index
     @branch = Branch.where(name: params[:branch], vendor: 'ALT Linux').first
     @branches = Branch.order('order_id')
-    if !fragment_exist?("#{I18n.locale}_top15_#{@branch.name}")
-      @top15 = Maintainer.top15(@branch)
-    end
-    if !fragment_exist?("#{I18n.locale}_srpms_#{@branch.name}_#{params[:page]}")
-      @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").includes(:branch, :builder).order('srpms.created_at DESC').page(params[:page]).per(50)
-    end
+    # if !fragment_exist?("#{I18n.locale}_top15_#{@branch.name}")
+    @top15 = Maintainer.top15(@branch)
+    # end
+    # if !fragment_exist?("#{I18n.locale}_srpms_#{@branch.name}_#{params[:page]}")
+    @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").includes(:branch, :builder).order('srpms.created_at DESC').page(params[:page]).per(50)
+    # end
   end
 
   def maintainers_list
