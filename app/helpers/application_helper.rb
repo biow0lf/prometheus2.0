d846e771d9c258da0d8b6bcd768f70611f4fdfe3
Igor Zubkov <igor.zubkov@gmail.com>
2014-09-26 20:28:16 +0300

    Cleanup helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
 
   def keywords(string)
     # http://en.wikipedia.org/wiki/Most_common_words_in_English
-    skiplist = %w(the be to of and a in i it for not on he as you do at this but his by from or an)
+    skiplist = %w(the be to of and a in i it for not on he as you do at this but
+                  his by from or an)
     content_for(:keywords) { (string.split(' ') - skiplist).join(', ') }
   end
 
@@ -14,7 +15,7 @@ module ApplicationHelper
   end
 
   def current_page(url, lang)
-    return "/#{lang}" if url == '/'
+    return "/#{ lang }" if url == '/'
     url[1, 2] = lang
     url
   end
