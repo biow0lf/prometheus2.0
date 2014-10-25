e56ad91299c4e3573907fc00a9293d68b4adb67b
Igor Zubkov <igor.zubkov@gmail.com>
2014-10-25 03:59:21 +0300

    Update group routing spec

--- spec/routing/group_routing_spec.rb
+++ spec/routing/group_routing_spec.rb
@@ -1,69 +1,69 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe GroupController, :type => :routing do
-  describe "routing" do
-    it "should route /Sisyphus/packages to group#index" do
-      expect({ :get => "/Sisyphus/packages" }).to route_to(:controller => 'group',
-                                                       :action => 'index',
-                                                       :branch => 'Sisyphus')
+  describe 'routing' do
+    it 'should route /Sisyphus/packages to group#index' do
+      expect({ get: '/Sisyphus/packages' }).to route_to(controller: 'group',
+                                                        action: 'index',
+                                                        branch: 'Sisyphus')
     end
 
-    it "should route /en/Sisyphus/packages to group#index" do
-      expect({ :get => "/en/Sisyphus/packages" }).to route_to(:controller => 'group',
-                                                          :action => 'index',
-                                                          :locale => 'en',
-                                                          :branch => 'Sisyphus')
+    it 'should route /en/Sisyphus/packages to group#index' do
+      expect({ get: '/en/Sisyphus/packages' }).to route_to(controller: 'group',
+                                                           action: 'index',
+                                                           locale: 'en',
+                                                           branch: 'Sisyphus')
     end
 
-    it "should route /Sisyphus/packages/Accessibility to group#bygroup" do
-      expect({ :get => "/Sisyphus/packages/Accessibility" }).to route_to(:controller => 'group',
-                                                                     :action => 'show',
-                                                                     :branch => 'Sisyphus',
-                                                                     :group => 'Accessibility')
+    it 'should route /Sisyphus/packages/Accessibility to group#bygroup' do
+      expect({ get: '/Sisyphus/packages/Accessibility' }).to route_to(controller: 'group',
+                                                                      action: 'show',
+                                                                      branch: 'Sisyphus',
+                                                                      group: 'Accessibility')
     end
 
-    it "should route /en/Sisyphus/packages/Accessibility to group#bygroup" do
-      expect({ :get => "/en/Sisyphus/packages/Accessibility" }).to route_to(:controller => 'group',
-                                                                        :action => 'show',
-                                                                        :locale => 'en',
-                                                                        :branch => 'Sisyphus',
-                                                                        :group => 'Accessibility')
+    it 'should route /en/Sisyphus/packages/Accessibility to group#bygroup' do
+      expect({ get: '/en/Sisyphus/packages/Accessibility' }).to route_to(controller: 'group',
+                                                                         action: 'show',
+                                                                         locale: 'en',
+                                                                         branch: 'Sisyphus',
+                                                                         group: 'Accessibility')
     end
 
-    it "should route /Sisyphus/packages/Archiving/Compression to group#bygroup" do
-      expect({ :get => "/Sisyphus/packages/Archiving/Compression" }).to route_to(:controller => 'group',
-                                                                             :action => 'show',
-                                                                             :branch => 'Sisyphus',
-                                                                             :group => 'Archiving',
-                                                                             :group2 => 'Compression')
+    it 'should route /Sisyphus/packages/Archiving/Compression to group#bygroup' do
+      expect({ get: '/Sisyphus/packages/Archiving/Compression' }).to route_to(controller: 'group',
+                                                                              action: 'show',
+                                                                              branch: 'Sisyphus',
+                                                                              group: 'Archiving',
+                                                                              group2: 'Compression')
     end
 
-    it "should route /en/Sisyphus/packages/Archiving/Compression to group#bygroup" do
-      expect({ :get => "/en/Sisyphus/packages/Archiving/Compression" }).to route_to(:controller => 'group',
-                                                                                :action => 'show',
-                                                                                :locale => 'en',
-                                                                                :branch => 'Sisyphus',
-                                                                                :group => 'Archiving',
-                                                                                :group2 => 'Compression')
+    it 'should route /en/Sisyphus/packages/Archiving/Compression to group#bygroup' do
+      expect({ get: '/en/Sisyphus/packages/Archiving/Compression' }).to route_to(controller: 'group',
+                                                                                 action: 'show',
+                                                                                 locale: 'en',
+                                                                                 branch: 'Sisyphus',
+                                                                                 group: 'Archiving',
+                                                                                 group2: 'Compression')
     end
 
-    it "should route /Sisyphus/packages/System/Configuration/Hardware to group#bygroup" do
-      expect({ :get => "/Sisyphus/packages/System/Configuration/Hardware" }).to route_to(:controller => 'group',
-                                                                                     :action => 'show',
-                                                                                     :branch => 'Sisyphus',
-                                                                                     :group => 'System',
-                                                                                     :group2 => 'Configuration',
-                                                                                     :group3 => 'Hardware')
+    it 'should route /Sisyphus/packages/System/Configuration/Hardware to group#bygroup' do
+      expect({ get: '/Sisyphus/packages/System/Configuration/Hardware' }).to route_to(controller: 'group',
+                                                                                      action: 'show',
+                                                                                      branch: 'Sisyphus',
+                                                                                      group: 'System',
+                                                                                      group2: 'Configuration',
+                                                                                      group3: 'Hardware')
     end
 
-    it "should route /en/Sisyphus/packages/System/Configuration/Hardware to group#bygroup" do
-      expect({ :get => "/en/Sisyphus/packages/System/Configuration/Hardware" }).to route_to(:controller => 'group',
-                                                                                        :action => 'show',
-                                                                                        :locale => 'en',
-                                                                                        :branch => 'Sisyphus',
-                                                                                        :group => 'System',
-                                                                                        :group2 => 'Configuration',
-                                                                                        :group3 => 'Hardware')
+    it 'should route /en/Sisyphus/packages/System/Configuration/Hardware to group#bygroup' do
+      expect({ get: '/en/Sisyphus/packages/System/Configuration/Hardware' }).to route_to(controller: 'group',
+                                                                                         action: 'show',
+                                                                                         locale: 'en',
+                                                                                         branch: 'Sisyphus',
+                                                                                         group: 'System',
+                                                                                         group2: 'Configuration',
+                                                                                         group3: 'Hardware')
     end
   end
 end
