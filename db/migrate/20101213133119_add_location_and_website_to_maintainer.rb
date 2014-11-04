88e73a5d4aa07d8661084166500df74e1d0c278f
Igor Zubkov <igor.zubkov@gmail.com>
2014-11-04 15:04:20 +0200

    Prefer single quote

--- db/migrate/20101213133119_add_location_and_website_to_maintainer.rb
+++ db/migrate/20101213133119_add_location_and_website_to_maintainer.rb
@@ -1,6 +1,6 @@
 class AddLocationAndWebsiteToMaintainer < ActiveRecord::Migration
   def change
-    add_column :maintainers, :website, :string, default: ""
-    add_column :maintainers, :location, :string, default: ""
+    add_column :maintainers, :website, :string, default: ''
+    add_column :maintainers, :location, :string, default: ''
   end
 end
