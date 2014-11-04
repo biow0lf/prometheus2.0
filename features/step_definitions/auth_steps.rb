93aadda5b93ca29c6f2f0fd3518e76e672cf7096
Igor Zubkov <igor.zubkov@gmail.com>
2014-11-04 19:07:37 +0200

    Cleanup

--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -9,14 +9,14 @@ end
 
 Given(/^I sign in as "(.*)"$/) do |email|
   password = 'password'
-  step %{I go to the sign in page}
-  step %{I fill in "Email" with "#{email}"}
-  step %{I fill in "Password" with "#{password}"}
-  step %{I press "Sign in"}
-  step %{I should see "Signed in successfully."}
+  step %(I go to the sign in page)
+  step %(I fill in "Email" with "#{ email }")
+  step %(I fill in "Password" with "#{ password }")
+  step %(I press "Sign in")
+  step %(I should see "Signed in successfully.")
 end
 
 Then(/^I sign out$/) do
-  step %{I go to the home page}
-  step %{I follow "Sign out"}
+  step %(I go to the home page)
+  step %(I follow "Sign out")
 end
