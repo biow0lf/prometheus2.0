b913a0c88c75e2f562ade7ebbb6da03a5f633d48
Igor Zubkov <igor.zubkov@gmail.com>
2014-11-13 00:24:59 +0200

    Enable lograge. Rails logs squasher for production env.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: 'packages.altlinux.org' }
+
+  # Enable lograge (rails logs squasher)
+  config.lograge.enabled = true
 end
