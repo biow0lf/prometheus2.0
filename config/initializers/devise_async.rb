bfbd95d625fe9cdb6166ea2ebc3babad12b0281a
Igor Zubkov <igor.zubkov@gmail.com>
2014-11-14 20:42:36 +0200

    Enable devise-async only in production

--- config/initializers/devise_async.rb
+++ config/initializers/devise_async.rb
@@ -1,2 +1,9 @@
 # Supported options: :resque, :sidekiq, :delayed_job, :queue_classic, :torquebox, :backburner
 Devise::Async.backend = :sidekiq
+
+if Rails.env.production?
+  Devise::Async.enabled = true
+else
+  Devise::Async.enabled = false
+end
+
