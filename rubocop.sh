7bd4c70d34b6d363aed67a7f1b598685f751b311
Igor Zubkov <igor.zubkov@gmail.com>
2015-03-04 22:05:15 +0200

    Update rubocop script

--- rubocop.sh
+++ rubocop.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
+# TODO:
+rubocop --only Lint/ElseLayout
+
 rubocop --only Lint/StringConversionInInterpolation
 rubocop --only Lint/DeprecatedClassMethods
 rubocop --only Style/ColonMethodCall
 rubocop --only Style/LeadingCommentSpace
 rubocop --only Style/SingleSpaceBeforeFirstArg
+rubocop --only Style/WhenThen
+rubocop --only Style/HashSyntax
+# rubocop --only Metrics/LineLength
