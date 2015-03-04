03b5c98c85f6cb70ba72f16423e8281d5154c353
Igor Zubkov <igor.zubkov@gmail.com>
2015-03-04 21:10:07 +0200

    Update and optimize sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -33,10 +33,12 @@ SitemapGenerator::Sitemap.create do
 
   ['en', 'ru', 'uk', 'br'].each do |locale|
     Branch.find_each do |branch|
-      branch.srpms.each do |srpm|
+      branch.srpms.find_each do |srpm|
         add srpm_path(locale, branch, srpm)
         add changelog_srpm_path(locale, branch, srpm)
         add spec_srpm_path(locale, branch, srpm)
+        add srpm_patches_path(locale, branch, srpm)
+        add srpm_sources_path(locale, branch, srpm)
         add get_srpm_path(locale, branch, srpm)
         add gear_srpm_path(locale, branch, srpm)
         add bugs_srpm_path(locale, srpm)
