10d4bdbfb1e97df23cf5701f5f6e4fdc6c929acb
Igor Zubkov <igor.zubkov@gmail.com>
2015-03-05 02:17:32 +0200

    Fix acl spec

--- spec/models/acl_spec.rb
+++ spec/models/acl_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
 describe Acl do
-  before(:each) do
-    branch = FactoryGirl.create(:branch)
+  before(:all) do
+    @branch = FactoryGirl.create(:branch)
 
     page = `cat spec/data/list.packages.sisyphus`
     url = 'http://git.altlinux.org/acl/list.packages.sisyphus'
     FakeWeb.register_uri(:get, url, response: page)
 
-    Acl.update_redis_cache(branch, url)
+    Acl.update_redis_cache(@branch, url)
   end
 
   it 'should import and update acls' do
-    expect(Redis.current.smembers('Sisyphus:rpm:acls').sort).to eq(%w(ldv at).sort)
-    expect(Redis.current.smembers('Sisyphus:maintainers:ldv')).to eq(%w(rpm))
-    expect(Redis.current.smembers('Sisyphus:maintainers:at')).to eq(%w(rpm))
+    expect(Redis.current.smembers("#{ @branch.name }:rpm:acls").sort).to eq(%w(ldv at).sort)
+    expect(Redis.current.smembers("#{ @branch.name }:maintainers:ldv")).to eq(%w(rpm))
+    expect(Redis.current.smembers("#{ @branch.name }:maintainers:at")).to eq(%w(rpm))
   end
 
   it 'should change "php_coder" to "php-coder"' do
-    expect(Redis.current.smembers('Sisyphus:chkrootkit:acls').sort).to eq(['php-coder', '@everybody'].sort)
+    expect(Redis.current.smembers("#{ @branch.name }:chkrootkit:acls").sort).to eq(%w(php-coder @everybody).sort)
   end
 
   it 'should change "psolntsev" to "p_solntsev"' do
-    expect(Redis.current.smembers('Sisyphus:docs-cppreference:acls').sort).to eq(['p_solntsev', '@qa'].sort)
+    expect(Redis.current.smembers("#{ @branch.name }:docs-cppreference:acls").sort).to eq(%w(p_solntsev @qa).sort)
   end
 
   it 'should change "@vim_plugins" to "@vim-plugins"' do
-    expect(Redis.current.smembers('Sisyphus:vim-plugin-CRefVIM:acls')).to eq(['@vim-plugins'])
+    expect(Redis.current.smembers("#{ @branch.name }:vim-plugin-CRefVIM:acls")).to eq(['@vim-plugins'])
   end
 end
