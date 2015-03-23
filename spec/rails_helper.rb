15e07bdff0791c841aaa56b3acedd66ff8dfc0be
Igor Zubkov <igor.zubkov@gmail.com>
2015-03-23 03:45:48 +0200

    Cleanup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,9 +25,6 @@ require 'rspec/rails'
 ActiveRecord::Migration.check_pending!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
