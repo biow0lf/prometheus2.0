7a174e0e7000840c688bf4bbac2c3ee0e8d163f6
Igor Zubkov <igor.zubkov@gmail.com>
2015-03-30 21:16:30 +0300

    Include devise helpers for testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,8 @@ RSpec.configure do |config|
 
   # Enable short FactoryGirl syntax
   config.include FactoryGirl::Syntax::Methods
+  # devise
+  config.include Devise::TestHelpers, type: :controller
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
