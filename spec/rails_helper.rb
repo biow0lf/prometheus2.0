626d69c467631b100375855630f07ed92b7d7de5
Igor Zubkov <igor.zubkov@gmail.com>
2015-04-12 02:36:58 +0300

    Extract devise stuff

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,9 +36,6 @@ RSpec.configure do |config|
   #   FactoryGirl.lint
   # end
 
-  # devise
-  config.include Devise::TestHelpers, type: :controller
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
