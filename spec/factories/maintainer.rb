bc2381256d2398ebc1d6773673e22ae0800491fe
Igor Zubkov <igor.zubkov@gmail.com>
2015-04-12 02:51:35 +0300

    Improve maintainer factory

--- spec/factories/maintainer.rb
+++ spec/factories/maintainer.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   factory :maintainer do
     name { Faker::Name.name }
-    login { Faker::Internet.user_name }
+    sequence :login do |n|
+      "#{ Faker::Internet.user_name }#{ n }"
+    end
     email { "#{ login }@altlinux.org" }
     time_zone 'UTC'
     jabber { "#{ login }@altlinux.org" }
