83f621e5da5cbd473fa16238522e056fdaa3ba2a
Igor Zubkov <igor.zubkov@gmail.com>
2015-04-20 15:35:57 +0300

    Improve factories

--- spec/factories/srpm.rb
+++ spec/factories/srpm.rb
@@ -1,13 +1,30 @@
 FactoryGirl.define do
   factory :srpm do
     branch { group.branch }
-    name { "#{ Faker::Lorem.word }" }
-    version { "#{ Faker::App.version }" }
+    name { Faker::Lorem.word }
+    version { Faker::App.version }
     sequence(:release) { |n| "alt#{ n }" }
-    epoch nil
+    epoch { Time.now.strftime('%Y%m%d').to_i }
     group
     groupname { group.name }
     filename { "#{ name }-#{ version }-#{ release }.src.rpm" }
-    md5 'f87ff0eaa4e16b202539738483cd54d1'
+    summary { Faker::Lorem.sentence }
+    license { Faker::Lorem.word.capitalize }
+    url { Faker::Internet.url }
+    description { Faker::Lorem.paragraph }
+    buildtime { Time.now }
+    vendor { Faker::Company.name }
+    distribution { Faker::Lorem.word }
+    repocop 'skip'
+    # TODO:
+    # t.string   "changelogname"
+    # changelogname ''
+    changelogtext { "- new version #{ version }" }
+    changelogtime { Time.now }
+    # TODO:
+    # t.integer  "builder_id"
+    # builder
+    md5 { SecureRandom.hex }
+    size { Faker::Number.number(9) }
   end
 end
