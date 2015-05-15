194d40c5ea1fdd5c8d109e05df370e2ef14eba91
Igor Zubkov <igor.zubkov@gmail.com>
2015-05-15 12:49:23 +0300

    Add more entities

---
+++ app/api/api/entities/provide.rb
@@ -0,0 +1,14 @@
+module API
+  module Entities
+    class Provide < Grape::Entity
+      expose :package_id
+      expose :name
+      expose :version
+      expose :release
+      expose :epoch
+      expose :flags
+      expose :created_at
+      expose :updated_at
+    end
+  end
+end
