6b92b83c3191beb071eb7b49aabc571fbd7acd83
Igor Zubkov <igor.zubkov@gmail.com>
2015-06-04 22:25:16 +0300

    Use find_by! and find_by

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -6,11 +6,11 @@ class GroupController < ApplicationController
 
   def show
     @branch = Branch.find_by!(name: params[:branch])
-    @group = @branch.groups.where(name: params[:group], parent_id: nil).first
+    @group = @branch.groups.find_by!(name: params[:group], parent_id: nil)
     if !params[:group2].nil?
-      @group = @branch.groups.where(name: params[:group2], parent_id: @group.id).first
+      @group = @branch.groups.find_by(name: params[:group2], parent_id: @group.id)
       if !params[:group3].nil?
-        @group = @branch.groups.where(name: params[:group3], parent_id: @group.id).first
+        @group = @branch.groups.find_by(name: params[:group3], parent_id: @group.id)
       end
     end
     @srpms = @group.srpms.order('LOWER(name)')
