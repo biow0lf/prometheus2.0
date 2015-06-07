e5308c359fd911c27d936094f5b5674a159f0de9
Igor Zubkov <igor.zubkov@gmail.com>
2015-06-07 15:52:06 +0300

    Use branch id if given

--- app/api/api/srpms.rb
+++ app/api/api/srpms.rb
@@ -6,7 +6,13 @@ module API
       optional :branch_id, type: Integer, desc: 'Branch id. Default: Sisyphus branch id.'
     end
     get ':srpm_name' do
-      present Srpm.find_by!(name: params[:srpm_name]), with: API::Entities::Srpm
+      if params[:branch_id]
+        branch = Branch.find_by!(id: params[:branch_id])
+      else
+        branch = Branch.find_by!(name: 'Sisyphus')
+      end
+      present branch.srpms.where(name: params[:srpm_name])
+         .includes(:branch).first!, with: API::Entities::Srpm
     end
   end
 end
