d3917d188de8d296099c93f917a7ba99867630c4
Igor Zubkov <igor.zubkov@gmail.com>
2015-06-25 00:47:46 +0300

    Rails 4.2 stuff

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Prometheus20::Application.config.session_store :cookie_store, key: '_prometheus2_0_session'
+Rails.application.config.session_store :cookie_store, key: '_prometheus2_0_session'
