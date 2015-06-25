01f3912cba4b739a8e2c6d98942509edea508139
Igor Zubkov <igor.zubkov@gmail.com>
2015-06-25 21:32:17 +0300

    Remove config.filter_parameters from config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -37,9 +37,6 @@ module Prometheus20
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = 'utf-8' # TODO: check this
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password] # TODO: check this
-
     # Enable the asset pipeline
     config.assets.enabled = true # TODO: check this
 
