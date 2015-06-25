b7925bf19ebac8dacfea67804ca37c1333f856de
Igor Zubkov <igor.zubkov@gmail.com>
2015-06-25 21:38:01 +0300

    Remove config.assets.enabled from config/application.rb (this is default value)

--- config/application.rb
+++ config/application.rb
@@ -34,9 +34,6 @@ module Prometheus20
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # Enable the asset pipeline
-    config.assets.enabled = true # TODO: check this
-
     config.generators do |g|
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl
