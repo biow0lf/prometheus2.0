a843367244e4225e2f5afe1320117cec70c471ec
Igor Zubkov <igor.zubkov@gmail.com>
2015-07-16 23:21:03 +0300

    Decorate more objects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
     @branch = Branch.find_by!(name: params[:branch])
     @branches = Branch.order('order_id')
     @top15 = Maintainer.top15(@branch)
-    @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").includes(:branch, :builder).order('srpms.created_at DESC').page(params[:page]).per(50)
+    @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").
+        includes(:branch, :builder).
+        order('srpms.created_at DESC').
+        page(params[:page]).per(50).decorate
   end
 
   def maintainers_list
