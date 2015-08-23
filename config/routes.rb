5ed5be441833cf02608b6be0fbd5b3ffb9d5003a
Igor Zubkov <igor.zubkov@gmail.com>
2015-08-23 13:56:30 +0300

    Fix routes for srpms api for packages with dots in name

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
     resources :bugs, only: :show
 
-    resources :srpms, only: :show
+    resources :srpms, id: /[^\/]+/, only: :show
   end
 
   scope '(:locale)', locale: SUPPORTED_LOCALES do
