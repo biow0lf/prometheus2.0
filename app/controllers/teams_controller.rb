243b4aa115841c9d9c3e01c5da15eafda8d9f697
Igor Zubkov <igor.zubkov@gmail.com>
2015-08-29 18:14:07 +0300

    Fix teams controller show

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,7 +6,7 @@ class TeamsController < ApplicationController
     @srpms_counter = @branch.srpms.where(name: Redis.current.smembers("#{ @branch.name }:maintainers:@#{ params[:id] }")).count
     @srpms = @branch.srpms.where(name: Redis.current.smembers("#{ @branch.name }:maintainers:@#{ params[:id] }")).
                            includes(:repocop_patch).
-                           select('repocop, srpms.name, srpms.version, srpms.release, buildtime, srpms.url, summary').
+                           select('repocop, srpms.name, srpms.epoch, srpms.version, srpms.release, buildtime, srpms.url, summary').
                            order('LOWER(srpms.name)').decorate
     @leader = Team.find_by_sql(["SELECT maintainers.login, maintainers.name
                                  FROM teams, maintainers, branches
