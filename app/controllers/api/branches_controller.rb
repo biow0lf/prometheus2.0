f30c942572afc30bb70618cd24a2df705977485f
Igor Zubkov <igor.zubkov@gmail.com>
2015-09-07 21:08:07 +0300

    Add missing branches#show

--- app/controllers/api/branches_controller.rb
+++ app/controllers/api/branches_controller.rb
@@ -10,9 +10,29 @@ module Api
       end
     end
 
+    swagger_path '/branches/{id}' do
+      operation :get do
+        key :description, 'Return branch information'
+        key :operationId, 'showBranch'
+        key :tags, ['branches']
+        parameter do
+          key :name, :id
+          key :in, :path
+          key :description, 'Branch ID. e.g. "1" for Sisyphus.'
+          key :required, true
+          key :type, :integer
+          key :format, :int64
+        end
+      end
+    end
+
     before_action :collection, only: :index
 
     private
+
+    def resource
+      @branch ||= Branch.find(params[:id])
+    end
 
     def collection
       @branches ||= Branch.all
