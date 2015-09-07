61a305ee0855efa6023be83daf248bc6af95e210
Igor Zubkov <igor.zubkov@gmail.com>
2015-09-07 21:32:40 +0300

    More specs

--- spec/routing/api/srpms_routing_spec.rb
+++ spec/routing/api/srpms_routing_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 describe Api::SrpmsController do
+  describe 'routing' do
+    it 'should route /api/srpms/:id to api/srpms#show' do
+      expect(get: 'api/srpms/openbox').to route_to(
+        controller: 'api/srpms',
+        action: 'show',
+        format: 'json',
+        id: 'openbox'
+      )
+    end
+
+    pending 'should route /api/srpms/:id.json to api/srpms#show' do
+      expect(get: 'api/srpms/openbox.json').to route_to(
+        controller: 'api/srpms',
+        action: 'show',
+        format: 'json',
+        id: 'openbox'
+      )
+    end
+  end
 end
-=begin
-
-                                       Prefix Verb   URI Pattern                                                        Controller#Action
-                                     api_docs GET    /api/docs(.:format)                                                api/docs#index {:format=>"json"}
-                                      api_bug GET    /api/bugs/:id(.:format)                                            api/bugs#show {:format=>"json"}
-                                     api_srpm GET    /api/srpms/:id(.:format)                                           api/srpms#show {:format=>"json", :id=>/[^\/]+/}
-
-=end
