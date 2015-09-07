7bc6bc7a60e5424b75d568e4d625beec7836edbe
Igor Zubkov <igor.zubkov@gmail.com>
2015-09-07 22:47:45 +0300

    Add missing ChangelogDecorator

---
+++ app/decorators/changelog_decorator.rb
@@ -0,0 +1,16 @@
+class ChangelogDecorator < Draper::Decorator
+  delegate_all
+
+  def as_json(*args)
+    {
+      id: id,
+      srpm_id: srpm_id,
+      changelogtime: changelogtime,
+      changelogname: changelogname,
+      changelogtext: changelogtext,
+      created_at: created_at.iso8601,
+      updated_at: updated_at.iso8601
+    }
+  end
+end
+
