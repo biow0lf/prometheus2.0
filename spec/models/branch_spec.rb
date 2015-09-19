7c400bca5bb552628c2dcd6973500c22dee74f0c
Igor Zubkov <igor.zubkov@gmail.com>
2015-09-19 12:34:04 +0300

    Cleanup

--- spec/models/branch_spec.rb
+++ spec/models/branch_spec.rb
@@ -3,20 +3,31 @@ require 'rails_helper'
 describe Branch do
   context 'Validation' do
     it { should validate_presence_of :name }
+
     it { should validate_presence_of :vendor }
   end
 
   context 'Associations' do
     it { should have_many :srpms }
+
     it { should have_many(:changelogs).through(:srpms) }
+
     it { should have_many(:packages).through(:srpms) }
+
     it { should have_many :groups }
+
     it { should have_many :teams }
+
     it { should have_many :mirrors }
+
     it { should have_many(:patches).through(:srpms) }
+
     it { should have_many(:sources).through(:srpms) }
+
     it { should have_many(:ftbfs).class_name('Ftbfs') }
+
     it { should have_many :repocops }
+
     it { should have_many :repocop_patches }
   end
 
