819e94d1e5b5416f858f9a1aa79679e954938f44
Igor Zubkov <igor.zubkov@gmail.com>
2015-10-04 16:34:52 +0300

    More specs

--- spec/decorators/maintainer_decorator_spec.rb
+++ spec/decorators/maintainer_decorator_spec.rb
@@ -1,6 +1,51 @@
 require 'rails_helper'
 
 describe MaintainerDecorator do
+  describe '#as_json' do
+    let(:created_at) { '2015-09-27T16:49:00Z' }
+
+    let(:updated_at) { '2015-09-28T16:59:34Z' }
+
+    let(:maintainer) do
+      stub_model Maintainer,
+                 id: 3,
+                 name: 'Igor Zubkov',
+                 email: 'icesik@altlinux.org',
+                 login: 'icesik',
+                 time_zone: 'Europe/Kiev',
+                 jabber: 'icesik@altlinux.org',
+                 info: 'Hello, World!',
+                 website: 'http://biow0lf.pp.ua',
+                 location: 'Ukraine',
+                 created_at: created_at,
+                 updated_at: updated_at
+    end
+
+    subject { maintainer.decorate.as_json }
+
+    its([:id]) { should eq(3) }
+
+    its([:name]) { should eq('Igor Zubkov') }
+
+    its([:email]) { should eq('icesik@altlinux.org') }
+
+    its([:login]) { should eq('icesik') }
+
+    its([:time_zone]) { should eq('Europe/Kiev') }
+
+    its([:jabber]) { should eq('icesik@altlinux.org') }
+
+    its([:info]) { should eq('Hello, World!') }
+
+    its([:website]) { should eq('http://biow0lf.pp.ua') }
+
+    its([:location]) { should eq('Ukraine') }
+
+    its([:created_at]) { should eq(created_at) }
+
+    its([:updated_at]) { should eq(updated_at) }
+  end
+
   describe '#avatar_url' do
     let(:maintainer) { stub_model Maintainer }
 
