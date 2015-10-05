7da7e31879557029beafc88b9dc438cd911ead9a
Igor Zubkov <igor.zubkov@gmail.com>
2015-10-05 21:45:50 +0300

    Cleanup

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -4,11 +4,11 @@ class Bug < ActiveRecord::Base
   class << self
     def opened_bugs_for(components)
       bug_statuses = %w(NEW ASSIGNED VERIFIED REOPENED)
-      where(component: components, bug_status: bug_statuses).order('bug_id DESC')
+      where(component: components, bug_status: bug_statuses).order(bug_id: :desc)
     end
 
     def all_bugs_for(components)
-      where(component: components).order('bug_id DESC')
+      where(component: components).order(bug_id: :desc)
     end
   end
 end
--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -2,27 +2,28 @@ require 'rails_helper'
 
 describe Bug do
   describe 'Validations' do
-    it { should validate_presence_of :bug_id }
+    it { should validate_presence_of(:bug_id) }
   end
 
   describe 'DB Indexes' do
-    it { should have_db_index :assigned_to }
-    it { should have_db_index :bug_status }
-    it { should have_db_index :product }
+    it { should have_db_index(:assigned_to) }
+    it { should have_db_index(:bug_status) }
+    it { should have_db_index(:product) }
   end
 
   describe '.opened_bugs_for' do
     before do
       #
+      # components = ['anything']
       # bug_statuses = %w(NEW ASSIGNED VERIFIED REOPENED)
-      # Bug.where(component: components, bug_status: bug_statuses).order('bug_id DESC')
+      # Bug.where(component: components, bug_status: bug_statuses).order(bug_id: :desc)
       #
       bug_statuses = %w(NEW ASSIGNED VERIFIED REOPENED)
       components = ['anything']
 
       expect(described_class).to receive(:where).with(component: components, bug_status: bug_statuses) do
         double.tap do |a|
-          expect(a).to receive(:order).with('bug_id DESC')
+          expect(a).to receive(:order).with(bug_id: :desc)
         end
       end
     end
@@ -33,12 +34,12 @@ describe Bug do
   describe '.all_bugs_for' do
     before do
       #
-      # Bug.where(component: components).order('bug_id DESC')
+      # Bug.where(component: components).order(bug_id: :desc)
       #
       components = ['anything']
       expect(described_class).to receive(:where).with(component: components) do
         double.tap do |a|
-          expect(a).to receive(:order).with('bug_id DESC')
+          expect(a).to receive(:order).with(bug_id: :desc)
         end
       end
     end
