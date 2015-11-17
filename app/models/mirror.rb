12befae031f41abd67055de2f2fddebec4125518
Igor Zubkov <igor.zubkov@gmail.com>
2015-11-17 18:53:16 +0200

    Cleanup

--- app/models/mirror.rb
+++ app/models/mirror.rb
@@ -2,8 +2,12 @@ class Mirror < ActiveRecord::Base
   belongs_to :branch
 
   validates :branch, presence: true
+
   validates :order_id, presence: true
+
   validates :name, presence: true
+
   validates :uri, presence: true
+
   validates :protocol, presence: true
 end
--- spec/models/mirror_spec.rb
+++ spec/models/mirror_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 describe Mirror do
   context 'Associations' do
-    it { should belong_to :branch }
+    it { should belong_to(:branch) }
   end
 
   context 'Validation' do
-    it { should validate_presence_of :branch }
-    it { should validate_presence_of :order_id }
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :uri }
-    it { should validate_presence_of :protocol }
+    it { should validate_presence_of(:branch) }
+
+    it { should validate_presence_of(:order_id) }
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_presence_of(:uri) }
+
+    it { should validate_presence_of(:protocol) }
   end
 
-  it { should have_db_index :branch_id }
+  it { should have_db_index(:branch_id) }
 end
