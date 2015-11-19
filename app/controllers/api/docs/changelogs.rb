aef8afbc98f908198243e20c7e2965a2b9da6e34
Igor Zubkov <igor.zubkov@gmail.com>
2015-11-19 18:05:24 +0200

    Add swagger schema for Changelog

--- app/controllers/api/docs/changelogs.rb
+++ app/controllers/api/docs/changelogs.rb
@@ -4,6 +4,45 @@ module Api
       # :nocov:
       include Swagger::Blocks
 
+      swagger_schema :Changelog do
+        key :required, [:id, :srpm_id, :changelogtime, :changelogname,
+                        :changelogext, :created_at, :updated_at]
+        property :id do
+          key :type, :integer
+          key :format, :int64
+          key :description, 'Changelog ID.'
+        end
+        property :srpm_id do
+          key :type, :integer
+          key :format, :int64
+          key :description, 'Srpm ID.'
+        end
+        property :changelogtime do
+          key :type, :string
+          key :description, 'Changelog time'
+        end
+        property :changelogname do
+          key :type, :string
+          key :format, :binary
+          key :description, 'Changelog name'
+        end
+        property :changelogext do
+          key :type, :string
+          key :format, :binary
+          key :description, 'Changelog text'
+        end
+        property :created_at do
+          key :type, :string
+          key :format, :'date-time'
+          key :description, 'Created at in ISO8601 format'
+        end
+        property :updated_at do
+          key :type, :string
+          key :format, :'date-time'
+          key :description, 'Updated at in ISO8601 format'
+        end
+      end
+
       swagger_path '/srpms/{name}/changelogs' do
         operation :get do
           key :description, 'Returns srpm changelogs info for given name'
