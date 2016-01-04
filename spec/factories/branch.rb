4d5d6f7bdc54293828c5f537ebf1c0abe77a6c21
Igor Zubkov <igor.zubkov@gmail.com>
2016-01-05 01:51:05 +0200

    Cleanup

--- spec/factories/branch.rb
+++ spec/factories/branch.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :branch do
-    name { "#{ Faker::Lorem.word.capitalize }" }
+    name { Faker::Lorem.word.capitalize }
     vendor { Faker::Company.name }
     sequence(:order_id, 0) { |n| "#{ n }" }
     # path '/Somewhere'
