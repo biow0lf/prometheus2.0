bf291096490fb96849c618a81f28384a84bccd50
Igor Zubkov <igor.zubkov@gmail.com>
2016-01-08 01:03:11 +0200

    Update deploy process

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,9 +25,15 @@ set :deploy_to, '/home/prometheusapp/www'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml',
+                                                 'config/newrelic.yml',
+                                                 'config/redis.conf',
+                                                 'config/redis.yml',
+                                                 'config/initializers/devise.rb',
+                                                 'config/initializers/secret_token.rb')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -52,3 +58,11 @@ namespace :deploy do
 end
 
 # after :finishing, 'whenever:update_crontab'
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
+
