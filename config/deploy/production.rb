a60eab0e86c21d9cc637e2393c3d310af4304ffe
Igor Zubkov <igor.zubkov@gmail.com>
2016-01-08 01:12:24 +0200

    Update deploy process

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'prometheus.altlinux.org', user: 'prometheusapp'
+server 'prometheus.altlinux.org', user: 'prometheusapp', roles: %w{app db web}
 
 # role-based syntax
 # ==================
