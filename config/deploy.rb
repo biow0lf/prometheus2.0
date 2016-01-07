037b17480f859606b8e440bf34479ab41a8fc455
Igor Zubkov <igor.zubkov@gmail.com>
2016-01-07 20:19:36 +0200

    Configure capistrano-rbenv

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ lock '3.4.0'
 set :application, 'my_app_name'
 set :repo_url, 'git@example.com:me/my_repo.git'
 
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
