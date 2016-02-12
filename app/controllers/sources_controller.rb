56dcbc6ece1c9dd55e9f4d63b5bc2619702f8a75
Igor Zubkov <igor.zubkov@gmail.com>
2016-02-12 17:07:02 +0200

    Cleanup

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -4,15 +4,4 @@ class SourcesController < ApplicationController
     @srpm = @branch.srpms.where(name: params[:srpm_id]).includes(:sources).first!
     @allsrpms = Srpm.where(name: params[:srpm_id]).includes(:branch).order('branches.order_id').decorate
   end
-
-  def download
-    @branch = Branch.find_by!(name: params[:branch])
-    @srpm = @branch.srpms.find_by!(name: params[:srpm_id])
-    @source = @srpm.sources.find_by!(filename: params[:id])
-    if @source && @source.source?
-      send_data @source.source, disposition: 'attachment', filename: @source.filename
-    elsif @source && !@source.source?
-      render layout: false
-    end
-  end
 end
