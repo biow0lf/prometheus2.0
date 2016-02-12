0089ae8e9d69ac690391b6ceaf0074bbca77e5e7
Igor Zubkov <igor.zubkov@gmail.com>
2016-02-12 16:59:16 +0200

    Fix patch_download#show

--- app/controllers/patch_download_controller.rb
+++ app/controllers/patch_download_controller.rb
@@ -3,9 +3,9 @@ class PatchDownloadController < ApplicationController
     @branch = Branch.find_by!(name: params[:branch])
     @srpm = @branch.srpms.find_by!(name: params[:srpm_id])
     @patch = @srpm.patches.find_by!(filename: params[:patch_id])
-    if @patch && @patch.patch?
+    if @patch.patch?
       send_data @patch.patch, disposition: 'attachment', filename: @patch.filename
-    elsif @patch && !@patch.patch?
+    elsif !@patch.patch?
       render layout: false
     end
   end
