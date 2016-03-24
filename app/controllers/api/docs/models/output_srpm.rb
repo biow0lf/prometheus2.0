f4b98d44078ed3e3f8525ffc64dbd4f76e3b29e5
Igor Zubkov <igor.zubkov@gmail.com>
2016-03-24 16:30:50 +0200

    Extract swagger schema

---
+++ app/controllers/api/docs/models/output_srpm.rb
@@ -0,0 +1,124 @@
+module Api
+  module Docs
+    module Models
+      class OutputSrpm
+        # :nocov:
+        include Swagger::Blocks
+
+        swagger_schema :OutputSrpm do
+          key :required, [:id, :branch_id, :branch, :name, :version, :release,
+                          :epoch, :summary, :license, :group, :group_id, :url,
+                          :description, :buildtime, :filename, :vendor,
+                          :distribution, :md5, :builder_id, :size, :repocop,
+                          :created_at, :updated_at]
+          property :id do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Srpm ID.'
+          end
+          property :branch_id do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Branch ID.'
+          end
+          property :branch do
+            key :type, :string
+            key :description, 'Branch name'
+          end
+          property :name do
+            key :type, :string
+            key :description, 'Srpm name'
+          end
+          property :version do
+            key :type, :string
+            key :description, 'Srpm version'
+          end
+          property :release do
+            key :type, :string
+            key :description, 'Srpm release'
+          end
+          property :epoch do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Srpm epoch'
+          end
+          property :summary do
+            key :type, :string
+            key :description, 'Srpm summary'
+          end
+          property :license do
+            key :type, :string
+            key :description, 'Srpm license'
+          end
+          property :group do
+            key :type, :string
+            key :description, 'Srpm group name'
+          end
+          property :group_id do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Srpm group id'
+          end
+          property :url do
+            key :type, :string
+            key :description, 'Srpm url'
+          end
+          property :description do
+            key :type, :string
+            key :description, 'Srpm description'
+          end
+          property :buildtime do
+            key :type, :string
+            key :format, :'date-time'
+            key :description, 'Buildtime in ISO8601 format'
+          end
+          property :filename do
+            key :type, :string
+            key :description, 'Srpm filename'
+          end
+          property :vendor do
+            key :type, :string
+            key :description, 'Srpm Vendor'
+          end
+          property :distribution do
+            key :type, :string
+            key :description, 'Srpm Distribution'
+          end
+          # TODO:
+          # changelogname: changelogname,
+          # changelogtext: changelogtext,
+          # changelogtime: changelogtime,
+          property :md5 do
+            key :type, :string
+            key :description, 'Srpm md5 sum'
+          end
+          property :builder_id do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Srpm builder id (maintainer id)'
+          end
+          property :size do
+            key :type, :integer
+            key :format, :int64
+            key :description, 'Srpm size'
+          end
+          property :repocop do
+            key :type, :string
+            key :description, 'Repocop status'
+          end
+          property :created_at do
+            key :type, :string
+            key :format, :'date-time'
+            key :description, 'Created at in ISO8601 format'
+          end
+          property :updated_at do
+            key :type, :string
+            key :format, :'date-time'
+            key :description, 'Updated at in ISO8601 format'
+          end
+        end
+        # :nocov:
+      end
+    end
+  end
+end
--- app/controllers/api/docs/srpms.rb
+++ app/controllers/api/docs/srpms.rb
@@ -4,119 +4,6 @@ module Api
       # :nocov:
       include Swagger::Blocks
 
-      swagger_schema :Srpm do
-        key :required, [:id, :branch_id, :branch, :name, :version, :release,
-                        :epoch, :summary, :license, :group, :group_id, :url,
-                        :description, :buildtime, :filename, :vendor,
-                        :distribution, :md5, :builder_id, :size, :repocop,
-                        :created_at, :updated_at]
-        property :id do
-          key :type, :integer
-          key :format, :int64
-          key :description, 'Srpm ID.'
-        end
-        property :branch_id do
-          key :type, :integer
-          key :format, :int64
-          key :description, 'Branch ID.'
-        end
-        property :branch do
-          key :type, :string
-          key :description, 'Branch name'
-        end
-        property :name do
-          key :type, :string
-          key :description, 'Srpm name'
-        end
-        property :version do
-          key :type, :string
-          key :description, 'Srpm version'
-        end
-        property :release do
-          key :type, :string
-          key :description, 'Srpm release'
-        end
-        property :epoch do
-          key :type, :integer
-          key :format, :int64
-          key :description, 'Srpm epoch'
-        end
-        property :summary do
-          key :type, :string
-          key :description, 'Srpm summary'
-        end
-        property :license do
-          key :type, :string
-          key :description, 'Srpm license'
-        end
-        property :group do
-          key :type, :string
-          key :description, 'Srpm group name'
-        end
-        property :group_id do
-          key :type, :integer
-          key :format, :int64
-          key :description, 'Srpm group id'
-        end
-        property :url do
-          key :type, :string
-          key :description, 'Srpm url'
-        end
-        property :description do
-          key :type, :string
-          key :description, 'Srpm description'
-        end
-        property :buildtime do
-          key :type, :string
-          key :format, :'date-time'
-          key :description, 'Buildtime in ISO8601 format'
-        end
-        property :filename do
-          key :type, :string
-          key :description, 'Srpm filename'
-        end
-        property :vendor do
-          key :type, :string
-          key :description, 'Srpm Vendor'
-        end
-        property :distribution do
-          key :type, :string
-          key :description, 'Srpm Distribution'
-        end
-        # TODO:
-        # changelogname: changelogname,
-        # changelogtext: changelogtext,
-        # changelogtime: changelogtime,
-        property :md5 do
-          key :type, :string
-          key :description, 'Srpm md5 sum'
-        end
-        property :builder_id do
-          key :type, :integer
-          key :format, :int64
-          key :description, 'Srpm builder id (maintainer id)'
-        end
-        property :size do
-          key :type, :integer
-          key :format, :int64
-          key :description, 'Srpm size'
-        end
-        property :repocop do
-          key :type, :string
-          key :description, 'Repocop status'
-        end
-        property :created_at do
-          key :type, :string
-          key :format, :'date-time'
-          key :description, 'Created at in ISO8601 format'
-        end
-        property :updated_at do
-          key :type, :string
-          key :format, :'date-time'
-          key :description, 'Updated at in ISO8601 format'
-        end
-      end
-
       swagger_path '/srpms/{name}' do
         operation :get do
           key :description, 'Returns srpm info for given name'
@@ -139,7 +26,7 @@ module Api
           response 200 do
             key :description, 'Response with srpm.'
             schema do
-              key :'$ref', :Srpm
+              key :'$ref', :OutputSrpm
             end
           end
           response 404 do
@@ -147,7 +34,6 @@ module Api
           end
         end
       end
-
       # :nocov:
     end
   end
