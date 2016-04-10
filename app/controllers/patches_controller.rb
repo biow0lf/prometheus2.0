77f20157fadadd9bb0a3fdad23472d94e5546b89
Igor Zubkov <igor.zubkov@gmail.com>
2016-04-10 21:37:28 +0300

    Fix 500 error

--- app/controllers/patches_controller.rb
+++ app/controllers/patches_controller.rb
@@ -9,6 +9,7 @@ class PatchesController < ApplicationController
     @branch = Branch.find_by!(name: params[:branch])
     @srpm = @branch.srpms.find_by!(name: params[:srpm_id])
     @patch = @srpm.patches.find_by!(filename: params[:id])
+    raise ActiveRecord::RecordNotFound unless @patch.patch
     @html_data = Rouge::Formatters::HTML.new(css_class: 'highlight', line_numbers: true, inline_theme: 'github').format(Rouge::Lexers::Diff.new.lex(@patch.patch))
   end
 end
