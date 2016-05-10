f9fa5a4f7cf720c79c9ea0d9894cf3029f62d36d
Igor Zubkov <igor.zubkov@gmail.com>
2016-05-10 17:24:17 +0300

    Sync with rails 5.0.0.rc1

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,14 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails'
 # Pick the frameworks you want:
 require 'active_model/railtie'
-# require 'active_job/railtie'
+require 'active_job/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
+require 'action_cable/engine'
 require 'sprockets/railtie'
 # require 'rails/test_unit/railtie'
 
