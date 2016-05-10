0fbd8f0bc58b9ab550c791bf8dc9fc39ab9058b2
Igor Zubkov <igor.zubkov@gmail.com>
2016-05-10 17:29:09 +0300

    Fix Rack::Cors

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,7 @@ module Prometheus20
       config.middleware.use Rack::ForceDomain, 'packages.altlinux.org'
     end
 
-    config.middleware.insert_before 0, 'Rack::Cors' do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
