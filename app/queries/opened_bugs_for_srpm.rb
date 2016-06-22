28397c733a390c58a285eb4196fec9c771d46c1d
Igor Zubkov <igor.zubkov@gmail.com>
2016-06-22 12:35:11 +0300

    Add OpenedBugsForSrpm

---
+++ app/queries/opened_bugs_for_srpm.rb
@@ -0,0 +1,13 @@
+class OpenedBugsForSrpm < Rectify::Query
+  BUG_STATUSES = %w(NEW ASSIGNED VERIFIED REOPENED).freeze
+
+  attr_reader :scope
+
+  def initialize(srpm)
+    @scope = AllBugsForSrpm.new(srpm)
+  end
+
+  def query
+    scope.query.where(bug_status: BUG_STATUSES).order(bug_id: :desc)
+  end
+end
