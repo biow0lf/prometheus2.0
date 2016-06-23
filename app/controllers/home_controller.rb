44b87fdbcc506a3f0687d690afc8c7661edce07b
Igor Zubkov <igor.zubkov@gmail.com>
2016-06-23 13:00:10 +0300

    Cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @branch = Branch.find_by!(name: params[:branch])
     @branches = Branch.order('order_id')
     @top15 = Maintainer.top15(@branch)
-    @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").includes(:branch, :builder).order('srpms.created_at DESC').page(params[:page]).per(40).decorate
+    @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").includes(:builder).order('srpms.created_at DESC').page(params[:page]).per(40).decorate
   end
 
   def maintainers_list
