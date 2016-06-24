63ad610aaedf6ae23ed48fb924446c37d57e561e
Igor Zubkov <igor.zubkov@gmail.com>
2016-06-24 19:11:32 +0300

    Add spec

--- spec/queries/all_bugs_for_maintainer_spec.rb
+++ spec/queries/all_bugs_for_maintainer_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 describe AllBugsForMaintainer do
+  describe '#initialize' do
+    let(:branch) { double }
+
+    let(:maintainer) { double }
+
+    subject { described_class.new(branch, maintainer) }
+
+    its(:branch) { should eq(branch) }
+
+    its(:maintainer) { should eq(maintainer) }
+  end
+
   # TODO: write
 end
