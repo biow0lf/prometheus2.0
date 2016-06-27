665f11df44a16b965dae969891b1dd530999a266
Igor Zubkov <igor.zubkov@gmail.com>
2016-06-27 16:51:02 +0300

    Add example for debuging used attributes

--- app/controllers/srpm_all_bugs_controller.rb
+++ app/controllers/srpm_all_bugs_controller.rb
@@ -1,8 +1,24 @@
 class SrpmAllBugsController < ApplicationController
+  # after_action :print_accessed_fields
+
   def index
     @branch = Branch.find_by!(name: params[:branch])
     @srpm = @branch.srpms.find_by!(name: params[:id])
     @all_bugs = AllBugsForSrpm.new(@srpm).decorate
     @opened_bugs = OpenedBugsForSrpm.new(@srpm).decorate
   end
+
+  # private
+  #
+  # def print_accessed_fields
+  #   # p @users.first.accessed_fields
+  #
+  #   p @branch.accessed_fields
+  #
+  #   p @srpm.accessed_fields
+  #
+  #   p @all_bugs.first.accessed_fields
+  #
+  #   p @opened_bugs.first.accessed_fields
+  # end
 end
