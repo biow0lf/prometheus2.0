780eccd047d9fbc5715703ec2cd94720e84cbc18
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-01 11:43:28 +0300

    Sync with rails 5.0.0 release

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,12 +42,15 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # devise
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
