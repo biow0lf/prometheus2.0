880d39ebcaf05656ff86e757fd0ce8bcbb3cc5ed
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-04 21:39:13 +0300

    Remove branch observer

--- app/observers/branch_observer.rb
+++
@@ -1,9 +0,0 @@
-class BranchObserver < ActiveRecord::Observer
-  def after_create(branch)
-    branch.counter.value = 0
-  end
-
-  def after_destroy(branch)
-    Redis.current.del("branch:#{ branch.id }:counter")
-  end
-end
