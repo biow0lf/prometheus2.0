660a74740ceca1b912a0a6cf73722e94d918bb24
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-26 15:10:40 +0300

    Cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,9 +39,6 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml',
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
 # Default value for keep_releases is 5
 set :keep_releases, 3
 
