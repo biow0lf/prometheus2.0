5ced26bcfe6478a138a0c10b67ad1e89863e9d10
Igor Zubkov <igor.zubkov@gmail.com>
2016-07-26 23:24:27 +0300

    Configure puma

--- config/puma.rb
+++ config/puma.rb
@@ -20,8 +20,8 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+workers ENV.fetch('WEB_CONCURRENCY') { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -29,8 +29,8 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # process behavior so workers use less memory. If you use this option
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
-#
-# preload_app!
+
+preload_app!
 
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
@@ -38,10 +38,14 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 # option you will want to use this block to reconnect to any threads
 # or connections that may have been created at application boot, Ruby
 # cannot share connections between processes.
-#
-# on_worker_boot do
-#   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-# end
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
+end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
