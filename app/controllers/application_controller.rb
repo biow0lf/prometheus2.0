c62b69a787a0cbc5d630b77f82612070edf4b831
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-02 13:39:43 +0300

    Disable redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :redirect_to_localized
+  # before_action :redirect_to_localized
   before_action :set_default_locale
   before_action :set_default_branch
   before_action :authorizer_for_profiler
