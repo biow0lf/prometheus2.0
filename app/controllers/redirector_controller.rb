f107caa299560e757682a15c09288697b2d45970
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-02 15:37:31 +0300

    Cleanup

--- app/controllers/redirector_controller.rb
+++ app/controllers/redirector_controller.rb
@@ -1,9 +1,9 @@
 class RedirectorController < ApplicationController
   def index
-    @branch = Branch.find_by!(name: 'Sisyphus')
-    @package = @branch.packages.where(name: params[:name]).includes(:srpm).first
-    if @package
-      redirect_to srpm_path('Sisyphus', @package.srpm), status: 302
+    branch = Branch.find_by!(name: 'Sisyphus')
+    package = branch.packages.includes(:srpm).find_by(name: params[:name])
+    if package
+      redirect_to srpm_path('Sisyphus', package.srpm), status: 302
     else
       redirect_to srpm_path('Sisyphus', params[:name]), status: 302
     end
