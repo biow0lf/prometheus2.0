b8194f74a5b3ba2be90bd9c2e5ac546fdb3ba07a
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-04 12:30:25 +0300

    Cleanup

--- app/controllers/srpm_repocops_controller.rb
+++ app/controllers/srpm_repocops_controller.rb
@@ -4,6 +4,5 @@ class SrpmRepocopsController < ApplicationController
     @srpm = @branch.srpms.find_by!(name: params[:id])
     @all_bugs = AllBugsForSrpm.new(@srpm).decorate
     @opened_bugs = OpenedBugsForSrpm.new(@srpm).decorate
-    @repocops = @srpm.repocops.page(params[:page]).per(1_000)
   end
 end
