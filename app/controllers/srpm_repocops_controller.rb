668c73b308f3461393f9830f01ff506cf87db435
Igor Zubkov <igor.zubkov@gmail.com>
2016-08-04 11:35:01 +0300

    Paginate repocops

--- app/controllers/srpm_repocops_controller.rb
+++ app/controllers/srpm_repocops_controller.rb
@@ -4,5 +4,6 @@ class SrpmRepocopsController < ApplicationController
     @srpm = @branch.srpms.find_by!(name: params[:id])
     @all_bugs = AllBugsForSrpm.new(@srpm).decorate
     @opened_bugs = OpenedBugsForSrpm.new(@srpm).decorate
+    @repocops = @srpm.repocops.page(params[:page]).per(1_000)
   end
 end
