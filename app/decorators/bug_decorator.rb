d067aaa141c7720df7d8a77989861d0eb77690ac
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2016-08-07 09:54:36 +0300

    updated bug decorator

--- app/decorators/bug_decorator.rb
+++ app/decorators/bug_decorator.rb
@@ -2,19 +2,8 @@ class BugDecorator < Draper::Decorator
   delegate_all
 
   def as_json(*args)
-    {
-      bug_id: bug_id,
-      bug_status: bug_status,
-      resolution: resolution,
-      bug_severity: bug_severity,
-      product: product,
-      component: component,
-      assigned_to: assigned_to,
-      reporter: reporter,
-      short_desc: short_desc,
-      created_at: created_at.iso8601,
-      updated_at: updated_at.iso8601
-    }
+    super only: [:bug_id, :bug_status, :resolution, :bug_severity, :product, :component, :assigned_to, :reporter, :short_desc],
+          methods: [:updated_at, :created_at]
   end
 
   def bugzilla_url
@@ -23,5 +12,14 @@ class BugDecorator < Draper::Decorator
 
   def link_to_bugzilla
     h.link_to(bug_id, bugzilla_url, class: 'news')
+  end
+
+  private
+  def created_at
+    model.created_at.iso8601
+  end
+
+  def updated_at
+    model.updated_at.iso8601
   end
 end
