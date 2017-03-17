1eadde1c9dc193e19cd752059226bbe4016d3739
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-17 23:18:21 +0200

    Add TODO notes

--- config/application.rb
+++ config/application.rb
@@ -22,14 +22,17 @@ module Prometheus20
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # TODO: extract to initializer
     config.generators do |g|
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl
     end
 
+    # TODO: extract to initializer
     # fallback for empty translations
     config.i18n.fallbacks = true
 
+    # TODO: extract to initializer
     if Rails.env.production?
       config.middleware.use ExceptionNotification::Rack,
         email: {
