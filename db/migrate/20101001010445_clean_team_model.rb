a6539575724f6cef64ca9203e6a5f77a89a5f093
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-19 23:49:07 +0200

    Fix migration

--- db/migrate/20101001010445_clean_team_model.rb
+++ db/migrate/20101001010445_clean_team_model.rb
@@ -1,6 +1,6 @@
 class CleanTeamModel < ActiveRecord::Migration[4.2]
   def change
-    remove_column :teams, :branch
-    remove_column :teams, :vendor
+    remove_column :teams, :branch, :string
+    remove_column :teams, :vendor, :string
   end
 end
