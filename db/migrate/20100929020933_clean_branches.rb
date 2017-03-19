1425fa2dd85417ea2776096fc31a006fd6e8963a
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-19 22:49:01 +0200

    Fix migration

--- db/migrate/20100929020933_clean_branches.rb
+++ db/migrate/20100929020933_clean_branches.rb
@@ -1,8 +1,8 @@
 class CleanBranches < ActiveRecord::Migration[4.2]
   def change
-    remove_column :branches, :binary_x86_path
-    remove_column :branches, :noarch_path
-    remove_column :branches, :binary_x86_64_path
-    remove_column :branches, :acls_groups_url
+    remove_column :branches, :binary_x86_path, :string
+    remove_column :branches, :noarch_path, :string
+    remove_column :branches, :binary_x86_64_path, :string
+    remove_column :branches, :acls_groups_url, :string
   end
 end
