a56a09cb9862637c055de908c348182ef79d1e1e
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 00:26:52 +0200

    Fix migration

--- db/migrate/20101004095233_add_srpm_id_to_gitrepo_model.rb
+++ db/migrate/20101004095233_add_srpm_id_to_gitrepo_model.rb
@@ -2,6 +2,6 @@ class AddSrpmIdToGitrepoModel < ActiveRecord::Migration[4.2]
   def change
     add_column :gitrepos, :srpm_id, :integer
     add_index :gitrepos, :srpm_id
-    remove_column :gitrepos, :login
+    remove_column :gitrepos, :login, :string
   end
 end
