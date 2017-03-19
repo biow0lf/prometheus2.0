ae7bdf3f8e8278d8dd594cf55dd236f46482fd60
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-19 23:39:39 +0200

    Fix migration

--- db/migrate/20100930232318_clean_srpm_model.rb
+++ db/migrate/20100930232318_clean_srpm_model.rb
@@ -1,6 +1,6 @@
 class CleanSrpmModel < ActiveRecord::Migration[4.2]
   def change
-    remove_column :srpms, :branch
-    remove_column :srpms, :vendor
+    remove_column :srpms, :branch, :string
+    remove_column :srpms, :vendor, :string
   end
 end
