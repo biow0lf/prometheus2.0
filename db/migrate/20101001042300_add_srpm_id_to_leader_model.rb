044f6a235721392854fbc6cb32027d6c28cbe2c1
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-19 23:55:58 +0200

    Fix migration

--- db/migrate/20101001042300_add_srpm_id_to_leader_model.rb
+++ db/migrate/20101001042300_add_srpm_id_to_leader_model.rb
@@ -2,7 +2,7 @@ class AddSrpmIdToLeaderModel < ActiveRecord::Migration[4.2]
   def change
     add_column :leaders, :srpm_id, :integer
     add_index :leaders, :srpm_id
-    remove_column :leaders, :package
+    remove_column :leaders, :package, :string
     add_column :leaders, :packager_id, :integer
     add_index :leaders, :packager_id
   end
