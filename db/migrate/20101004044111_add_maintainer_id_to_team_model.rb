0cbb3024adfacb5adcf3d65400b39e44ed4c4a37
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 00:23:32 +0200

    Fix migration

--- db/migrate/20101004044111_add_maintainer_id_to_team_model.rb
+++ db/migrate/20101004044111_add_maintainer_id_to_team_model.rb
@@ -2,6 +2,6 @@ class AddMaintainerIdToTeamModel < ActiveRecord::Migration[4.2]
   def change
     add_column :teams, :maintainer_id, :integer
     add_index :teams, :maintainer_id
-    remove_column :teams, :login
+    remove_column :teams, :login, :string
   end
 end
