bfa7e569d137ad932f9d2a59b7b4774a98c6ab43
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 00:07:19 +0200

    Fix migration

--- db/migrate/20101004024333_add_srpm_id_to_acl_model.rb
+++ db/migrate/20101004024333_add_srpm_id_to_acl_model.rb
@@ -3,6 +3,6 @@ class AddSrpmIdToAclModel < ActiveRecord::Migration[4.2]
     add_column :acls, :srpm_id, :integer
     add_index :acls, :srpm_id
     remove_index :acls, :package
-    remove_column :acls, :package
+    remove_column :acls, :package, :string
   end
 end
