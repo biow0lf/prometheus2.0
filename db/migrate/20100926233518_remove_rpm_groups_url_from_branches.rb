7c28491a76c8f41a5f21bf2b95fd979dd879cc68
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-19 21:46:07 +0200

    Fix migration

--- db/migrate/20100926233518_remove_rpm_groups_url_from_branches.rb
+++ db/migrate/20100926233518_remove_rpm_groups_url_from_branches.rb
@@ -1,5 +1,5 @@
 class RemoveRpmGroupsUrlFromBranches < ActiveRecord::Migration[4.2]
   def change
-    remove_column :branches, :rpm_groups_url
+    remove_column :branches, :rpm_groups_url, :string
   end
 end
