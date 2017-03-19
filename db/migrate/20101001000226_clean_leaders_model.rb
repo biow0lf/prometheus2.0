146dfe1c6ee5e09cb64b69ad446bbb858d718885
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-19 23:47:36 +0200

    Fix migration

--- db/migrate/20101001000226_clean_leaders_model.rb
+++ db/migrate/20101001000226_clean_leaders_model.rb
@@ -1,6 +1,6 @@
 class CleanLeadersModel < ActiveRecord::Migration[4.2]
   def change
-    remove_column :leaders, :branch
-    remove_column :leaders, :vendor
+    remove_column :leaders, :branch, :string
+    remove_column :leaders, :vendor, :string
   end
 end
