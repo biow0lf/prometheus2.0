736bd0019055fa22e14e85f0996878bff6156942
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 23:16:04 +0200

    Add branch factory

--- spec/factories/branch.rb
+++ spec/factories/branch.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
+  sequence :name do |n|
+    "Branch #{ n }"
+  end
+
   factory :branch do
-    name { Faker::Lorem.word.capitalize }
-    vendor { Faker::Company.name }
-    sequence(:order_id, 0) { |n| "#{ n }" }
-    # path '/Somewhere'
-    # srpm_dir ['/ALT/Sisyphus/files/SRPMS/']
-    # rpm_dir ['/ALT/Sisyphus/files/i586/RPMS/*.i586.rpm',
-    #           '/ALT/Sisyphus/files/noarch/RPMS/*.noarch.rpm',
-    #           '/ALT/Sisyphus/files/x86_64/RPMS/*.x86_64.rpm']
+    name
+    vendor 'Vendor'
+    order_id 0
+    path '/Anything'
   end
 end
