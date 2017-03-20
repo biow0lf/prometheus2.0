ce00e9cbbe5b56bf35450446ede69f715e1f4527
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 12:56:35 +0200

    Fix migration

--- db/migrate/20110320150232_update_conflicts_model.rb
+++ db/migrate/20110320150232_update_conflicts_model.rb
@@ -1,6 +1,6 @@
 class UpdateConflictsModel < ActiveRecord::Migration[4.2]
   def change
-    remove_column :conflicts, :type
+    remove_column :conflicts, :type, :string
     add_column :conflicts, :epoch, :string
     add_column :conflicts, :flags, :integer
   end
