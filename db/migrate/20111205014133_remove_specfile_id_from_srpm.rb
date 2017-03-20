ad6b5d4d58a4c55b289f694fbf495bb674fdc4b6
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 13:04:17 +0200

    Fix migration

--- db/migrate/20111205014133_remove_specfile_id_from_srpm.rb
+++ db/migrate/20111205014133_remove_specfile_id_from_srpm.rb
@@ -1,5 +1,5 @@
 class RemoveSpecfileIdFromSrpm < ActiveRecord::Migration[4.2]
   def change
-    remove_column :srpms, :specfile_id
+    remove_column :srpms, :specfile_id, :integer, default: nil
   end
 end
