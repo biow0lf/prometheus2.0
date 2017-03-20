4e02250f7a6ea04979da2adc159518f88d6de8ab
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 13:02:16 +0200

    Fix migration

--- db/migrate/20110330164031_remove_password_salt_from_user_model.rb
+++ db/migrate/20110330164031_remove_password_salt_from_user_model.rb
@@ -1,5 +1,5 @@
 class RemovePasswordSaltFromUserModel < ActiveRecord::Migration[4.2]
   def change
-    remove_column :users, :password_salt
+    remove_column :users, :password_salt, :string
   end
 end
