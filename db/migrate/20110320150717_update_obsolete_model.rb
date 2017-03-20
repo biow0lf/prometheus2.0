f6edc59f4299b1af5a66e567a24b28508204b231
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-20 12:59:01 +0200

    Fix migration

--- db/migrate/20110320150717_update_obsolete_model.rb
+++ db/migrate/20110320150717_update_obsolete_model.rb
@@ -1,6 +1,6 @@
 class UpdateObsoleteModel < ActiveRecord::Migration[4.2]
   def change
-    remove_column :obsoletes, :type
+    remove_column :obsoletes, :type, :string
     add_column :obsoletes, :epoch, :string
     add_column :obsoletes, :flags, :integer
   end
