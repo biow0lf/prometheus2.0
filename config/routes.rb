8d9a1d41e719cfa106f5d101f7c31f0c72ffa19f
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-22 13:51:09 +0200

    Mount pghero

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticate :user, -> (user) { user.admin? } do
+    mount PgHero::Engine, at: 'pghero'
+  end
+
   namespace :api, defaults: { format: 'json' } do
     resources :docs, only: :index
 
