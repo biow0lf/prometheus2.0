55ef425ce34883d48c9385bc43f649826895b823
Igor Zubkov <igor.zubkov@gmail.com>
2017-03-22 17:08:38 +0200

    Update source download controller

--- app/controllers/source_download_controller.rb
+++ app/controllers/source_download_controller.rb
@@ -3,8 +3,8 @@ class SourceDownloadController < ApplicationController
     @branch = Branch.find_by!(name: params[:branch])
     @srpm = @branch.srpms.find_by!(name: params[:srpm_id])
     @source = @srpm.sources.find_by!(filename: params[:source_id])
-    if @source.source?
-      send_data @source.source, disposition: 'attachment', filename: @source.filename
+    if @source.content?
+      send_data @source.content, disposition: 'attachment', filename: @source.filename
     else
       raise ActiveRecord::RecordNotFound
     end
