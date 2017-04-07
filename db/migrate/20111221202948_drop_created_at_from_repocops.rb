7bd7f583513c0bf4b95c73faa4176545058628d6
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-07 22:16:26 +0300

    Fix rubocop warnings

--- db/migrate/20111221202948_drop_created_at_from_repocops.rb
+++ db/migrate/20111221202948_drop_created_at_from_repocops.rb
@@ -1,6 +1,6 @@
 class DropCreatedAtFromRepocops < ActiveRecord::Migration[4.2]
   def change
-    remove_column :repocops, :created_at
-    remove_column :repocops, :updated_at
+    remove_column :repocops, :created_at, :datetime
+    remove_column :repocops, :updated_at, :datetime
   end
 end
