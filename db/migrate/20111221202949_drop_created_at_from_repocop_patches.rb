fdcfd3f1dea40796f6b2d63bdcd0a3de16c757bc
Igor Zubkov <igor.zubkov@gmail.com>
2017-04-07 22:26:00 +0300

    Fix migrations

--- db/migrate/20111221202949_drop_created_at_from_repocop_patches.rb
+++ db/migrate/20111221202949_drop_created_at_from_repocop_patches.rb
@@ -1,6 +1,6 @@
 class DropCreatedAtFromRepocopPatches < ActiveRecord::Migration[4.2]
   def change
-    remove_column :repocop_patches, :created_at
-    remove_column :repocop_patches, :updated_at
+    remove_column :repocop_patches, :created_at, :datetime
+    remove_column :repocop_patches, :updated_at, :datetime
   end
 end
--- db/migrate/20120111035155_drop_acls.rb
+++ db/migrate/20120111035155_drop_acls.rb
@@ -1,5 +1,14 @@
 class DropAcls < ActiveRecord::Migration[4.2]
   def change
-    drop_table :acls
+    drop_table :acls do |t|
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.integer :branch_id
+      t.integer :maintainer_id
+      t.integer :srpm_id
+      t.index :branch_id
+      t.index :maintainer_id
+      t.index :srpm_id
+    end
   end
 end
