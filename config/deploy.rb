14abcdc7717c9a99cacc798584a314afc78b3f67
Igor Zubkov <igor.zubkov@gmail.com>
2017-06-30 14:55:45 +0300

    Update deploy process

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,8 @@ set :conditionally_migrate, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml',
+set :linked_files, fetch(:linked_files, []).push('.env',
+                                                 'config/database.yml',
                                                  'config/newrelic.yml',
                                                  'config/redis.conf',
                                                  'config/redis.yml',
