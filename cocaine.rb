de8bf132505cbde48ac6770c53d00442ae06f065
Igor Zubkov <igor.zubkov@gmail.com>
2017-07-04 22:01:38 +0300

    Update example

--- cocaine.rb
+++ cocaine.rb
@@ -1,5 +1,5 @@
 require 'cocaine'
 
-line = Cocaine::CommandLine.new("rpm", "-qp --queryformat=%{NAME} /Users/igor/opensource/prometheus2.0/spec/data/catpkt-1.0-alt5.src.rpm")
+line = Cocaine::CommandLine.new("rpm", "-qp --queryformat=%{NAME} /Users/biow0lf/opensource/prometheus2.0/spec/data/catpkt-1.0-alt5.src.rpm")
 line.run # => "catpkt"
-line.command_output # => "catpkt"
+puts line.command_output # => "catpkt"
