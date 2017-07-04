85ec3d0e963ba3a8e526d327b30c9f7492ae1557
Igor Zubkov <igor.zubkov@gmail.com>
2017-07-04 12:55:11 +0300

    Rubocop updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  authenticate :user, -> (user) { user.admin? } do
+  authenticate :user, ->(user) { user.admin? } do
     mount PgHero::Engine, at: 'pghero'
   end
 
