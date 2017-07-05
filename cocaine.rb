dfd46bc471b0b8149cb1e7fdd4287052674346a4
Igor Zubkov <igor.zubkov@gmail.com>
2017-07-06 00:52:13 +0300

    Update cocaine example

--- cocaine.rb
+++ cocaine.rb
@@ -3,3 +3,9 @@ require 'cocaine'
 line = Cocaine::CommandLine.new("rpm", "-qp --queryformat=%{NAME} /Users/biow0lf/opensource/prometheus2.0/spec/data/catpkt-1.0-alt5.src.rpm")
 line.run # => "catpkt"
 puts line.command_output # => "catpkt"
+
+p Cocaine::CommandLine.send(:best_runner).class
+
+line = Cocaine::CommandLine.new('date', '', environment: { 'LANG' => 'ru_RU.UTF-8' })
+line.run
+puts line.command_output
