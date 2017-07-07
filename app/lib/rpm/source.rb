e456782d29abf5e0243b028ea216ae25652837a9
Igor Zubkov <igor.zubkov@gmail.com>
2017-07-07 12:35:09 +0300

    Memoizate RPM::Source#filename

--- app/lib/rpm/source.rb
+++ app/lib/rpm/source.rb
@@ -1,7 +1,7 @@
 module RPM
   class Source < Base
     def filename
-      "#{ name }-#{ version }-#{ release }.src.rpm"
+      @filename ||= "#{ name }-#{ version }-#{ release }.src.rpm"
     end
   end
 end
