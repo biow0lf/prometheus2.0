0d38d55536996cc1aa88a439c46e713008a9a413
Igor Zubkov <igor.zubkov@gmail.com>
2017-07-07 12:43:50 +0300

    RPM::Binary

---
+++ app/lib/rpm/binary.rb
@@ -0,0 +1,15 @@
+module RPM
+  class Binary < Base
+    def filename
+      @filename ||= "#{ name }-#{ version }-#{ release }.#{ arch }.rpm"
+    end
+
+    def arch
+      @arch ||= read('%{ARCH}')
+    end
+
+    def sourcerpm
+      @sourcerpm ||= read('%{SOURCERPM}')
+    end
+  end
+end
