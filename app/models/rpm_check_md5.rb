50fedd74731f7e54d5e3844eeb9ca8bb931b82d8
Igor Zubkov <igor.zubkov@gmail.com>
2018-04-16 16:59:55 +0300

    Fix packages import

--- app/models/rpm_check_md5.rb
+++ app/models/rpm_check_md5.rb
@@ -2,8 +2,8 @@
 
 class RPMCheckMD5
   def self.check_md5(file)
-    output = `export LANG=C && rpm -K --nogpg #{ file }`
-    if !output.empty? && output.chop.split(': ').last == 'md5 OK'
+    output = `export LANG=C && rpm -K --nosignature #{ file }`
+    if !output.empty? && output.chop.split(': ').last == 'sha1 md5 OK'
       true
     else
       false
