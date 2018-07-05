5b271a8c6e576e588b776447f14adc0bc48487eb
Malo Skrylevo <majioa@yandex.ru>
2018-07-05 11:26:19 +0300

    debug msgs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,14 @@
 class HomeController < ApplicationController
   def index
     @branch = Branch.find_by!(name: params[:branch])
+    logger.debug "1"
     @branches = Branch.order('order_id')
+    logger.debug "2"
     @top15 = Maintainer.top15(@branch)
+    logger.debug "3"
     @srpms = @branch.srpms.where("srpms.created_at > '2010-11-09 09:00:00'").includes(:builder).order('srpms.created_at DESC').page(params[:page]).per(40).decorate
+  rescue
+    logger.debug "E"
+    logger.debug "#{$!}"
   end
 end
