e08e613953453fc3e1747315d66170db2de11964
Malo Skrylevo <majioa@yandex.ru>
2018-07-24 12:54:34 +0300

    Fix migration

--- db/migrate/20180723104300_add_uniq_for_md5_in_srpms.rb
+++ db/migrate/20180723104300_add_uniq_for_md5_in_srpms.rb
@@ -1,16 +1,18 @@
 class AddUniqForMd5InSrpms < ActiveRecord::Migration[5.1]
   def change
-    ActiveRecord::Base.connection.execute("DELETE FROM packages WHERE NOT (srpm_id IN (SELECT id FROM srpms))")
+    add_foreign_key "packages", "srpms", on_delete: :cascade
 
     add_column :srpms, :alias, :string
     add_index :srpms, :alias
 
+    ActiveRecord::Base.connection.execute("DELETE FROM srpms a USING srpms b WHERE a.id > b.id AND a.md5 = b.md5")
+
     change_column_null :srpms, :md5, false
     add_index :srpms, :md5, unique: true
 
+    ActiveRecord::Base.connection.execute("DELETE FROM packages a USING packages b WHERE a.id > b.id AND a.md5 = b.md5")
+
     change_column_null :packages, :md5, false
     add_index :packages, :md5, unique: true
-
-    add_foreign_key "packages", "srpms", on_delete: :cascade
   end
 end
