e45d8cffb20d884d89548a1eb0372292413a5ec0
Malo Skrylevo <majioa@yandex.ru>
2018-08-15 15:32:27 +0300

    fixed staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -10,7 +10,7 @@ server '10.10.3.49', user: 'apache', roles: ['app', 'db', 'web']
 
 set :ssh_options, port: 22
 
-set :rvm_type, :system
+set :rvm_type, :user
 
 set :app_server_host, "staging.office.basealt.ru"
 #set :app_server_host, "10.10.3.49"
